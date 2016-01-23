65652f5435bffeab1817c5d2dfaea69da87a8e14
Kohsuke Kawaguchi <kk@kohsuke.org>
2016-01-23 15:38:23 -0800

    Added initial root object

---
+++ core/src/main/java/io/jenkins/blueocean/BlueOceanUI.java
@@ -0,0 +1,11 @@
+package io.jenkins.blueocean;
+
+import hudson.Extension;
+import io.jenkins.blueocean.embryo.App;
+
+/**
+ * @author Kohsuke Kawaguchi
+ */
+@Extension
+public class BlueOceanUI extends App {
+}
