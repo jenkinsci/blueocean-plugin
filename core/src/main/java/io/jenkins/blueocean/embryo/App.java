f6ed11144f23d5b0a9d92bede4947245e1bf4602
Kohsuke Kawaguchi <kk@kohsuke.org>
2016-01-23 15:35:45 -0800

    basic embyro functionality

---
+++ core/src/main/java/io/jenkins/blueocean/embryo/App.java
@@ -0,0 +1,9 @@
+package io.jenkins.blueocean.embryo;
+
+import hudson.ExtensionPoint;
+
+/**
+ * @author Kohsuke Kawaguchi
+ */
+public abstract class App implements ExtensionPoint {
+}
---
+++ core/src/main/java/io/jenkins/blueocean/embryo/DescriptorFilterImpl.java
@@ -0,0 +1,32 @@
+package io.jenkins.blueocean.embryo;
+
+import hudson.Extension;
+import hudson.ExtensionComponent;
+import hudson.scm.SCM;
+import hudson.tools.ToolInstallation;
+import hudson.widgets.Widget;
+import jenkins.ExtensionFilter;
+import jenkins.model.Jenkins;
+import org.jenkinsci.plugins.variant.OptionalExtension;
+
+/**
+ * Removes unwanted functionalities from Jenkins
+ *
+ * @author Kohsuke Kawaguchi
+ */
+@Extension
+public class DescriptorFilterImpl extends ExtensionFilter {
+    @Override
+    public <T> boolean allows(Class<T> type, ExtensionComponent<T> component) {
+        if (type==Widget.class && isFromCore(component))     return false;
+        if (component.isDescriptorOf(SCM.class)) return false;
+        if (component.isDescriptorOf(ToolInstallation.class)) return false;
+        // TODO: lot more can be removed here
+
+        return true;
+    }
+
+    private <T> boolean isFromCore(ExtensionComponent<T> component) {
+        return component.getInstance().getClass().getClassLoader()==Jenkins.class.getClassLoader();
+    }
+}
