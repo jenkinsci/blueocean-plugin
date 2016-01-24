caf3976e761f61eacae0e16fbc12b251b86d3fda
Kohsuke Kawaguchi <kk@kohsuke.org>
2016-01-23 16:58:19 -0800

    Doc improvement

--- core/src/main/java/io/jenkins/embryo/App.java
+++ core/src/main/java/io/jenkins/embryo/App.java
@@ -3,6 +3,8 @@ package io.jenkins.embryo;
 import hudson.ExtensionPoint;
 
 /**
+ * Thre must be a sole implementation of this, which is the root of the application.
+ *
  * @author Kohsuke Kawaguchi
  */
 public abstract class App implements ExtensionPoint {
