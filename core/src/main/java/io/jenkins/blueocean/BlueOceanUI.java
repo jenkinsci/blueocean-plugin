10daf76790517cad06480b8ebda4ff9d36607b80
Kohsuke Kawaguchi <kk@kohsuke.org>
2016-01-23 16:46:48 -0800

    defined additional route to demonstrate features

--- core/src/main/java/io/jenkins/blueocean/BlueOceanUI.java
+++ core/src/main/java/io/jenkins/blueocean/BlueOceanUI.java
@@ -2,10 +2,17 @@ package io.jenkins.blueocean;
 
 import hudson.Extension;
 import io.jenkins.embryo.App;
+import org.kohsuke.stapler.HttpResponse;
+import org.kohsuke.stapler.HttpResponses;
 
 /**
+ * Root of Blue Ocean UI
+ *
  * @author Kohsuke Kawaguchi
  */
 @Extension
 public class BlueOceanUI extends App {
+    public HttpResponse doHello() {
+        return HttpResponses.plainText("Hello wolrd!");
+    }
 }
