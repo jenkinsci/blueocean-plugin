c76e6c9f964d3cfc34ede623abe33ada287ec29d
Kohsuke Kawaguchi <kk@kohsuke.org>
2016-02-19 17:05:56 -0800

    We want the state transfer to be able to use the tree parameter

--- blueocean-rest/src/main/java/io/jenkins/blueocean/rest/sandbox/Resource.java
+++ blueocean-rest/src/main/java/io/jenkins/blueocean/rest/sandbox/Resource.java
@@ -10,7 +10,11 @@ public abstract class Resource {
      * Returns the DTO object that gets databound to Json/XML etc.
      */
     @WebMethod(name="",method="GET")
-    @JsonResponse
+//    @JsonResponse
+//    public void /*getState*/ doIndex(StaplerRequest req, StaplerResponse rsp) {
+//        new Api(this).doJson(req,rsp);
+//    }
+    @TreeResponse /* this annotation does the above new Api(...).doJson(...) */
     public Object /*getState*/ doIndex() {
         return this;
     }
