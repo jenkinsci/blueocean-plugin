7da2e9214edc57a0b8091b6c60b9f793bcfc27f1
tfennelly <tom.fennelly@gmail.com>
2016-02-22 19:34:00 +0000

    Clean up core .js bundle build a bit

--- blueocean-web/gulpfile.js
+++ blueocean-web/gulpfile.js
@@ -1,11 +1,8 @@
 //
-// See https://github.com/tfennelly/jenkins-js-builder
+// See https://github.com/jenkinsci/js-builder
 //
 var builder = require('jenkins-js-builder');
 
 builder.bundle('src/main/js/blueocean.js')
-//  .withExternalModuleMapping('bootstrap-detached', 'bootstrap:bootstrap3', {addDefaultCSS: true})
-//  .withExternalModuleMapping('handlebars', 'handlebars:handlebars3')
-//  .less('src/main/less/pipelineeditor.less')
   .inDir('target/classes/io/jenkins/blueocean');
 
