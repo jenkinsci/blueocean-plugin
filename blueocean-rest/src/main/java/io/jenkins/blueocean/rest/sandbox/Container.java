0c4fb4711e3df76e74466ca65f3549616b65f8c8
Kohsuke Kawaguchi <kk@kohsuke.org>
2016-02-23 14:11:43 -0800

    Defined a collection GET operation

--- blueocean-rest/src/main/java/io/jenkins/blueocean/rest/sandbox/Container.java
+++ blueocean-rest/src/main/java/io/jenkins/blueocean/rest/sandbox/Container.java
@@ -1,6 +1,13 @@
 package io.jenkins.blueocean.rest.sandbox;
 
+import com.google.common.collect.Iterables;
+import io.jenkins.blueocean.commons.stapler.TreeResponse;
+import org.kohsuke.stapler.WebMethod;
+import org.kohsuke.stapler.verb.GET;
+
 /**
+ * Stapler-bound REST endpoint for a collection of objects.
+ *
  * @author Kohsuke Kawaguchi
  */
 public abstract class Container<T> implements Iterable<T> {
@@ -14,5 +21,14 @@ public abstract class Container<T> implements Iterable<T> {
     // for stapler
     public final T getDynamic(String name) {
         return get(name);
+    }
+
+    /**
+     * When GET is requested on '/', serve the collection
+     */
+    @WebMethod(name="") @GET @TreeResponse
+    public Object[] list() {
+        // TODO: pagenation
+        return Iterables.toArray(this,Object.class);
     }
 }
