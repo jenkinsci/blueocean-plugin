c7a81f6b2c4911eaf249a892104dc5f14f368147
Kohsuke Kawaguchi <kk@kohsuke.org>
2016-02-23 16:29:52 -0800

    Cleaned up unused imports

--- blueocean-rest/src/main/java/io/jenkins/blueocean/rest/sandbox/Organization.java
+++ blueocean-rest/src/main/java/io/jenkins/blueocean/rest/sandbox/Organization.java
@@ -1,8 +1,6 @@
 package io.jenkins.blueocean.rest.sandbox;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import org.kohsuke.stapler.HttpResponse;
-import org.kohsuke.stapler.WebMethod;
 
 /**
  * API endpoint for an organization that houses all the pipelines.
