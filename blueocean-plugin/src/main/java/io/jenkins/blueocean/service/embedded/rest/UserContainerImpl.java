d9db9e6dce8e1ef107f0a68b9687f0c2db252ac2
Kohsuke Kawaguchi <kk@kohsuke.org>
2016-02-24 14:33:16 -0800

    To implement org membership I have to have this

--- blueocean-plugin/src/main/java/io/jenkins/blueocean/service/embedded/rest/UserContainerImpl.java
+++ blueocean-plugin/src/main/java/io/jenkins/blueocean/service/embedded/rest/UserContainerImpl.java
@@ -2,11 +2,14 @@ package io.jenkins.blueocean.service.embedded.rest;
 
 import com.google.common.collect.ImmutableMap;
 import hudson.Extension;
+import hudson.util.AdaptedIterator;
 import io.jenkins.blueocean.api.profile.CreateUserRequest;
 import io.jenkins.blueocean.commons.ServiceException;
 import io.jenkins.blueocean.commons.stapler.JsonBody;
 import io.jenkins.blueocean.rest.sandbox.User;
 import io.jenkins.blueocean.rest.sandbox.UserContainer;
+
+import java.util.Iterator;
 
 /**
  *
@@ -25,5 +28,18 @@ public class UserContainerImpl extends UserContainer {
         hudson.model.User user = hudson.model.User.get(name, false, ImmutableMap.of());
         if (user==null)     return null;
         return new UserImpl(user);
+    }
+
+    /**
+     * Iterates all the users in the system
+     */
+    @Override
+    public Iterator<User> iterator() {
+        return new AdaptedIterator<hudson.model.User, User>(hudson.model.User.getAll()) {
+            @Override
+            protected User adapt(hudson.model.User item) {
+                return new UserImpl(item);
+            }
+        };
     }
 }
