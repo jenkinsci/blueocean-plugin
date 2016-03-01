4ae85740ea9a2749e0c69489a6494b2f822daa11
Josh McDonald <josh@joshmcdonald.info>
2016-03-01 22:38:54 +1100

    UX-31 - Creating CSS theme
     * Start bringing things over from BS into a much smaller package so we need less crap to build the jenkins theme than all of BS
     * Got the default bootstrap theme building from less, and the theme example slightly modified to our needs
     * Watch added
     * More progress correcting missing / incorrect overrides in theme
     * Got everything pink in the bootstrap theme example at least.
     * Jenkins colours in
     * Flatten alert divs
     * Pull in Lato font
     * Buttons flattened
     * Progress on BO-specific example HTML
     * Bring octicons files into build process
     * Bootstrap removed, needs some more things put into the theme, but it's not crazy.
     * Add a nice CSS normalise
     * Fix package.json a bit, shrinkwrap
     * Fix up font sizes
     * Fix a bunch of padding and alignment issues, add code styles, add current weather icons
    
    TODO:
    
    - [ ] Add a minify step for dist, update examples to use it
    - [ ] Make sure package json has correct metadata showing the author as me, as well as somehow pointing out CB footed the bill
    - [ ] Make sure “npm publish” or whatever will not do so without first doing a complete clean and build.
    - [ ] Publish to npm
    - [ ] Basic form components
    - [ ] Pull the css reset into the one jenkins css file, which would then be minimised
    - [ ] Clean out all the styles we’re not using from the css
    - [ ] Extract static HTML from example into React Components for BO to depend on
    
    Squashed commit of the following:
    
    commit 9046b73ddd8b7f3e0bf8cb197cba56fac11c02e3
    Merge: 9bf34ff 1802605
    commit 9bf34ffcdd65e449d44666517ea5dc3c4ee55da9
    commit 51eb9584d6981d9f7145ba68b8f1e878086b43d7
    commit be55dbe1da52fbcefd39c10aaafad28134ae5174
    commit d0552607e98edc23ff4568343b177484631dee79
    commit 88bb1e12c25cad6bd3deb529dd6b0f5e2839766f
    Merge: 921a11d 0be26ff
    commit 921a11d97a4de4489c3292e83cd2493ec6779eb0
    commit 260a05816ef3979cfafe5f244c041326aa39eea6
    commit 554f5f24d32d51454d492f4acd327991a61c173c
    commit 322f5140425b7a4a287c61998c208c1cdd0f2e8d
    commit bbc3a5b02ba5e60eb3f486dc1bb3e91064330d71
    Merge: dfcd07a 3744d56
    commit dfcd07a52593d0af8097f6083c2f4c513782ef9b
    commit 386079beaa5daf054dc926ff07dceddcdf98983b
    commit 9c69a1739203776ddb4e6f8ce4822550a40cc751
    commit 149cd1699b8d646ab7ac21d98ce8b6a5e0ecc472
    commit be09bbcb68f423c644927482cd536bf66b344d9b
    commit a84cc86d83db621275d3173bb846b70496b7c4c2
    commit b0b3b26a39065bb63b34d1bab4e509250975ecb5
    commit 2ebe6e3aa3be51fff2f3226e75e827bf7184a042
    commit 09a4a22144307ec5a88877acf73001bf6ef6b4af
    commit b60c9ec4c335f1b21baba0b37fc71d4c7c2a7c7c
    commit 0ef42762db8dfbbfd7df767d31ec44d79545688a

---
+++ jenkins-bootstrap/Gruntfile.js
@@ -0,0 +1,102 @@
+/*
+Build file for Jenkins Blue Ocean Bootstrap theme.
+*/
+
+var autoprefixerBrowsers = [
+    "Android 2.3",
+    "Android >= 4",
+    "Chrome >= 20",
+    "Firefox >= 24",
+    "Explorer >= 8",
+    "iOS >= 6",
+    "Opera >= 12",
+    "Safari >= 6"
+];
+
+module.exports = function (grunt) {
+    "use strict";
+
+    // Force use of Unix newlines
+    grunt.util.linefeed = "\n";
+
+    // Load grunt tasks automatically. For some reason this is not default behaviour.
+    require("load-grunt-tasks")(grunt);
+
+    // Time how long tasks take. Can help when optimizing build times
+    require("time-grunt")(grunt);
+
+    grunt.initConfig({
+
+        pkg: grunt.file.readJSON("package.json"),
+
+        clean: {
+            dist: "dist"
+        },
+
+        less: {
+            main: {
+                options: {
+                    strictMath: true,
+                    sourceMap: true,
+                    outputSourceFiles: true,
+                    sourceMapURL: "<%= pkg.name %>-theme.css.map",
+                    sourceMapFilename: "dist/css/<%= pkg.name %>-theme.css.map"
+                },
+                src: "less/theme.less",
+                dest: "dist/css/<%= pkg.name %>-theme.css"
+            }
+        },
+
+        autoprefixer: {
+            options: {
+                browsers: autoprefixerBrowsers
+            },
+            main: {
+                options: {
+                    map: true
+                },
+                src: "dist/css/<%= pkg.name %>-theme.css"
+            }
+        },
+
+        copy: {
+            normalize: {
+                expand: true,
+                cwd: "node_modules/normalize.css/",
+                src: [
+                    "normalize.css"
+                ],
+                dest: "dist/css/"
+            },
+            icons: {
+                expand: true,
+                src: [
+                    "icons/*"
+                ],
+                dest: "dist/"    
+            },
+            octicons: {
+                expand: true,
+                cwd: "node_modules/octicons/",
+                src: [
+                    "octicons/octicons.eot",
+                    "octicons/octicons.woff",
+                    "octicons/octicons.ttf",
+                    "octicons/octicons.svg",
+                    "octicons/*.css"
+                ],
+                dest: "dist/"
+            }
+        },
+
+        watch: {
+            less: {
+                files: "less/**/*.less",
+                tasks: "less"
+            }
+        },
+    });
+
+    // Default task.
+    grunt.registerTask("default", ["clean:dist", "less", "autoprefixer", "copy"]);
+}
