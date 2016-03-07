ce28bd805ee59f87aaec94421ac8b8cf61a47c62
tfennelly <tom.fennelly@gmail.com>
2016-03-07 14:37:04 +0000

    Fixing eslint errors

--- blueocean-admin/src/main/js/OrganisationPipelines.jsx
+++ blueocean-admin/src/main/js/OrganisationPipelines.jsx
@@ -1,48 +1,49 @@
-import React, {Component} from 'react';
-import  Pipelines from './components/Pipelines';
-import {components} from '@jenkins-cd/design-language';
+import React, { Component } from 'react';
+import Pipelines from './components/Pipelines';
+import { components } from '@jenkins-cd/design-language';
 
 const { Page } = components;
 
-export default class OrganisationPipelines extends Component {
-  constructor() {
-    super();
-    this.state = {pipelines: []};
-  }
-
-  componentDidMount() {
-    fetchPipelineData((data) => {
-      this.setState({pipelines: data});
-    });
-  }
-
-  render() {
-    const
-      { pipelines } = this.state,
-      link = <a target='_blank' href="/jenkins/view/All/newJob">New Pipeline</a>;
-
-    return <Page>
-      <div>CloudBees {link}</div>
-      {(pipelines && pipelines.length > 0) ? <Pipelines pipelines={pipelines}/> : link}
-    </Page>;
-  }
-}
-
 /** FIXME: Ghetto ajax loading of pipeline data for an org @store*/
 function fetchPipelineData(onLoad) {
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.onreadystatechange =  () => {
-    if (xmlhttp.readyState == XMLHttpRequest.DONE) {
-      if (xmlhttp.status == 200) {
-        console.log(xmlhttp.responseText);
-        var pipes = JSON.parse(xmlhttp.responseText);
-        onLoad(pipes);
-        console.log(pipes);
-      } else {
-        console.log('something else other than 200 was returned')
-      }
+    const xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = () => {
+        if (xmlhttp.readyState === XMLHttpRequest.DONE) {
+            if (xmlhttp.status === 200) {
+                console.log(xmlhttp.responseText);
+                const pipes = JSON.parse(xmlhttp.responseText);
+                onLoad(pipes);
+                console.log(pipes);
+            } else {
+                console.log('something else other than 200 was returned');
+            }
+        }
+    };
+    // TODO: fixme ... the url may not have a "/jenkins" prefix
+    xmlhttp.open('GET', '/jenkins/blue/rest/organizations/jenkins/pipelines/', true);
+    xmlhttp.send();
+}
+
+export default class OrganisationPipelines extends Component {
+    constructor() {
+        super();
+        this.state = { pipelines: [] };
     }
-  };
-  xmlhttp.open("GET", "/jenkins/blue/rest/organizations/jenkins/pipelines/", true);
-  xmlhttp.send();
+
+    componentDidMount() {
+        fetchPipelineData((data) => {
+            this.setState({ pipelines: data });
+        });
+    }
+
+    render() {
+        const { pipelines } = this.state;
+        // TODO: fixme "/jenkins"
+        const link = <a target="_blank" href="/jenkins/view/All/newJob">New Pipeline</a>;
+
+        return (<Page>
+            <div>CloudBees {link}</div>
+            {(pipelines && pipelines.length > 0) ? <Pipelines pipelines={pipelines} /> : link}
+        </Page>);
+    }
 }
--- blueocean-admin/src/main/js/components/Pipelines.jsx
+++ blueocean-admin/src/main/js/components/Pipelines.jsx
@@ -1,49 +1,49 @@
-import React, {Component, PropTypes} from 'react';
+import React, { Component, PropTypes } from 'react';
 import Pipeline from './Pipeline';
-import {components} from '@jenkins-cd/design-language';
+import { components } from '@jenkins-cd/design-language';
 const { Table } = components;
 
 export default class Pipelines extends Component {
 
-  render() {
-    const {pipelines} = this.props;
-    // Early out
-    if (!pipelines) {
-      return null;
+    render() {
+        const { pipelines } = this.props;
+        // Early out
+        if (!pipelines) {
+            return null;
+        }
+        const multiBranch = pipelines.filter(pipeline => { // eslint-disable-line arrow-body-style
+            return !!pipeline.branchNames;
+        });
+        const noMultiBranch = pipelines.filter(pipeline => { // eslint-disable-line arrow-body-style
+            return !pipeline.branchNames;
+        });
+        return (
+            <Table className="multiBranch">
+                <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Status</th>
+                    <th>Branches</th>
+                    <th>Pull Requests</th>
+                    <th></th>
+                </tr>
+                </thead>
+                <tbody>
+                { multiBranch.map(
+                    (pipeline, index) =>
+                        <Pipeline key={index} pipeline={pipeline} />
+                )}
+                { noMultiBranch.map(
+                    (pipeline, index) =>
+                        <Pipeline key={index}
+                          simple={true} // eslint-disable-line react/jsx-boolean-value
+                          pipeline={pipeline}
+                        />)}
+                </tbody>
+            </Table>);
     }
-    const multiBranch = pipelines.filter(pipeline => {
-      return !!pipeline.branchNames;
-    });
-    const noMultiBranch = pipelines.filter(pipeline => {
-      return !pipeline.branchNames;
-    });
-    return (
-      <Table className="multiBranch">
-        <thead>
-        <tr>
-          <th>Name</th>
-          <th>Status</th>
-          <th>Branches</th>
-          <th>Pull Requests</th>
-          <th></th>
-        </tr>
-        </thead>
-        <tbody>
-        { multiBranch.map(
-          (pipeline, index) => <Pipeline
-            key={index}
-            pipeline={pipeline}/>
-        )}
-        { noMultiBranch.map(
-          (pipeline, index) => <Pipeline
-            key={index}
-            simple={true}
-            pipeline={pipeline}/>)}
-        </tbody>
-      </Table>);
-  }
 }
 
 Pipelines.propTypes = {
-  pipelines: PropTypes.array.isRequired
+    pipelines: PropTypes.array.isRequired,
 };
