33c55258760762edf9f185d8e4641a047fb8d296
Josh McDonald <josh@joshmcdonald.info>
2016-03-07 12:11:56 +1100

    Attempt to run grunt bin directly

--- jenkins-design-language/jenkins-bootstrap/pre-publish.sh
+++ jenkins-design-language/jenkins-bootstrap/pre-publish.sh
@@ -4,5 +4,5 @@ set -e
 # This fix makes baby Jesus cry: We need to test for "node_modules" because npm will do
 # a "prepublish" when pulling in a local dep, but doesn't run "install" first... 
 # It *also* liket to run "prepublish" after "install" which gives us a loop. 
-test -d "node_modules" || npm install 
-npm run grunt
+test -f "node_modules/.bin/grunt" || npm install 
+node_modules/.bin/grunt
