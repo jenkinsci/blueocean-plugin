c477b39b5e782baba957be30a054b6e354546e86
Josh McDonald <josh@joshmcdonald.info>
2016-03-10 10:51:17 +1100

    josh/UX-127 * Changes to HTML and some components to JDLify the OrganisationPipelines screen

--- blueocean-admin/src/main/js/OrganisationPipelines.jsx
+++ blueocean-admin/src/main/js/OrganisationPipelines.jsx
@@ -2,47 +2,57 @@ import React, {Component} from 'react';
 import  Pipelines from './components/Pipelines';
 import {components} from '@jenkins-cd/design-language';
 
-const { Page } = components;
+const { Page, PageHeader, Title } = components;
 
 export default class OrganisationPipelines extends Component {
-  constructor() {
-    super();
-    this.state = {pipelines: []};
-  }
+    constructor() {
+        super();
+        this.state = {pipelines: []};
+    }
 
-  componentDidMount() {
-    fetchPipelineData((data) => {
-      this.setState({pipelines: data});
-    });
-  }
+    componentDidMount() {
+        fetchPipelineData((data) => {
+            this.setState({pipelines: data});
+        });
+    }
 
-  render() {
-    const
-      { pipelines } = this.state,
-      link = <a target='_blank' href="/jenkins/view/All/newJob">New Pipeline</a>;
+    render() {
+        const
+            { pipelines } = this.state,
+            link = <a target='_blank' href="/jenkins/view/All/newJob">New Pipeline</a>;
 
-    return <Page>
-      <div>CloudBees {link}</div>
-      {(pipelines && pipelines.length > 0) ? <Pipelines pipelines={pipelines}/> : link}
-    </Page>;
-  }
+        return (
+            <Page>
+                <PageHeader>
+                    <Title>CloudBees</Title>
+                </PageHeader>
+                <main>
+                    <article>
+                        {/* TODO: Move the link into the Title */}
+                        <div>{link}</div>
+                        {(pipelines && pipelines.length > 0 ? <Pipelines pipelines={pipelines}/>
+                            : <p>No pipelines</p> )}
+                    </article>
+                </main>
+            </Page>);
+    }
 }
 
 /** FIXME: Ghetto ajax loading of pipeline data for an org @store*/
 function fetchPipelineData(onLoad) {
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.onreadystatechange =  () => {
-    if (xmlhttp.readyState == XMLHttpRequest.DONE) {
-      if (xmlhttp.status == 200) {
-        console.log(xmlhttp.responseText);
-        var pipes = JSON.parse(xmlhttp.responseText);
-        onLoad(pipes);
-        console.log(pipes);
-      } else {
-        console.log('something else other than 200 was returned')
-      }
-    }
-  };
-  xmlhttp.open("GET", "/jenkins/blue/rest/organizations/jenkins/pipelines/", true);
-  xmlhttp.send();
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = () => {
+        if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+            if (xmlhttp.status == 200) {
+                console.log(xmlhttp.responseText);
+                var pipes = JSON.parse(xmlhttp.responseText);
+                onLoad(pipes);
+                console.log(pipes);
+            } else {
+                console.log('something else other than 200 was returned')
+            }
+        }
+    };
+    xmlhttp.open("GET", "/jenkins/blue/rest/organizations/jenkins/pipelines/", true);
+    xmlhttp.send();
 }
--- blueocean-web/src/main/js/main.jsx
+++ blueocean-web/src/main/js/main.jsx
@@ -10,26 +10,21 @@ class App extends Component {
     render() {
         return (
             <div id="outer">
-                <header>
-                    <span className="jenkins-logo" />
+                <header className="global-header">
                     <nav>
-                        <div className="nav-link"><a href="/">Home</a></div>
-                        <ExtensionPoint name="jenkins.topNavigation.menu" />
+                        <a href="#">Pipelines</a>
+                        <a href="#">Applications</a>
+                        <a href="#">Reports</a>
+                        <a href="#">Administration</a>
                     </nav>
+                    {/* TODO: <ExtensionPoint name="jenkins.main.globalNav" wrappingElement="nav"/> */}
                 </header>
                 <main>
-                    <div className="label-warning">
-                        This is a label warning styled using the Jenkins Design Language (using CSS class <span className="label-info">label-warning</span>)
-                    </div>
-
                     <ExtensionPoint name="jenkins.main.body"/>
                 </main>
-                <footer>
-                    <p>This is a footer. I'm sure you'll agree.</p>
-                </footer>
             </div>
         );
     }
 }
 
-render(<App />, document.getElementById('blueocean-root'));
+render(<App />, document.getElementById('root'));
