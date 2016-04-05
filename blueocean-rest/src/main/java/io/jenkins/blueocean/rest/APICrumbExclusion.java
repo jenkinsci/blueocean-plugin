79b44436ecedcf1837811281ad5c0c230cbfd064
Ivan Meredith <ivan@ivan.net.nz>
2016-04-06 11:08:00 +1200

    Move APICrumbExclusion to blueocean-rest

--- blueocean-rest/src/main/java/io/jenkins/blueocean/rest/APICrumbExclusion.java
+++ blueocean-rest/src/main/java/io/jenkins/blueocean/rest/APICrumbExclusion.java
@@ -1,4 +1,4 @@
-package io.jenkins.blueocean;
+package io.jenkins.blueocean.rest;
 
 import java.io.IOException;
 
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import hudson.Extension;
 import hudson.ExtensionList;
 import hudson.security.csrf.CrumbExclusion;
+import io.jenkins.blueocean.RootRoutable;
 
 /**
  * This class forces the Blueocean API to require json for POSTs so that we do not need a crumb.
