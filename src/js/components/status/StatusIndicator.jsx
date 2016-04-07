defb93f3a260a288fb2caaa4d3efdbb6913a8f44
Thorsten Scherler <scherler@gmail.com>
2016-04-07 02:41:25 +0200

    [feature/UX-205] Finish move to jdl and finish css to spin and poc to override the the svg circle with css

---
+++ src/js/components/status/StatusIndicator.jsx
@@ -0,0 +1,46 @@
+import React, { Component, PropTypes } from 'react';
+import SvgSpinner from './svgSpinner';
+import SvgStatus from './svgStatus';
+
+const { number, string } = PropTypes;
+
+class StatusIndicator extends Component {
+    render() {
+        const {
+            result,
+            percentage,
+            width = '32px',
+            height = '32px',
+        } = this.props;
+        // early out
+        if (!result && !result.toLowerCase) {
+            return null;
+        }
+        const resultClean = result.toLowerCase();
+        const props = {
+            percentage,
+            height,
+            width,
+            result: resultClean,
+            title: resultClean,
+        };
+        return (resultClean === 'running' || resultClean === 'queued' ? <SvgSpinner
+          {...props}
+        /> : <SvgStatus
+          {...props}
+        />);
+    }
+}
+
+StatusIndicator.propTypes = {
+    result: string.isRequired,
+    percentage: number,
+    width: string,
+    height: string,
+};
+
+export {
+    StatusIndicator,
+    SvgSpinner,
+    SvgStatus,
+};
---
+++ src/js/components/status/svgStatus.jsx
@@ -0,0 +1,74 @@
+import React, { Component, PropTypes } from 'react';
+const { string, object } = PropTypes;
+
+const results = {
+    success: {
+        fill: '#8cc04f',
+        stroke: '#7cb445',
+    },
+    failure: {
+        fill: '#d54c53',
+        stroke: '#cf3a41',
+    }
+};
+
+export default class SvgStatus extends Component {
+    render() {
+        const {
+            result = 'failure',
+            title = 'No title',
+            width = '32px',
+            height = '32px',
+            colors = {
+                backgrounds: {
+                    box: 'none',
+                    inner: results[result.toLowerCase()] ? results[result.toLowerCase()].fill : 'none',
+                    outer: 'none',
+                },
+                strokes: {
+                    inner: results[result.toLowerCase()] ? results[result.toLowerCase()].stroke : 'none',
+                    outer: '#a9c6e6',
+                    path: '#4a90e2',
+
+                },
+            },
+        } = this.props;
+
+        return (<svg xmlns="http://www.w3.org/2000/svg"
+          width={width}
+          height={height}
+          viewBox="0 0 100 100"
+          preserveAspectRatio="xMidYMid"
+        >
+            <title id="title">{title}</title>
+            <rect
+              x="0"
+              y="0"
+              width="100"
+              height="100"
+              fill={colors.backgrounds.box}
+              className="bk"
+            />
+
+            <circle
+              className={result}
+              cx="50"
+              cy="50"
+              r="40"
+              stroke={colors.strokes.inner}
+              fill={colors.backgrounds.inner}
+              strokeWidth="2"
+              strokeLinecap="round"
+            />
+
+        </svg>);
+    }
+}
+
+SvgStatus.propTypes = {
+    title: string,
+    result: string,
+    width: string,
+    height: string,
+    colors: object,
+};
