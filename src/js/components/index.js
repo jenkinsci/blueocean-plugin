302b12c0e7149f292ffd2b931c9ddae9e90d4621
Josh McDonald <josh@joshmcdonald.info>
2016-04-07 22:12:42 +1000

    UX-203 / UX-204  * Add TabBar components  * Integrate TabBar with pipelines screens  * Create new parent for pipeline views  * Remove pipelinePropProvider
    Squashed commit of the following:
    
    commit 6d4687efbbf5cd4926ed4918697af9466b2b1b2e
    commit f0e9888ac00a5d3977446c0a01d044448e8f7817

--- src/js/components/index.js
+++ src/js/components/index.js
@@ -2,5 +2,5 @@ export {ModalView, ModalBody, ModalStyles, ModalHeader} from './modal/modalview'
 export {WeatherIcon} from './weather-icon';
 export {Page} from './page';
 export {GlobalHeader, GlobalNav} from './global-header';
-export {PageHeader, Title, PageTabs} from './page-header';
+export {PageHeader, Title, PageTabs, TabLink} from './page-header';
 export {Table} from './table';
