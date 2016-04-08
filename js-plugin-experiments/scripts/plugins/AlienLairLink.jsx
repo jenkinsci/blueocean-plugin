035d0255d1e5420c0c7ca2bea8afb09f3e969f2d
Michael Neale <michael.neale@gmail.com>
2016-04-08 16:47:42 +1000

    cleanup old experiments and actually add an ExtensionPoint for real

--- js-plugin-experiments/scripts/plugins/AlienLairLink.jsx
+++
@@ -1,9 +0,0 @@
-import React, {Component} from 'react';
-
-export default class AlienLairLink extends Component {
-    render() {
-        return <div>
-            <a href="#">This is a link to the lair</a>
-        </div>;
-    }
-}
