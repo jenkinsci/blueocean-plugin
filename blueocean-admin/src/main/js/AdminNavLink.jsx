3aae567f186ef5d22d82341ae61fec7485961eb8
Michael Neale <michael.neale@gmail.com>
2016-04-11 12:13:42 +1000

    cleanup as per #UX-260

--- blueocean-admin/src/main/js/AdminNavLink.jsx
+++ blueocean-admin/src/main/js/AdminNavLink.jsx
@@ -2,6 +2,6 @@ import React, { Component } from 'react';
 
 export default class AdminNavLink extends Component {
     render() {
-        return <span className="nav-link"><a href="#">Admin (from admin plugin)</a></span>;
+        return <div></div>;
     }
 }
