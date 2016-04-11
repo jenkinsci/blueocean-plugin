326a698d6716cc2abe680548a6224ede73cd0fc1
Thorsten Scherler <scherler@gmail.com>
2016-04-11 13:19:13 +0200

    [feature/UX-233] Remove modal view from branches since this was not asked for

--- blueocean-admin/src/main/js/components/Branches.jsx
+++ blueocean-admin/src/main/js/components/Branches.jsx
@@ -1,6 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import moment from 'moment';
-import { WeatherIcon, ModalView, ModalBody, StatusIndicator } from '@jenkins-cd/design-language';
+import { WeatherIcon, StatusIndicator } from '@jenkins-cd/design-language';
 
 const { object } = PropTypes;
 
@@ -18,38 +18,10 @@ export default class Branches extends Component {
         const { latestRun, weatherScore, name } = data;
         const { result, endTime, changeSet, state } = latestRun;
         const { commitId, msg } = changeSet[0] || {};
-        const afterClose = () => this.setState({ isVisible: false });
-        const open = () => this.setState({ isVisible: true });
         return (<tr key={name}>
             <td><WeatherIcon score={weatherScore} /></td>
             <td>
-                {
-                    this.state.isVisible && <ModalView hideOnOverlayClicked
-                      title={`Branch ${name}`}
-                      isVisible={this.state.isVisible}
-                      afterClose={afterClose}
-                    >
-                        <ModalBody>
-                            <dl>
-                                <dt>Health</dt>
-                                <dd><WeatherIcon score={weatherScore} /></dd>
-                                <dt>Status</dt>
-                                <dd>{result}</dd>
-                                <dt>Branch</dt>
-                                <dd>{decodeURIComponent(name)}</dd>
-                                <dt>Commit</dt>
-                                <dd>{commitId || '-'}</dd>
-                                <dt>Message</dt>
-                                <dd>{msg || '-'}</dd>
-                                <dt>Completed</dt>
-                                <dd>{moment(endTime).fromNow()}</dd>
-                            </dl>
-                        </ModalBody>
-                    </ModalView>
-                }
-                <a onClick={open}>
-                    <StatusIndicator result={result === 'UNKNOWN' ? state : result} />
-                </a>
+                <StatusIndicator result={result === 'UNKNOWN' ? state : result} />
             </td>
             <td>{decodeURIComponent(name)}</td>
             <td>{commitId || '-'}</td>
