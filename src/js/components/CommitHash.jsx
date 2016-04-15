f4b5e1d32a431b34b706999bda8071adedee92de
Cliff Meyers <cmeyers@cloudbees.com>
2016-04-15 11:21:54 -0400

    UX-200: build a reusable component that displays a commit hash

---
+++ src/js/components/CommitHash.jsx
@@ -0,0 +1,23 @@
+import React, {Component, PropTypes} from 'react';
+
+/**
+ * Displays a commit hash in the proper style.
+ */
+export default class CommitHash extends Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        if (this.props.changeset && this.props.changeset.commitId) {
+            return (
+                <code className="hash">{this.props.changeset.commitId.substring(0, 8)}</code>
+            );
+        }
+        return '-';
+    }
+}
+
+CommitHash.propTypes = {
+    changeset: PropTypes.object.isRequired,
+};
