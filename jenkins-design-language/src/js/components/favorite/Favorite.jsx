c39d2815fbac131d966e70d418d7425e5222899c
Cliff Meyers <cmeyers@cloudbees.com>
2016-04-15 09:02:23 -0400

    UX-201: use more sensible export approach

--- jenkins-design-language/src/js/components/favorite/Favorite.jsx
+++ jenkins-design-language/src/js/components/favorite/Favorite.jsx
@@ -1,6 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 
-export class Favorite extends Component {
+export default class Favorite extends Component {
     constructor(props){
         super(props);
         this.state = {
