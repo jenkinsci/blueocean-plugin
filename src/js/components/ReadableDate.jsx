4c3ed133c659120a33c145207158c42970bd26a8
Cliff Meyers <cmeyers@cloudbees.com>
2016-04-19 13:50:18 -0400

    UX-261: enhance ReadableDate to display a dash when bogus date is passed in; use non-deprecated moment constructor w/ explicit 8601 format

--- src/js/components/ReadableDate.jsx
+++ src/js/components/ReadableDate.jsx
@@ -4,7 +4,7 @@ import moment from 'moment';
 /**
  * Displays a date in moment's "fromNow" format, e.g. "2 hours ago", "5 days ago" etc
  * Also displays the original date on hover.
- * Expects "date" to be passed in as ISO string (or any parseable by moment)
+ * Expects "date" to be passed in as ISO-8601 string
  */
 export class ReadableDate extends Component {
     constructor() {
@@ -12,11 +12,17 @@ export class ReadableDate extends Component {
     }
 
     render() {
-        const display = moment(this.props.date).fromNow();
+        if (this.props.date) {
+            const date = moment(this.props.date, moment.ISO_8601);
 
-        return (
-            <span title={this.props.date}>{display}</span>
-        );
+            if (date.isValid()) {
+                return (
+                    <span title={this.props.date}>{date.fromNow()}</span>
+                );
+            }
+        }
+
+        return (<span>-</span>);
     }
 }
 
