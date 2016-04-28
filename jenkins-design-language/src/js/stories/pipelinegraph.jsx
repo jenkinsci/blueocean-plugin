04e9ba995f169528c9d8e32c634c66c12ef4dcb1
Josh McDonald <josh@joshmcdonald.info>
2016-04-28 19:23:19 +1000

    Add pipeline graph constructor story, with required changes to component
    Squashed commit of the following:
    
    commit b1fa6aac2e69d11c5e3e9787487da90b4b0a60c2
    commit 092f6d182d28e80616e249678b09b2174997b284
    commit a1a9e0da420d757ed922e0fc8f551ab74f29e9e3

--- jenkins-design-language/src/js/stories/pipelinegraph.jsx
+++ jenkins-design-language/src/js/stories/pipelinegraph.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, {Component, PropTypes} from 'react';
 import { storiesOf } from '@kadira/storybook';
-import PipelineGraph, {pipelineStageState} from '../components/PipelineGraph.jsx';
+import PipelineGraph, {pipelineStageState, defaultLayout} from '../components/PipelineGraph.jsx';
 
 storiesOf('PipelineGraph', module)
     .add('Mixed', renderMultiParallelPipeline)
+    .add('Fat', renderFlatPipelineFat)
     .add('Legend', renderFlatPipeline)
+    .add('Constructor', renderConstructomatic)
     .add('Listeners', renderListenersPipeline)
     .add('Parallel', renderParallelPipeline)
     .add('Parallel (Deep)', renderParallelPipelineDeep);
@@ -21,6 +23,69 @@ function renderFlatPipeline() {
     ];
 
     return <div><PipelineGraph stages={stages}/></div>;
+}
+
+function renderFlatPipelineFat() {
+
+    const stages = [
+        makeNode("Success", [], pipelineStageState.success),
+        makeNode("Failure", [], pipelineStageState.failure),
+        makeNode("Running", [
+            makeNode("Job 1", [], pipelineStageState.running),
+            makeNode("Job 2", [], pipelineStageState.running),
+            makeNode("Job 3", [], pipelineStageState.running)
+        ]),
+        makeNode("Queued", [
+            makeNode("Job 4", [], pipelineStageState.queued),
+            makeNode("Job 5", [], pipelineStageState.queued),
+            makeNode("Job 6", [], pipelineStageState.queued),
+            makeNode("Job 7", [], pipelineStageState.queued),
+            makeNode("Job 8", [], pipelineStageState.queued)
+        ]),
+        makeNode("Not Built", [], pipelineStageState.notBuilt),
+        makeNode("Bad data", [], "this is not my office")
+    ];
+
+    const layout = {
+        connectorStrokeWidth: 10,
+        nodeRadius: 20,
+        curveRadius: 10,
+    };
+
+    return (
+        <div style={{padding:10}}>
+            <h1>Same data, different layout</h1>
+            <h3>Normal</h3>
+            <PipelineGraph stages={stages}/>
+            <h3>Fat</h3>
+            <PipelineGraph stages={stages} layout={layout}/>
+        </div>
+    );
+}
+
+
+function renderConstructomatic() {
+
+    const stages = [
+        makeNode("Success", [], pipelineStageState.success),
+        makeNode("Failure", [], pipelineStageState.failure),
+        makeNode("Running", [
+            makeNode("Job 1", [], pipelineStageState.running),
+            makeNode("Job 2", [], pipelineStageState.running),
+            makeNode("Job 3", [], pipelineStageState.running)
+        ]),
+        makeNode("Queued", [
+            makeNode("Job 4", [], pipelineStageState.queued),
+            makeNode("This is Job number 5", [], pipelineStageState.queued),
+            makeNode("Job 6", [], pipelineStageState.queued),
+            makeNode("Job 7", [], pipelineStageState.queued),
+            makeNode("Job 8", [], pipelineStageState.queued)
+        ]),
+        makeNode("Not Built", [], pipelineStageState.notBuilt),
+        makeNode("Bad data", [], "this is not my office")
+    ];
+
+    return <PipelineGraphConstructionKit stages={stages}/>;
 }
 
 function renderListenersPipeline() {
@@ -110,7 +175,89 @@ function renderParallelPipelineDeep() {
     return <div><PipelineGraph stages={stages}/></div>;
 }
 
+/// Simple helper for data generation
 function makeNode(name, children = [], state = pipelineStageState.notBuilt) {
     const completePercent = (state == pipelineStageState.running) ? Math.floor(Math.random() * 60 + 20) : 50;
     return {name, children, state, completePercent};
 }
+
+/// Wrap a PipelineGraph with some controls to tweak the layout properties
+class PipelineGraphConstructionKit extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {layout: defaultLayout};
+    }
+
+    control(label, property, min, max) {
+        const value = this.state.layout[property];
+
+        const changed = (e) => {
+            const value = e.target.value;
+
+            let layout = Object.assign({}, this.state.layout);
+            layout[property] = parseInt(value);
+
+            this.setState({layout});
+        };
+
+        return (
+            <tr>
+                <td>{label}</td>
+                <td><input type="range" min={min} max={max} defaultValue={value} onChange={changed}/></td>
+                <td>{value}</td>
+            </tr>
+        );
+    }
+
+    render() {
+
+        const wrapperStyle = {
+            margin: "5px",
+            padding: "10px",
+            border: "dashed 1px #ccc",
+            borderRadius: "10px"
+        };
+
+        const tableStyle = {
+            width: "auto",
+            borderSpacing: "5px",
+            borderCollapse: "separate"
+        };
+
+        const controlDivStyle = {
+            display:"flex",
+            justifyContent:"center"
+        };
+
+        return (
+            <div style={wrapperStyle}>
+                <h1>PipelineGraph Construct-o-matic&trade;</h1>
+                <div style={controlDivStyle}>
+                    <table style={tableStyle}>
+                        <tbody>
+                        {this.control("Line Thickness", "connectorStrokeWidth", 1, 20)}
+                        {this.control("Curve Radius", "curveRadius", 0, 50)}
+                        {this.control("H Spacing", "nodeSpacingH", 10, 200)}
+                        {this.control("V Spacing", "nodeSpacingV", 10, 200)}
+                        </tbody>
+                    </table>
+                    <table style={tableStyle}>
+                        <tbody>
+                        {this.control("Node radius", "nodeRadius", 5, 40)}
+                        {this.control("Big Label offset", "labelOffsetV", 0, 100)}
+                        {this.control("Small Label offset", "smallLabelOffsetV", 0, 100)}
+                        </tbody>
+                    </table>
+                </div>
+                <PipelineGraph stages={this.props.stages}
+                               layout={this.state.layout}
+                               onNodeClick={this.props.onNodeClick}/>
+            </div>
+        );
+    }
+}
+
+PipelineGraphConstructionKit.propTypes = {
+    stages: PropTypes.array,
+    onNodeClick: PropTypes.func
+};
