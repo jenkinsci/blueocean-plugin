1ce547e71a6520e6be2cd448a5c8dcbf79d56e36
Cliff Meyers <cmeyers@cloudbees.com>
2016-05-09 18:31:15 -0400

    [UX-76] add an empty state for the "Changes" tab in Run Detail modal

--- blueocean-admin/src/main/js/components/RunDetailsChanges.jsx
+++ blueocean-admin/src/main/js/components/RunDetailsChanges.jsx
@@ -1,13 +1,33 @@
 import React, { Component, PropTypes } from 'react';
-import { CommitHash } from '@jenkins-cd/design-language';
+import { CommitHash, EmptyStateView } from '@jenkins-cd/design-language';
 import Table from './Table';
 
 const { array } = PropTypes;
 
 export default class RunDetailsChanges extends Component {
+    renderEmptyState() {
+        return (
+            <EmptyStateView iconName="shoes">
+                <h1>Ready, get set...</h1>
+
+                <p>
+                    Hmm, looks like there were no changesets associated with this run.
+                </p>
+
+                <p>
+                    This should happen only for the first push of a branch.
+                </p>
+            </EmptyStateView>
+        );
+    }
+
     render() {
         const { runs } = this.props;
         const { changeSet } = runs[0];
+
+        if (!changeSet || !changeSet.length) {
+            return this.renderEmptyState();
+        }
 
         const headers = [
             'Commit',
