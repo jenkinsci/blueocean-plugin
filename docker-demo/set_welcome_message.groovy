d4539f2ed427147651c81eba49ddd7d24f7c8ae2
Nicolas De Loof <nicolas.deloof@gmail.com>
2016-05-29 20:45:33 +0200

    make it more visible to user the credentials we have set for the demo
    Signed-off-by: Nicolas De Loof <nicolas.deloof@gmail.com>

--- docker-demo/set_welcome_message.groovy
+++ docker-demo/set_welcome_message.groovy
@@ -3,6 +3,6 @@ jenkins.model.Jenkins.instance.setSystemMessage(
 <p>Welcome to Jenkins Blue Ocean Demo. Click on the image below: </p>
 <p><a href="/blue"><img src="/userContent/blueocean-demo.png" /></a></p>
 
-<p>log in with <em>admin/admin</em></p>
+<p>log in with <strong>admin/admin</strong></p>
 """
 )
