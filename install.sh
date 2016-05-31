9f93db1d577fd08007c8a0eb411f0acd6ff4139b
Paul Dragoonis <dragoonis@gmail.com>
2016-05-31 22:14:59 +0100

    The beginning of the blueocean installer

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# todo - check for mvn being installed
+
+# todo - check for version of maven installed
+
+# todo - check for version of java installed
+
+mvn clean install \
+&& cd ${DIR}/blueocean-plugin \
+&& mvn hpi:run
