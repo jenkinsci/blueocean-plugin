a1bd4bc54d19187193be639388050452d27492df
Cliff Meyers <cmeyers@cloudbees.com>
2016-05-31 16:29:02 -0400

    [UX-295] add storybook for Table

--- src/js/stories/index.jsx
+++ src/js/stories/index.jsx
@@ -3,5 +3,6 @@ require('./filesize.jsx');
 require('./modalview.jsx');
 require('./pipelinegraph.jsx');
 require('./pipelineresult.jsx');
+require('./table.jsx');
 require('./toast.jsx');
 require('./statusIndicatorStories.jsx');
