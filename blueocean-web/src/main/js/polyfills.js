4f91239b5811cdaf2f00101350ceb4935c75ce32
tfennelly <tom.fennelly@gmail.com>
2016-06-08 16:55:28 +0100

    Switch to using the babeljs ES6 polyfill
    https://babeljs.io/docs/usage/polyfill/

--- blueocean-web/src/main/js/polyfills.js
+++ blueocean-web/src/main/js/polyfills.js
@@ -1,26 +1,3 @@
-// Misc polyfills.
-
-//
-// Object.assign polyfill
-// See https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Object/assign#Polyfill
-//
-if (typeof Object.assign != 'function') {
-    Object.assign = function (target) {
-        if (target == null) {
-            throw new TypeError('Cannot convert undefined or null to object');
-        }
-
-        target = Object(target);
-        for (var index = 1; index < arguments.length; index++) {
-            var source = arguments[index];
-            if (source != null) {
-                for (var key in source) {
-                    if (Object.prototype.hasOwnProperty.call(source, key)) {
-                        target[key] = source[key];
-                    }
-                }
-            }
-        }
-        return target;
-    };
-}
+// TODO: maybe we should create a separate bundle for this
+// and only include it in the top level .jelly file for MS IE.
+require('babel-polyfill');
