b0be61c6606856c13477b2c6dbfc2d3410ad677a
Cliff Meyers <cmeyers@cloudbees.com>
2016-06-08 17:07:32 -0400

    [UX-310] delint

--- blueocean-dashboard/src/main/js/AllPipelines.jsx
+++ blueocean-dashboard/src/main/js/AllPipelines.jsx
@@ -1,4 +1,4 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
 import OrganisationPipelines from './OrganisationPipelines';
 
 class AllPipelines extends Component {
