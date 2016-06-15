63a850376a3445b9521845a850001d9bb385b6e0
Thorsten Scherler <scherler@gmail.com>
2016-06-15 17:14:22 +0900

    [feature/reduxTools] Adding support for https://chrome.google.com/webstore/detail/redux-devtools/lmhkpmbekcpmknklioeibfkpmmfibljd

--- blueocean-web/src/main/js/redux/configureStore.js
+++ blueocean-web/src/main/js/redux/configureStore.js
@@ -4,7 +4,8 @@ import thunk from 'redux-thunk';
 export function configureStore(rootReducer):Object {
 
     const finalCreateStore = compose(
-      applyMiddleware(thunk)
+      applyMiddleware(thunk),
+      window.devToolsExtension ? window.devToolsExtension() : f => f
     )(createStore);
 
     return finalCreateStore(rootReducer);
