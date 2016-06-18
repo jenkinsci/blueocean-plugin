b5aee18c907f6633a7822cd2dcf32e16c607eebc
Thorsten Scherler <scherler@gmail.com>
2016-06-18 14:32:07 +0900

    eslint - formating changes and fix offences

--- blueocean-dashboard/src/main/js/components/LogToolbar.jsx
+++ blueocean-dashboard/src/main/js/components/LogToolbar.jsx
@@ -24,8 +24,7 @@ export default class LogToolbar extends Component {
                   filename: fileName,
                   contents: data,
                   mime: 'text/plain',
-              } }}
-              />}
+              } }}/>}
 
                 <a {...{
                     title: 'Display the log in new window',
