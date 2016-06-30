ca6b7d8855b333cae0f712e7bfa98ab88490c522
Cliff Meyers <cmeyers@cloudbees.com>
2016-06-30 14:44:36 -0400

    [JENKINS-35828] first draft of pipeline card

--- blueocean-personalization/src/main/js/components/PipelineCard.jsx
+++ blueocean-personalization/src/main/js/components/PipelineCard.jsx
@@ -1,14 +1,112 @@
 /**
  * Created by cmeyers on 6/28/16.
  */
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { Icon } from 'react-material-icons-blue';
+import { Favorite, LiveStatusIndicator } from '@jenkins-cd/design-language';
 
+/**
+ * PipelineCard displays an informational card about a Pipeline and its status.
+ *
+ * Properties:
+ * status: 'result' or 'status' value e.g. 'success', 'failure', etc.
+ * percentage: for status=running, the percent complete
+ * organization: name of org
+ * pipeline: name of pipeline
+ * branch: name of branch
+ * commitId: ID of commit
+ * favorite: whether or not the pipeline is favorited
+ * onRunClick: callback invoked when 'Run Again' is clicked
+ * onFavoriteToggle: callback invokved when favorite checkbox is toggled.
+ */
 export class PipelineCard extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            favorite: false,
+        };
+    }
+
+    componentWillMount() {
+        this._updateState(this.props);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (this.props.favorite !== nextProps.favorite) {
+            this._updateState(nextProps);
+        }
+    }
+
+    _updateState(props) {
+        this.setState({
+            favorite: props.favorite,
+        });
+    }
+
+    _onRunClick() {
+        if (this.props.onRunClick) {
+            this.props.onRunClick();
+        }
+    }
+
+    _onFavoriteToggle() {
+        var value = !this.state.favorite;
+        this.setState({
+            favorite: value,
+        });
+
+        if (this.props.onFavoriteToggle) {
+            this.props.onFavoriteToggle(value);
+        }
+    }
+
+    _getBackgroundClass(status) {
+        return status !== null && status.length > 0 ?
+            `${status.toLowerCase()}-bg-lite` :
+            '';
+    }
+
     render() {
+        const iconStyle = { fill: '#fff', cursor: 'pointer' };
+        const bgClass = this._getBackgroundClass(this.props.status);
+
         return (
-            <div className="pipeline-card">
-                AAA
+            <div className={`pipeline-card ${bgClass}`}>
+                <LiveStatusIndicator result={this.props.status} noBackground />
+
+                <span className="name">
+                    {this.props.organization} / {this.props.pipeline}
+                </span>
+
+                <span className="branch">{this.props.branch}</span>
+                <pre className="commit">#{this.props.commitId}</pre>
+
+                <a className="run" title="Run Again" onClick={() => this._onRunClick()}>
+                    <Icon size={24} icon="replay" style={iconStyle} />
+                </a>
+
+                <span className="actions">
+                    <Favorite checked={this.state.favorite} darkTheme onToggle={() => this._onFavoriteToggle()} />
+                </span>
             </div>
         );
     }
 }
+
+PipelineCard.propTypes = {
+    status: PropTypes.string,
+    percentage: PropTypes.number, // TODO: might need startTime and estimatedDuration
+    organization: PropTypes.string,
+    pipeline: PropTypes.string,
+    branch: PropTypes.string,
+    commitId: PropTypes.string,
+    favorite: PropTypes.bool,
+    onRunClick: PropTypes.func,
+    onFavoriteToggle: PropTypes.func,
+};
+
+PipelineCard.defaultProps = {
+    favorite: false,
+};
--- blueocean-personalization/src/main/js/stories/PipelineCardStories.jsx
+++ blueocean-personalization/src/main/js/stories/PipelineCardStories.jsx
@@ -2,15 +2,28 @@
  * Created by cmeyers on 6/28/16.
  */
 import React from 'react';
-import { storiesOf } from '@kadira/storybook';
+import { action, storiesOf } from '@kadira/storybook';
 
 import { PipelineCard } from '../components/PipelineCard';
 
 storiesOf('PipelineCard', module)
-    .add('test', scenario1);
+    .add('all states', () => {
+        const style = { padding: '10px' };
+        const style2 = { paddingBottom: '10px' };
+        const states = 'SUCCESS,QUEUED,RUNNING,FAILURE,ABORTED,UNSTABLE,NOT_BUILT,UNKNOWN'.split(',');
 
-function scenario1() {
-    return (
-        <PipelineCard />
-    );
-}
+
+        return (
+            <div style={style}>
+            { states.map(s =>
+                <div key={s} style={style2}>
+                    <PipelineCard status={s} organization="jenkinsci" pipeline="blueocean"
+                      branch="master" commitId="447d8e1" favorite
+                      onRunClick={action('run')} onFavoriteToggle={action('toggle')}
+                    />
+                </div>
+            ) }
+            </div>
+        );
+    });
+
