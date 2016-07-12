3cecbde3c0b13b567fac5f8a901172ca39872aab
kzantow <kzantow@gmail.com>
2016-07-13 06:29:02 +0900

    JENKINS-36336 - show list of fixed tests

--- blueocean-dashboard/src/main/js/components/testing/TestResults.jsx
+++ blueocean-dashboard/src/main/js/components/testing/TestResults.jsx
@@ -7,23 +7,28 @@ import moment from 'moment';
 const TestCaseResultRow = (props) => {
     const t = props.testCase;
     const duration = moment.duration(Number(t.duration), 'milliseconds').humanize();
-    const expandable = t.errorStackTrace;
 
-    let testDetails = !expandable ? null : (<div className="test-details">
-        <div className="test-detail-text" style={{ display: 'none' }}>
-            {duration}
-        </div>
-        <div className="test-console">
-            <h4>Error</h4>
-            <div className="error-message">
-                {t.errorDetails}
+    let testDetails = null;
+    
+    if (t.errorStackTrace) {
+        testDetails = (
+            <div className="test-details">
+                <div className="test-detail-text" style={{ display: 'none' }}>
+                    {duration}
+                </div>
+                <div className="test-console">
+                    <h4>Error</h4>
+                    <div className="error-message">
+                        {t.errorDetails}
+                    </div>
+                    <h4>Output</h4>
+                    <div className="stack-trace">
+                        {t.errorStackTrace}
+                    </div>
+                </div>
             </div>
-            <h4>Output</h4>
-            <div className="stack-trace">
-                {t.errorStackTrace}
-            </div>
-        </div>
-    </div>);
+        );
+    }
     
     let statusIndicator = null;
     switch (t.status) {
@@ -33,6 +38,7 @@ const TestCaseResultRow = (props) => {
     case 'SKIPPED':
         statusIndicator = StatusIndicator.validResultValues.unstable;
         break;
+    case 'FIXED':
     case 'PASSED':
         statusIndicator = StatusIndicator.validResultValues.success;
         break;
@@ -63,6 +69,7 @@ export default class TestResult extends Component {
         
         // possible statuses: PASSED, FAILED, SKIPPED
         const failures = tests.filter(t => t.status === 'FAILED');
+        const fixed = tests.filter(t => t.status === 'FIXED');
         const skipped = tests.filter(t => t.status === 'SKIPPED');
         const newFailures = failures.filter(t => t.age === 1);
         const existingFailures = failures.filter(t => t.age > 1);
@@ -70,6 +77,7 @@ export default class TestResult extends Component {
         let passBlock = null;
         let newFailureBlock = null;
         let existingFailureBlock = null;
+        let fixedBlock = null;
         let skippedBlock = null;
         
         if (testResults.failCount === 0) {
@@ -105,6 +113,13 @@ export default class TestResult extends Component {
             ];
         }
 
+        if (fixed.length > 0) {
+            fixedBlock = [
+                <h4>Fixed</h4>,
+                fixed.map((t, i) => <TestCaseResultRow key={i} testCase={t} />),
+            ];
+        }
+
         if (skipped.length > 0) {
             skippedBlock = [
                 <h4>Skipped - {skipped.length}</h4>,
@@ -115,6 +130,7 @@ export default class TestResult extends Component {
         return (<div>
             {newFailureBlock}
             {existingFailureBlock}
+            {fixedBlock}
             {skippedBlock}
             {passBlock}
         </div>);
