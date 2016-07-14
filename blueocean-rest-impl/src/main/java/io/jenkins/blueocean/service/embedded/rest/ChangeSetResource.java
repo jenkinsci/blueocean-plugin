ec86c4da8a5762017ba977f6196f211f9e1108c5
Ivan Meredith <ivan@ivan.net.nz>
2016-07-15 11:20:35 +1200

    [JENKINS-35868] Add BlueChangeSetEntry to rest api

--- blueocean-rest-impl/src/main/java/io/jenkins/blueocean/service/embedded/rest/ChangeSetResource.java
+++ blueocean-rest-impl/src/main/java/io/jenkins/blueocean/service/embedded/rest/ChangeSetResource.java
@@ -5,15 +5,16 @@ import hudson.scm.ChangeLogSet.Entry;
 import hudson.scm.RepositoryBrowser;
 import io.jenkins.blueocean.rest.Reachable;
 import io.jenkins.blueocean.rest.hal.Link;
+import io.jenkins.blueocean.rest.model.BlueChangeSetEntry;
 import io.jenkins.blueocean.rest.model.BlueRun;
 import io.jenkins.blueocean.rest.model.BlueUser;
-import io.jenkins.blueocean.rest.model.Resource;
 import org.kohsuke.stapler.export.Exported;
 import org.kohsuke.stapler.export.ExportedBean;
 
 import java.io.IOException;
 import java.net.URL;
 import java.text.SimpleDateFormat;
+import java.util.Collection;
 
 /**
  * Represents a single commit as a REST resource.
@@ -25,7 +26,7 @@ import java.text.SimpleDateFormat;
  * @author Vivek Pandey
  */
 @ExportedBean
-public class ChangeSetResource extends Resource {
+public class ChangeSetResource extends BlueChangeSetEntry {
     private final ChangeLogSet.Entry changeSet;
     private final Reachable parent;
 
@@ -34,17 +35,13 @@ public class ChangeSetResource extends Resource {
         this.parent = parent;
     }
 
-    @Exported(merge=true)
-    public ChangeLogSet.Entry getDelegate() {
-        return changeSet;
-    }
 
-    @Exported(inline = true)
+    @Override
     public BlueUser getAuthor() {
         return new UserImpl(changeSet.getAuthor());
     }
 
-    @Exported
+    @Override
     public String getTimestamp(){
         if(changeSet.getTimestamp() > 0) {
             return new SimpleDateFormat(BlueRun.DATE_FORMAT_STRING).format(changeSet.getTimestamp());
@@ -53,7 +50,7 @@ public class ChangeSetResource extends Resource {
         }
     }
 
-    @Exported
+    @Override
     public String getUrl() {
         RepositoryBrowser browser = changeSet.getParent().getBrowser();
         if(browser != null) {
@@ -69,7 +66,22 @@ public class ChangeSetResource extends Resource {
     }
 
     @Override
+    public String getCommitId() {
+        return changeSet.getCommitId();
+    }
+
+    @Override
+    public String getMsg() {
+        return changeSet.getMsg();
+    }
+
+    @Override
+    public Collection<String> getAffectedPaths() {
+        return changeSet.getAffectedPaths();
+    }
+
+    @Override
     public Link getLink() {
-        return parent.getLink().rel("changeset/"+getDelegate().getCommitId());
+        return parent.getLink().rel("changeset/"+getCommitId());
     }
 }
