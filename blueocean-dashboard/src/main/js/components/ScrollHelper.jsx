84692913adeb492c6ee797dc1b9126de34780a37
Thorsten Scherler <scherler@gmail.com>
2016-07-20 14:57:05 +0200

    Jenkins 36700 (#360)
    * [JENKINS-36640] make sure that all timeouts are cleared when unmount
    
    * [JENKINS-36640] make sure we have a match
    
    * [JENKINS-36619] fix the bug by properly encoding the fallback URL used if the user navigated directly to run details
    
    * eslint - formating changes and fix offences
    
    * [JENKINS-36700] Fix issue that console log stayed opened, regression test added to https://github.com/jenkinsci/blueocean-acceptance-test/pull/9
    
    * [JENKINS-36640] fix expand of resultItems.
    
    * eslint - formating changes and fix offences
    
    * [JENKINS-36700] add empty state via emptyState
    
    * [JENKINS-36700] remove logHeader when no steps are shown
    
    * [JENKINS-36700] fix linking in long logs, and change scroll behaviour to wait until render method has finished and then scroll
    
    * [JENKINS-36700] JENKINS-36684 remove scroll to item feature

---
+++ blueocean-dashboard/src/main/js/components/ScrollHelper.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+
+/*
+ * Will inject the following functions as properties into the component which is using this.
+ *
+ *  * scrollBottom
+ *  * scrollToAnchorTimeOut
+ *  * scrollToAnchor
+ *
+ *  Sample usage: export default scrollHelper(LogConsole);
+  */
+export const scrollHelper = ComposedComponent => class extends Component {
+    // Find the modal view container and adopt the scrollTop to focus the end
+    scrollBottom() {
+        const nodes = document.getElementsByClassName('content');
+        const element = nodes[0];
+        if (element) {
+            element.scrollTop = element.scrollHeight - element.clientHeight;
+        }
+    }
+
+    // This will scroll to the hash that is selected in the window.location
+    // We assume an element having the id
+    scrollToAnchor() {
+        const anchorName = window.location.hash;
+        if (anchorName) {
+            const anchorElement = document.getElementById(anchorName.replace('#', ''));
+            if (anchorElement) {
+                anchorElement.scrollIntoView();
+            }
+        }
+    }
+
+    render() {
+        // React needs the timeout to have the dom ready
+        const scrollToAnchorTimeOut = (timeout = 1) => setTimeout(() => this.scrollToAnchor(), timeout);
+        // create a composedComponent and inject the functions we want to expose
+        return (<ComposedComponent
+          {...this.props}
+          {...this.state}
+          scrollBottom={this.scrollBottom}
+          scrollToAnchorTimeOut={scrollToAnchorTimeOut}
+          scrollToAnchor={this.scrollToAnchor}
+        />);
+    }
+};
