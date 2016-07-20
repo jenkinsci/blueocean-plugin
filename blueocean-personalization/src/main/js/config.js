1650db28484af4e3f11a6d2a7bf51f23f2a4dfc5
Cliff Meyers <cmeyers@cloudbees.com>
2016-07-19 21:01:00 -0700

    [JENKINS-35831] catch error when running in mocha test

--- blueocean-personalization/src/main/js/config.js
+++ blueocean-personalization/src/main/js/config.js
@@ -5,15 +5,23 @@
 // Passing it around in the react context is silly.
 //
 
+exports.blueoceanAppURL = '/';
+exports.jenkinsRootURL = '';
+
 exports.loadConfig = function () {
-    const headElement = document.getElementsByTagName('head')[0];
+    try {
+        const headElement = document.getElementsByTagName('head')[0];
 
-    // Look up where the Blue Ocean app is hosted
-    exports.blueoceanAppURL = headElement.getAttribute('data-appurl');
+        // Look up where the Blue Ocean app is hosted
+        exports.blueoceanAppURL = headElement.getAttribute('data-appurl');
 
-    if (typeof exports.blueoceanAppURL !== 'string') {
-        exports.blueoceanAppURL = '/';
+        if (typeof exports.blueoceanAppURL !== 'string') {
+            exports.blueoceanAppURL = '/';
+        }
+
+        exports.jenkinsRootURL = headElement.getAttribute('data-rooturl');
+    } catch (error) {
+        // eslint-disable-next-line no-console
+        console.warn('error reading attributes from document; urls will be empty');
     }
-
-    exports.jenkinsRootURL = headElement.getAttribute('data-rooturl');
 };
