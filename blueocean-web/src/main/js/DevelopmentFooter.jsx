2e3222ee24167024dc8fdcc2afac4683f5b1bead
Vivek Pandey <vivek.pandey@gmail.com>
2016-07-22 12:37:15 -0700

    Handle the case when revisionInfo is not there, returning null for now

--- blueocean-web/src/main/js/DevelopmentFooter.jsx
+++ blueocean-web/src/main/js/DevelopmentFooter.jsx
@@ -5,6 +5,12 @@ import revisionInfo from '../../../target/classes/io/jenkins/blueocean/revisionI
 
 export class DevelopmentFooter extends Component {
     render() {
+        if (!revisionInfo || !revisionInfo.name) {
+            // TODO: At minimum we should return Jenkins version. Jenkins version is always present
+             // in X-Hudson HTTP header. Something to be handled elsewhere during load time by
+             // inspecting HTTP response headers
+            return null;
+        }
         return (
           <div className="development-footer">
               <span>Built at {moment(revisionInfo.timestamp).format('Do MMMM YYYY hh:mm A')}&nbsp;</span>
