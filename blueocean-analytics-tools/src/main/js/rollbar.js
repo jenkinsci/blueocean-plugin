e181cf5a9103c0168137f6301700b8c07d5f1153
vivek <vivek.pandey@gmail.com>
2016-07-28 08:50:24 -0700

    JENKINS-36884# Analytics-tools plugin to integrated rollbar (#374)
    * JENKINS-36884# Analytics-tools plugin to integrated rollbar
    
    Also includes infrastrucutre to inject HTML head tags and HTTP header by any plugin that implements BluePageDecorator
    and appropriate jelly files.
    
    * Doc update
    
    * Wrapped rollbar JS code inside IIFE.
    
    * Use rollbar-browser package and build browser bundle
    
    * Fixes and doc update
    
    - Stapler includei jelly tag needs escaping $
    - Added missing dependency in gulpfile.js
    - require should load browser-rollbar

---
+++ blueocean-analytics-tools/src/main/js/rollbar.js
@@ -0,0 +1,59 @@
+function getConfigAttribute(name) {
+    var headElements = document.getElementsByTagName('head');
+    if (headElements.length === 1) {
+        return headElements[0].getAttribute(name);
+    }
+    return undefined;
+}
+
+function normalizeURL(location) {
+    var normalizedUrl = 'http://anon.blueocean.io' + location;
+    var rootUrl = getConfigAttribute('data-rooturl');
+    if (rootUrl && location.startsWith(rootUrl)) {
+        normalizedUrl = 'http://anon.blueocean.io' + location.substring(rootUrl.length - 1, location.length);
+    }
+    return normalizedUrl;
+}
+
+var transformer = function (payload) {
+    payload.data.request.user_ip = '0.0.0.0';
+    payload.data.request.url = normalizeURL(window.location.pathname);
+};
+
+//
+// Configure rollbar ...
+// See https://github.com/rollbar/rollbar.js/tree/master/examples/browserify
+//
+
+var _rollbarConfig = {
+    accessToken: '81f3134dedf44871b9cc0a347b1313df',
+    captureUncaught: true,
+    code_version: window.$blueocean_pluginVersion, // see header.jelly
+    source_map_enabled: true,
+    guess_uncaught_frames: true,
+    transform: transformer
+};
+
+var rollbarBrowser = require('rollbar-browser');
+var Rollbar = rollbarBrowser.init(_rollbarConfig);
+
+// Looking at docs (https://github.com/rollbar/rollbar.js/tree/master/examples/browserify)
+// it seems like they stuff it into a global. We are trying hard not to do that
+// under any circumstances, but maybe this is an exception if it's only going to be
+// used in a closed/controlled env.
+//
+// Soooo .... lets export it to global for now, but as $blueocean_Rollbar ...
+//
+
+window.$blueocean_Rollbar = Rollbar;
+
+//
+// Usage ...
+//
+//try {
+//    foo();
+//    $blueocean_Rollbar.debug('foo() called');
+//} catch (e) {
+//    $blueocean_Rollbar.error('Problem calling foo()', e);
+//}
+
