fcc0a421a4d4204490e5a0412c9eb9fff96e718c
Cliff Meyers <cmeyers@cloudbees.com>
2016-07-29 19:57:45 -0400

    [JENKINS-36582] change animation timing, per James

--- blueocean-personalization/src/main/js/components/DashboardCards.jsx
+++ blueocean-personalization/src/main/js/components/DashboardCards.jsx
@@ -166,8 +166,8 @@ export class DashboardCards extends Component {
         return (
             <div className="favorites-card-stack">
                 <TransitionGroup transitionName="vertical-expand-collapse"
-                  transitionEnterTimeout={150}
-                  transitionLeaveTimeout={150}
+                  transitionEnterTimeout={300}
+                  transitionLeaveTimeout={300}
                 >
                     {favoriteCards}
                 </TransitionGroup>
