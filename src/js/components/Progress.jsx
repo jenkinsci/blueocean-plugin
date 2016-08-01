fead1bf5dfb51f839aa838f8d99aec23569a2630
James Dumay <jdumay@cloudbees.com>
2016-08-01 21:25:34 +1000

    remove unused branch

--- src/js/components/Progress.jsx
+++ src/js/components/Progress.jsx
@@ -11,9 +11,6 @@ export class Progress extends Component {
             groupClasses.push('indeterminate');
             percentage = 12.5;
         }
-        else if (percentage === 100) {
-            percentage = 100;
-        }
         else if (percentage > 100) {
             groupClasses.push('bounce');
             percentage = 10;
