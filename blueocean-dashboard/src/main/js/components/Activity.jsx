edd1ba09a17c427cb3d83a7a6f58c416b9eae8a7
Thorsten Scherler <scherler@gmail.com>
2016-08-22 17:37:56 +0200

    [JENKINS-37021] fix message

--- blueocean-dashboard/src/main/js/components/Activity.jsx
+++ blueocean-dashboard/src/main/js/components/Activity.jsx
@@ -71,7 +71,7 @@ export class Activity extends Component {
 
         const { capabilities } = this.props;
         const isMultiBranchPipeline = capabilities[pipeline._class].contains(MULTIBRANCH_PIPELINE);
-        
+
         // Only show the Run button for non multi-branch pipelines.
         // Multi-branch pipelines have the Run/play button beside them on
         // the Branches/PRs tab.
