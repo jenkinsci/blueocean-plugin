37602dd28bb18cc113e83ecd23531e435da985ba
Cliff Meyers <cmeyers@cloudbees.com>
2016-08-23 11:54:08 -0400

    [JENKINS-36199] assign Toast.id automatically if not added; add some tests

--- blueocean-core-js/src/js/ToastService.js
+++ blueocean-core-js/src/js/ToastService.js
@@ -1,7 +1,7 @@
 /**
  * Created by cmeyers on 8/18/16.
  */
-import { observable } from 'mobx';
+import { observable, computed } from 'mobx';
 
 /**
  * Holds one or more toasts in state for display in UI.
@@ -11,6 +11,10 @@ export class ToastService {
     @observable toasts = [];
 
     newToast(toast) {
+        if (!toast.id) {
+            toast.id = Math.random() * Math.pow(10, 16);
+        }
+
         // TODO: determine why it's necessary to re-set the "toasts" field to trigger the UI update
         const copy = this.toasts.slice();
         copy.push(toast);
@@ -22,4 +26,10 @@ export class ToastService {
             return toast.id !== item.id;
         });
     }
+
+    @computed
+    get count() {
+        return this.toasts ? this.toasts.length : 0;
+    }
+
 }
