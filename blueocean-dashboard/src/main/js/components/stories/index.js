8e58c8bdb2cd93d9b765f82e0a25f786cf280b0b
kzantow <kzantow@gmail.com>
2016-08-26 00:29:45 -0400

    lint

--- blueocean-dashboard/src/main/js/components/stories/index.js
+++ blueocean-dashboard/src/main/js/components/stories/index.js
@@ -1,11 +1,11 @@
-var ext = require('@jenkins-cd/js-extensions');
+const ext = require('@jenkins-cd/js-extensions');
 ext.store.init({
     extensionDataProvider: (cb) => {
         cb([]);
     },
     typeInfoProvider: (type, cb) => {
         cb(null);
-    }
+    },
 });
 
 require('./pipelines');
