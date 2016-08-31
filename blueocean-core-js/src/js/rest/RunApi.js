b78b4cc90a53ecb6a35f597d2342e90766e066f2
Cliff Meyers <cmeyers@cloudbees.com>
2016-08-31 11:07:02 -0400

    [JENKINS-36687] update RunsApi to use new JWT-enabled Fetch library

--- blueocean-core-js/src/js/rest/RunApi.js
+++ blueocean-core-js/src/js/rest/RunApi.js
@@ -1,43 +1,11 @@
 /**
  * Created by cmeyers on 8/29/16.
  */
-import isoFetch from 'isomorphic-fetch';
+import { Fetch } from '../fetch';
 
 import { cleanSlashes } from '../sse/UrlUtils';
 import config from '../sse/config';
 config.loadConfig();
-
-const defaultFetchOptions = {
-    credentials: 'same-origin',
-};
-
-// TODO: remove all this code once JWT Fetch is integration
-function checkStatus(response) {
-    if (response.status >= 300 || response.status < 200) {
-        const error = new Error(response.statusText);
-        error.response = response;
-        throw error;
-    }
-    return response;
-}
-
-function parseJSON(response) {
-    return response.json()
-    // FIXME: workaround for status=200 w/ empty response body that causes error in Chrome
-    // server should probably return HTTP 204 instead
-        .catch((error) => {
-            if (error.message === 'Unexpected end of JSON input') {
-                return {};
-            }
-            throw error;
-        });
-}
-
-const fetch = (url, options) => {
-    return isoFetch(url, options)
-        .then(checkStatus)
-        .then(parseJSON);
-};
 
 export class RunApi {
 
@@ -46,15 +14,13 @@ export class RunApi {
         const runUrl = cleanSlashes(`${path}/${item._links.self.href}/runs/`);
 
         const fetchOptions = {
-            ...defaultFetchOptions,
             method: 'PUT',
             headers: {
                 'Content-Type': 'application/json',
             },
         };
 
-        // once job is queued, SSE will fire and trigger "updateRun" so no need to dispatch an action here
-        return fetch(runUrl, fetchOptions);
+        return Fetch.fetch(runUrl, { fetchOptions });
     }
 
     stopRun(run) {
@@ -63,15 +29,13 @@ export class RunApi {
         const stopUrl = cleanSlashes(`${path}/${runUrl}/stop/?blocking=true&timeOutInSecs=10`);
 
         const fetchOptions = {
-            ...defaultFetchOptions,
             method: 'PUT',
             headers: {
                 'Content-Type': 'application/json',
             },
         };
 
-        // once job is queued, SSE will fire and trigger "updateRun" so no need to dispatch an action here
-        return fetch(stopUrl, fetchOptions);
+        return Fetch.fetch(stopUrl, { fetchOptions });
     }
 
     replayRun(run) {
@@ -80,14 +44,13 @@ export class RunApi {
         const replayPipelineUrl = cleanSlashes(`${path}/${runUrl}/replay/`);
 
         const fetchOptions = {
-            ...defaultFetchOptions,
             method: 'PUT',
             headers: {
                 'Content-Type': 'application/json',
             },
         };
 
-        return fetch(replayPipelineUrl, fetchOptions);
+        return Fetch.fetchJSON(replayPipelineUrl, { fetchOptions });
     }
 
 }
