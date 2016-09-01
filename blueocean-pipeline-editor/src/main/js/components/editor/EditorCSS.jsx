4b3e9ac5d85faf1b7328a0cda6c1a6337f406d32
tfennelly <tom.fennelly@gmail.com>
2016-09-01 19:12:19 +0100

    Add a noddy extension point to trigger rendering of pipeline editor CSS

---
+++ blueocean-pipeline-editor/src/main/js/components/editor/EditorCSS.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+// Noddy component to render via ExtensionRenderer
+// and trigger CSS loading from this plugin
+export default class EditorCSS extends Component {
+    render() {
+        return (<span />);
+    }
+}
