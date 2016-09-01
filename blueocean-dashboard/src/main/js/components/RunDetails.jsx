c530784298123f7229f5891a932c45169a04d2af
Cliff Meyers <cmeyers@cloudbees.com>
2016-09-01 11:04:34 -0400

    [JENKINS-35789] fix a bug where clicking the Re-run button on Run Details would not appopriately trigger a refetch of the Run

--- blueocean-dashboard/src/main/js/components/RunDetails.jsx
+++ blueocean-dashboard/src/main/js/components/RunDetails.jsx
@@ -33,17 +33,43 @@ import IfCapability from './IfCapability';
 const { func, object, any, string } = PropTypes;
 
 class RunDetails extends Component {
+
     componentWillMount() {
+        this._fetchRun(this.props, true);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (!this._didRunChange(this.props.params, nextProps.params)) {
+            return;
+        }
+
+        // in some cases the route params might have actually changed (such as 'runId' during a Re-run) so re-fetch
+        // also don't update the 'previous route' otherwise closing the modal will try to navigate back to last run
+        this._fetchRun(nextProps, false);
+    }
+
+    _fetchRun(props, storePreviousRoute) {
         if (this.context.config && this.context.params) {
-            this.props.fetchRun({
-                organization: this.props.params.organization,
-                pipeline: this.props.params.pipeline,
-                branch: this.props.isMultiBranch && this.props.params.branch,
-                runId: this.props.params.runId,
+            props.fetchRun({
+                organization: props.params.organization,
+                pipeline: props.params.pipeline,
+                branch: props.isMultiBranch && props.params.branch,
+                runId: props.params.runId,
             });
-            this.opener = this.props.previous;
+
+            if (storePreviousRoute) {
+                this.opener = props.previous;
+            }
         }
     }
+
+    _didRunChange(oldParams, newParams) {
+        return oldParams.organization !== newParams.organization ||
+                oldParams.pipeline !== newParams.pipeline ||
+                oldParams.branch !== newParams.branch ||
+                oldParams.runId !== newParams.runId;
+    }
+
     navigateToOrganization() {
         const { organization } = this.props.pipeline;
         const organizationUrl = buildOrganizationUrl(organization);
