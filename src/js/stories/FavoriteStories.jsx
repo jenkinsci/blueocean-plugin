7196f05b3e0b048fc0c7a3e30f5911906619f640
Cliff Meyers <cliff.meyers@gmail.com>
2016-09-07 14:21:33 -0400

    Bug/jenkins 37603 favorite hover state (#88)
    * [JENKINS-37603] use inline SVG in Favorite to more easily support theme'ing and hover states
    
    * [JENKINS-37603] improve readability of CSS by embedding some class names in the SVG
    
    * [JENKINS-37603] fix regression where "dark-yellow" uses an empty yellow star instead of an empty white star (correct)
    
    * update PR template
    
    * update PR template
    
    * [JENKINS-37603] format the SVG nicely and add some comments
    
    * 0.0.76
    
    * [JENKINS-37603] format package.json
    
    * experiment with different way of handling site-publish.sh
    
    * revert version tick for now

--- src/js/stories/FavoriteStories.jsx
+++ src/js/stories/FavoriteStories.jsx
@@ -1,28 +1,31 @@
 import React from 'react';
-import { action, storiesOf } from '@kadira/storybook';
+import { storiesOf } from '@kadira/storybook';
 import { Favorite } from '../components';
 
 storiesOf('Favorite', module)
     .add('default', () => {
         const style = { display: 'flex', alignItems: 'center' };
-        const style2 = { ... style, backgroundColor: 'blue', color: 'white' };
+        const style2 = { ... style, backgroundColor: '#4A90E2', color: 'white' };
 
         return (
             <div>
                 <div style={style}>
-                    <Favorite checked /> Checked
+                    <Favorite /> Default
                 </div>
                 <div style={style}>
-                    <Favorite onToggle={action('test')} /> With Callback
-                </div>
-                <div style={style}>
-                    <Favorite /> Normal
+                    <Favorite checked /> Default, Checked
                 </div>
                 <div style={style2}>
-                    <Favorite className="dark-yellow" /> Dark, Yellow
+                    <Favorite className="dark-yellow" /> Dark / Yellow
                 </div>
                 <div style={style2}>
-                    <Favorite className="dark-white" /> Dark, White
+                    <Favorite className="dark-yellow" checked /> Dark / Yellow, Checked
+                </div>
+                <div style={style2}>
+                    <Favorite className="dark-white" /> Dark / White
+                </div>
+                <div style={style2}>
+                    <Favorite className="dark-white" checked /> Dark / White, Checked
                 </div>
             </div>
         );
