6d3c364b0a9d4d9f5fa1cebd753b19246b413cf7
Cliff Meyers <cliff.meyers@gmail.com>
2016-09-07 21:41:36 -0400

    [JENKINS-36906] add "small" button style (#93)
    * [JENKINS-36906] add "small" button style
    
    * [JENKINS-36906] layout for "button-bar" in global nav
    
    * [JENKINS-36906] beta version tick

--- src/js/stories/buttons.jsx
+++ src/js/stories/buttons.jsx
@@ -9,7 +9,7 @@ const container = {
     display: 'flex',
     justifyContent: 'center',
     width: '800px',
-    height: '200px',
+    height: '400px',
     padding: '10px',
 };
 
@@ -31,18 +31,27 @@ function buttonsLightBg() {
                     <button>Default Button</button>
                     <a>Default Anchor</a>
                     <button disabled="disabled">Default Button Disabled</button>
+                    <button className="small">Small Button</button>
+                    <a className="small">Small Anchor</a>
+                    <button className="small" disabled="disabled">Small Button Disabled</button>
                 </div>
                 <div style={buttons}>
                     <div>.btn, .btn-default, .btn-primary</div>
                     <button className="btn-primary">Primary Button</button>
                     <a className="btn-primary">Primary Anchor</a>
                     <button className="btn-primary" disabled="disabled">Primary Button Disabled</button>
+                    <button className="btn-primary small">Small Button</button>
+                    <a className="btn-primary small">Small Anchor</a>
+                    <button className="btn-primary small" disabled="disabled">Small Button Disabled</button>
                 </div>
                 <div style={buttons}>
                     <div>.btn-secondary</div>
                     <button className="btn-secondary">Secondary Button</button>
                     <a className="btn-secondary">Secondary Anchor</a>
                     <button className="btn-secondary" disabled="disabled">Secondary Button Disabled</button>
+                    <button className="btn-secondary small">Small Button</button>
+                    <a className="btn-secondary small">Small Anchor</a>
+                    <button className="btn-secondary small" disabled="disabled">Small Button Disabled</button>
                 </div>
             </div>
         </div>
