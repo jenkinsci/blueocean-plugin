f316cdc81b57702a26e5833f2b1237d7e3d0c7f4
Cliff Meyers <cmeyers@cloudbees.com>
2016-09-08 15:21:43 -0400

    [JENKINS-37519] add an "enum" of a few available capabilities

---
+++ blueocean-core-js/src/js/capability/Capability.js
@@ -0,0 +1,8 @@
+/**
+ * Created by cmeyers on 9/8/16.
+ */
+export const CAPABILITY = {
+    SIMPLE_PIPELINE: 'org.jenkinsci.plugins.workflow.job.WorkflowJob',
+    MULTIBRANCH_PIPELINE: 'io.jenkins.blueocean.rest.model.BlueMultiBranchPipeline',
+    MULTIBRANCH_BRANCH: 'io.jenkins.blueocean.rest.model.BlueBranch',
+};
