1df08478171e660d1088f4515d34d6a4b599483b
Cliff Meyers <cmeyers@cloudbees.com>
2016-09-16 14:07:37 -0400

    [JENKINS-37877] add a utility for doing security checks that will avoid magic strings scattered throughout all plugins

---
+++ blueocean-core-js/src/js/security.js
@@ -0,0 +1,28 @@
+/**
+ * Created by cmeyers on 9/16/16.
+ */
+
+/**
+ * Returns a key of permissions functions that each return boolean to indicate authorization.
+ * Usage:
+ *      permit(pipeline).create();
+ *
+ * @param subject
+ * @returns {{read: (function()), create: (function()), start: (function()), stop: (function())}}
+ */
+const permit = (subject) => {
+    const checkPermissions = (permissionName) => {
+        return subject && subject.permissions && !!subject.permissions[permissionName];
+    };
+
+    return {
+        read: () => checkPermissions('read'),
+        create: () => checkPermissions('create'),
+        start: () => checkPermissions('start'),
+        stop: () => checkPermissions('stop'),
+    };
+};
+
+export default {
+    permit,
+};
---
+++ blueocean-core-js/test/js/security-spec.js
@@ -0,0 +1,39 @@
+/**
+ * Created by cmeyers on 9/16/16.
+ */
+
+import React from 'react';
+import { assert } from 'chai';
+
+import utils from '../../src/js/utils';
+import Security from '../../src/js/security';
+
+const { permit } = Security;
+
+describe('Security', () => {
+    describe('permit', () => {
+        let pipeline;
+
+        beforeEach(() => {
+            pipeline = utils.clone(require('./data/pipeline-1.json'));
+        });
+
+
+        it('returns true when user has permission', () => {
+            assert.isTrue(permit(pipeline).read());
+            assert.isTrue(permit(pipeline).create());
+            assert.isTrue(permit(pipeline).start());
+            assert.isTrue(permit(pipeline).stop());
+        });
+
+        it('returns false when user does not have permission', () => {
+            const { permissions: perms } = pipeline;
+            perms.create = perms.read = perms.start = perms.stop = false;
+
+            assert.isFalse(permit(pipeline).read());
+            assert.isFalse(permit(pipeline).create());
+            assert.isFalse(permit(pipeline).start());
+            assert.isFalse(permit(pipeline).stop());
+        });
+    });
+});
