d104949b80784e8badd7b2ba52a44f353fd88896
Cliff Meyers <cmeyers@cloudbees.com>
2016-09-21 13:53:21 -0400

    [JENKINS-37425] fix a regression in this branch where clicking "Re-run" from Run Details would not automatically navigate the user to the Run Details screen for the new run; bake permissions logic into ReplayButton to hide itself if not a pipeline

--- blueocean-core-js/src/js/ToastUtils.js
+++ blueocean-core-js/src/js/ToastUtils.js
@@ -41,5 +41,7 @@ export default {
                 }
             },
         });
+
+        return runDetailsUrl;
     },
 };
--- blueocean-core-js/src/js/components/ReplayButton.jsx
+++ blueocean-core-js/src/js/components/ReplayButton.jsx
@@ -5,12 +5,18 @@
 import React, { Component, PropTypes } from 'react';
 import { Icon } from 'react-material-icons-blue';
 
-import { RunApi as runApi } from '../';
-import { ToastUtils } from '../';
+import { capable } from '../index';
+import { RunApi as runApi } from '../index';
+import { ToastUtils } from '../index';
 
 const stopProp = (event) => {
     event.stopPropagation();
 };
+
+const CAPABILITY_MULTIBRANCH_PIPELINE = 'io.jenkins.blueocean.rest.model.BlueMultiBranchPipeline';
+const CAPABILITY_MULTIBRANCH_BRANCH = 'io.jenkins.blueocean.rest.model.BlueBranch';
+const CAPABILITY_SIMPLE_PIPELINE = 'org.jenkinsci.plugins.workflow.job.WorkflowJob';
+const PIPELINE_CAPABILITIES = [CAPABILITY_SIMPLE_PIPELINE, CAPABILITY_MULTIBRANCH_PIPELINE, CAPABILITY_MULTIBRANCH_BRANCH];
 
 /**
  * ReplayButton allows a pipeline or branch to be re-run when in a failure state.
@@ -35,7 +41,14 @@ export class ReplayButton extends Component {
         });
 
         runApi.replayRun(this.props.latestRun)
-            .then((runInfo) => ToastUtils.createRunStartedToast(this.props.runnable, runInfo, this.props.onNavigation));
+            .then(runInfo => ToastUtils.createRunStartedToast(this.props.runnable, runInfo, this.props.onNavigation))
+            .then(runDetailsUrl => this._afterReplayStarted(runDetailsUrl));
+    }
+
+    _afterReplayStarted(runDetailsUrl) {
+        if (this.props.autoNavigate && this.props.onNavigation) {
+            this.props.onNavigation(runDetailsUrl);
+        }
     }
 
     render() {
@@ -45,17 +58,20 @@ export class ReplayButton extends Component {
 
         const status = this.props.latestRun ? this.props.latestRun.result : '';
         const failedStatus = status && (status.toLowerCase() === 'failure' || status.toLowerCase() === 'aborted');
+        const isPipeline = capable(this.props.runnable, PIPELINE_CAPABILITIES);
 
         const replayLabel = 'Re-run';
 
+        if (!isPipeline || !failedStatus) {
+            return null;
+        }
+
         return (
             <div className={`replay-button-component ${outerClass}`} onClick={(event => stopProp(event))}>
-                { failedStatus &&
                 <a className={`replay-button ${innerButtonClass}`} title={replayLabel} onClick={() => this._onReplayClick()}>
                     <Icon size={24} icon="replay" />
                     <span className="button-label">{replayLabel}</span>
                 </a>
-                }
             </div>
         );
     }
@@ -65,6 +81,6 @@ ReplayButton.propTypes = {
     className: PropTypes.string,
     runnable: PropTypes.object,
     latestRun: PropTypes.object,
-    autoNavigate: PropTypes.object,
+    autoNavigate: PropTypes.bool,
     onNavigation: PropTypes.func,
 };
