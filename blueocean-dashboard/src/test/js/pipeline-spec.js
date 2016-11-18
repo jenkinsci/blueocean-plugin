02fea7ab099fa441c83cf881ae9e3870b47f0953
Thorsten Scherler <scherler@gmail.com>
2016-11-18 12:23:09 +0100

    [JENKINS-39846_regressions_caption_defaultFallback_i18n] Implement default configuration if no windows is defined and use it in test (removing i18next as direct dep)

--- blueocean-dashboard/src/test/js/pipeline-spec.js
+++ blueocean-dashboard/src/test/js/pipeline-spec.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { assert } from 'chai';
 import { shallow } from 'enzyme';
-import i18next from 'i18next';
-
+import { I18n } from '@jenkins-cd/blueocean-core-js';
 import PipelineRowItem from '../../main/js/components/PipelineRowItem.jsx';
 import { PipelineRecord } from '../../main/js/components/records.jsx';
 
@@ -51,29 +50,8 @@ const pipelineSimple = {
     'weatherScore': 0,
 };
 /* eslint-enable quote-props */
-i18next.init({
-    lng: 'en',
-    // have a common namespace used around the full app
-    ns: ['translation'],
-    defaultNS: 'translation',
-    preload: ['en'],
-    keySeparator: false, // we do not have any nested keys in properties files
-    interpolation: {
-        prefix: '{',
-        suffix: '}',
-        escapeValue: false, // not needed for react!!
-    },
-    resources: {
-        en: {
-            translation: {
-                'home.pipelineslist.row.failing': '{0} failing',
-                'home.pipelineslist.row.passing': '{0} passing',
-            },
-    },
-  },
-});
 
-const t = i18next.getFixedT('en', 'translation');
+const t = I18n.getFixedT('en', 'translation');
 
 describe('PipelineRecord', () => {
     it('create without error', () => {
