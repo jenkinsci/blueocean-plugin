873bb30fa90a159f5a8fbc89a180f0674393a4cc
Michael Neale <michael.neale@gmail.com>
2016-11-21 09:54:07 +1100

    fix for cog and anon JENKINS-39889

--- blueocean-dashboard/src/main/js/components/RunDetails.jsx
+++ blueocean-dashboard/src/main/js/components/RunDetails.jsx
@@ -35,7 +35,7 @@ const { func, object, any, string } = PropTypes;
 
 const classicConfigLink = (pipeline) => {
     let link = null;
-    if (User.current().isAnonymous()) {
+    if (!User.current().isAnonymous()) {
         let url = buildClassicConfigUrl(pipeline);
         link = (
             <a href={url} target="_blank" style={{ height: '24px' }}>
