6ae1586f100580f583945ee99f44a0d93dbaaec6
James Dumay <jameswdumay@gmail.com>
2016-11-21 22:09:10 +1100

    add pagedQuery w/ lazy loading

--- blueocean-indexing/src/main/java/io/jenkins/blueocean/indexing/Index.java
+++ blueocean-indexing/src/main/java/io/jenkins/blueocean/indexing/Index.java
@@ -2,6 +2,7 @@ package io.jenkins.blueocean.indexing;
 
 
 import com.google.common.base.Function;
+import com.google.common.base.Supplier;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Iterables;
 import hudson.model.Item;
@@ -90,6 +91,101 @@ class Index<T> implements Closeable {
                     }
                 }
             }));
+        }
+    }
+
+    public ClosableIterator<Supplier<T>> pagedQuery(final Query query, @Nullable final Sort sort, final Function<Document, T> transformer) throws IOException {
+        final QueryIterator queryIterator = new QueryIterator(query, sort);
+        return new ClosableIterator<Supplier<T>>() {
+            @Override
+            public boolean hasNext() {
+                return queryIterator.hasNext();
+            }
+
+            @Override
+            public Supplier<T> next() {
+                return new Supplier<T>() {
+                    @Override
+                    public T get() {
+                        ScoreDoc next = queryIterator.next();
+                        Document document = null;
+                        try {
+                            document = queryIterator.searcher.doc(next.doc);
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                        return transformer.apply(document);
+                    }
+                };
+            }
+
+            @Override
+            public void close() throws IOException {
+                queryIterator.close();
+            }
+        };
+    }
+
+    public interface ClosableIterator<I> extends Iterator<I>, Closeable {}
+
+    class QueryIterator implements ClosableIterator<ScoreDoc> {
+
+        public static final int LIMIT = 500;
+        private final Query query;
+        private final Sort sort;
+        private final DirectoryReader reader;
+        public final IndexSearcher searcher;
+        private Iterator<ScoreDoc> scoreDocs;
+        private ScoreDoc lastScoreDoc;
+
+        public QueryIterator(Query query, Sort sort) throws IOException {
+            this.query = query;
+            this.sort = sort;
+            this.reader = DirectoryReader.open(index);
+            this.searcher = new IndexSearcher(reader);
+        }
+
+        @Override
+        public boolean hasNext() {
+            if (scoreDocs == null) { // First page
+                final TopDocs docs;
+                try {
+                    docs = searcher.search(query, LIMIT, sort);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                final List<ScoreDoc> scoreDocs = Arrays.asList(docs.scoreDocs);
+                if (scoreDocs.isEmpty()) {
+                    return false;
+                }
+                this.scoreDocs = scoreDocs.iterator();
+            } else if (scoreDocs.hasNext()) { // Iterate through current page
+                return true;
+            } else { // N+1 page
+                final TopDocs docs;
+                try {
+                    docs = searcher.searchAfter(lastScoreDoc, query, LIMIT, sort);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                final List<ScoreDoc> scoreDocs = Arrays.asList(docs.scoreDocs);
+                if (scoreDocs.isEmpty()) {
+                    return false;
+                }
+                this.scoreDocs = scoreDocs.iterator();
+            }
+            return false;
+        }
+
+        @Override
+        public ScoreDoc next() {
+            lastScoreDoc = scoreDocs.next();
+            return lastScoreDoc;
+        }
+
+        @Override
+        public void close() throws IOException {
+            this.reader.close();
         }
     }
 
