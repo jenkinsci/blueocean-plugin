e1aad53bbd5ed419ec32c0ff9ee0d4bb5df34ac7
tfennelly <tom.fennelly@gmail.com>
2016-11-22 10:27:34 +0000

    BlueI18n.getBundleParams

--- blueocean-i18n/src/main/java/io/jenkins/blueocean/i18n/BlueI18n.java
+++ blueocean-i18n/src/main/java/io/jenkins/blueocean/i18n/BlueI18n.java
@@ -26,18 +26,20 @@ package io.jenkins.blueocean.i18n;
 import hudson.Extension;
 import hudson.model.RootAction;
 import hudson.util.HttpResponses;
-import jenkins.util.ResourceBundleUtil;
+import net.sf.json.JSONObject;
 import org.kohsuke.accmod.Restricted;
 import org.kohsuke.accmod.restrictions.NoExternalUse;
 import org.kohsuke.stapler.HttpResponse;
 import org.kohsuke.stapler.StaplerRequest;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
+import java.util.regex.Pattern;
 
 /**
- * Internationalization REST (ish) API.
+ * Internationalization REST (ish) API for Blue Ocean.
  * @author <a href="mailto:tom.fennelly@gmail.com">tom.fennelly@gmail.com</a>
- * @since 2.0
  */
 @Extension
 @Restricted(NoExternalUse.class)
@@ -70,55 +72,100 @@ public class BlueI18n implements RootAction {
     /**
      * Get a localised resource bundle.
      * <p>
-     * URL: {@code i18n/resourceBundle}.
-     * <p>
-     * Parameters:
-     * <ul>
-     *     <li>{@code baseName}: The resource bundle base name.</li>
-     *     <li>{@code language}: {@link Locale} Language. (optional)</li>
-     *     <li>{@code country}: {@link Locale} Country. (optional)</li>
-     *     <li>{@code variant}: {@link Locale} Language variant. (optional)</li>
-     * </ul>
+     * URL: {@code blueocean-i18n/$PLUGIN_NAME/$PLUGIN_VERSION/$BUNDLE_NAME/$LOCALE} (where {@code $LOCALE} is optional).
      *
      * @param request The request.
      * @return The JSON response.
      */
-    public HttpResponse doResourceBundle(StaplerRequest request) {
-        String baseName = request.getParameter("baseName");
+    public HttpResponse doDynamic(StaplerRequest request) {
+        BundleParams bundleParams = getBundleParameters(request.getRestOfPath());
 
-        if (baseName == null) {
-            return HttpResponses.errorJSON("Mandatory parameter 'baseName' not specified.");
+        if (bundleParams == null) {
+            return HttpResponses.errorJSON("All mandatory bundle identification parameters not specified: '$PLUGIN_NAME/$PLUGIN_VERSION/$BUNDLE_NAME' (and optional $LOCALE).");
         }
 
-        String language = request.getParameter("language");
-        String country = request.getParameter("country");
-        String variant = request.getParameter("variant");
-        // https://www.w3.org/International/questions/qa-lang-priorities
-        // in case we have regions/countries in the language query parameter
-        if (language != null) {
-            String[] languageTokens = language.split("-|_");
-            language = languageTokens[0];
-            if (country == null && languageTokens.length > 1) {
-                country = languageTokens[1];
-                if (variant == null && languageTokens.length > 2) {
-                    variant = languageTokens[2];
+        try {
+            Locale locale = bundleParams.getLocale();
+
+            if (locale == null) {
+                locale = request.getLocale();
+            }
+
+            return HttpResponses.okJSON(getBundle(bundleParams, locale));
+        } catch (Exception e) {
+            return HttpResponses.errorJSON(e.getMessage());
+        }
+    }
+
+    private JSONObject getBundle(BundleParams bundleParams, Locale locale) {
+        return null;
+    }
+
+    static BundleParams getBundleParameters(String restOfPath) {
+        if (restOfPath == null || restOfPath.length() == 0) {
+            return null;
+        }
+
+        String[] pathTokens = restOfPath.split("/");
+        List<String> bundleParameters = new ArrayList<>();
+
+        for (String pathToken : pathTokens) {
+            if (pathToken.length() > 0) {
+                bundleParameters.add(pathToken);
+            }
+        }
+
+        if (bundleParameters.size() != 3 && bundleParameters.size() != 4) {
+            return null;
+        }
+
+        BundleParams bundleParams = new BundleParams();
+        bundleParams.pluginName = bundleParameters.get(0);
+        bundleParams.pluginVersion = bundleParameters.get(1);
+        bundleParams.bundleName = bundleParameters.get(2);
+
+        if (bundleParameters.size() == 4) {
+            // https://www.w3.org/International/questions/qa-lang-priorities
+            // in case we have regions/countries in the language query parameter
+            String locale = bundleParameters.get(3);
+            String[] localeTokens = locale.split("-|_");
+            bundleParams.language = localeTokens[0];
+            if (localeTokens.length > 1) {
+                bundleParams.country = localeTokens[1];
+                if (localeTokens.length > 2) {
+                    bundleParams.variant = localeTokens[2];
                 }
             }
         }
-        try {
-            Locale locale = request.getLocale();
 
+        return bundleParams;
+    }
+
+    static class BundleParams {
+        String pluginName;
+        String pluginVersion;
+        String bundleName;
+        String language;
+        String country;
+        String variant;
+
+        Locale getLocale() {
             if (language != null && country != null && variant != null) {
-                locale = new Locale(language, country, variant);
+                return new Locale(language, country, variant);
             } else if (language != null && country != null) {
-                locale = new Locale(language, country);
+                return new Locale(language, country);
             } else if (language != null) {
-                locale = new Locale(language);
+                return new Locale(language);
+            } else {
+                return null;
             }
+        }
 
-            return HttpResponses.okJSON(ResourceBundleUtil.getBundle(baseName, locale));
-        } catch (Exception e) {
-            return HttpResponses.errorJSON(e.getMessage());
+        // We declare the plugin to be a release version iff
+        // the version string contains only numerics.
+        private static Pattern RELEASE_VERSION_PATTERN = Pattern.compile("[\\d/.]{3,}");
+        boolean isReleaseVersion() {
+            return RELEASE_VERSION_PATTERN.matcher(pluginVersion).matches();
         }
     }
 }
