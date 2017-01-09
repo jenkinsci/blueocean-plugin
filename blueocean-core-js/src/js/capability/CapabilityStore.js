68d5f0bd92a9b0f0ba08e52028dea07031468840
Tom Fennelly <tom.fennelly@gmail.com>
2017-01-09 12:58:59 +0000

    [FIX JENKINS-40080] Cache class info in browser storage (#683)
    * Add basic browser storage API wrapper to core-js
    
    * Cache classes info in blowser storage
    
    * @jenkins-cd/blueocean-core-js 0.0.41-tfbeta1
    
    * Catch lang detector failures and fallback to the fallback lang.
    
    Need this now because we are using localStorage, as does the i18next package. We are initializing window.localStorage if it's not there already (as is the case in tests). This is causing i18next to baulk.
    
    * Fix CapabilityTestUtils.js
    
    * core-js dependants to 0.0.42 for publish
    
    * core-js dependants to 0.0.42 on dependants

--- blueocean-core-js/src/js/capability/CapabilityStore.js
+++ blueocean-core-js/src/js/capability/CapabilityStore.js
@@ -2,6 +2,13 @@
  * Created by cmeyers on 8/31/16.
  */
 import es6Promise from 'es6-promise'; es6Promise.polyfill();
+import { installInfo } from '../storage';
+
+// Create a dedicated storage namespace that we use to store classes
+// info in the browser, eliminating client REST call overhead for classes
+// info. This storage namespace will be auto-cleared if the jesnkins version
+// changes, or if the active plugins change.
+const classesInfoNS = installInfo.subspace('classesInfo');
 
 /**
  * Retrieves capability metadata for class names.
@@ -10,7 +17,7 @@ import es6Promise from 'es6-promise'; es6Promise.polyfill();
 export class CapabilityStore {
 
     constructor(capabilityApi) {
-        this._store = {};
+        this._localStore = {};
         this._capabilityApi = capabilityApi;
     }
 
@@ -27,8 +34,9 @@ export class CapabilityStore {
 
         // determine which class names are already in the cache and which aren't
         for (const className of classNames) {
-            if (this._store[className]) {
-                result[className] = this._store[className];
+            const classInfo = this._getStoredClassInfo(className);
+            if (classInfo) {
+                result[className] = classInfo;
             } else {
                 classesToFetch.push(className);
             }
@@ -68,9 +76,25 @@ export class CapabilityStore {
 
         Object.keys(map).forEach(className => {
             const capabilities = map[className];
-            this._store[className] = storedCapabilities[className] = capabilities.classes.slice();
+            this._localStore[className] = storedCapabilities[className] = capabilities.classes.slice();
+            // Also store in the browser so we don't have to look
+            // up this info again (unless the storage namespace is
+            // cleared due to jenkins or plugin changes).
+            classesInfoNS.set(className, this._localStore[className]);
         });
 
         return storedCapabilities;
+    }
+
+    _getStoredClassInfo(className) {
+        if (!this._localStore[className]) {
+            // If we don't have a copy of the class info in the localStore,
+            // check the browser storage and copy it into the localStore.
+            // We still want to use the localStore because it holds deserialized
+            // copies of the class info, which means that a localStore lookup
+            // would be lower overhead and probably faster.
+            this._localStore[className] = classesInfoNS.get(className);
+        }
+        return this._localStore[className];
     }
 }
