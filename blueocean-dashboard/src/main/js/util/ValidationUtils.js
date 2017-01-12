8b4e24cede13e853825c71ce0360b8c87dcc1b13
Cliff Meyers <cmeyers@cloudbees.com>
2017-01-12 12:09:26 -0500

    [JENKINS-38597] relax validation constraints on the repository URL

--- blueocean-dashboard/src/main/js/util/ValidationUtils.js
+++ blueocean-dashboard/src/main/js/util/ValidationUtils.js
@@ -1,5 +1,8 @@
-import validateUrl from './regex-weburl';
+// import validateUrl from './regex-weburl';
 
 export default {
-    validateUrl,
+    // TODO: maybe we want a better way to validate this URL but there are too many permutations to obsess about it now.
+    validateUrl(url) {
+        return !!url && !!url.trim();
+    },
 };
