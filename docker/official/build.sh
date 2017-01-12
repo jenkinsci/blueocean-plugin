6e5295d67102d6546d706136cc05647831a42cf9
R. Tyler Croy <tyler@monkeypox.org>
2017-01-11 21:08:28 -0800

    Simplify the Dockerfile and build.sh to pull the latest released plugin instead. (#703)
    No need for the maven chicanery

--- docker/official/build.sh
+++ docker/official/build.sh
@@ -1,7 +1,4 @@
-#!/bin/bash
-
-set -e
-set -x
+#!/bin/bash -xe
 
 HERE=$(dirname $0)
 cd $HERE
@@ -15,22 +12,12 @@ if docker pull jenkinsci/blueocean:$BLUEOCEAN_VERSION; then
   exit 0
 fi
 
-# ensure that we have the latest Jenkins LTS image available
-docker pull jenkins:latest
-
-# Fetch BlueOcean plugins using Maven
-#
-# Note: we don't use "install-plugins.sh" script from the official Jenkins Docker image because we want to use Maven to resolve
-# plugin dependency. For BO we will all blueocean-* plugins to be the same version. The aforementioned script downloads latest version
-# of dependent plugins, so we would need to list all BO plugins and do it in the right order (if there's any)
-docker run -i --rm -v "$PWD":/usr/src/boplugins -w /usr/src/boplugins maven:3.3.9 -u "$(id -u)" mvn "-Dblueocean.version=$BLUEOCEAN_VERSION" package
-
 # Build the image
-docker build --no-cache --pull \
+docker build --rm --no-cache --pull \
              --tag "jenkinsci/blueocean:$BLUEOCEAN_VERSION" .
 
 # Consider this build is the latest
-docker tag -f "jenkinsci/blueocean:$BLUEOCEAN_VERSION" jenkinsci/blueocean:latest
+docker tag "jenkinsci/blueocean:$BLUEOCEAN_VERSION" jenkinsci/blueocean:latest
 
 docker push "jenkinsci/blueocean:$BLUEOCEAN_VERSION"
 docker push jenkinsci/blueocean:latest
