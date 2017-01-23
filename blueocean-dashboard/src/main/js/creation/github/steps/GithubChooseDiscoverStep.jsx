c3080070b1f9cada7acc884e381b9e3419c5cca5
Cliff Meyers <cmeyers@cloudbees.com>
2017-01-23 16:19:51 -0500

    [JENKINS-38598] skinning of org list and discover steps

--- blueocean-dashboard/src/main/js/creation/github/steps/GithubChooseDiscoverStep.jsx
+++ blueocean-dashboard/src/main/js/creation/github/steps/GithubChooseDiscoverStep.jsx
@@ -5,18 +5,44 @@ import { observer } from 'mobx-react';
 @observer
 export default class GithubChooseDiscoverStep extends React.Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            discover: null,
+        };
+    }
+
     selectDiscover(discover) {
+        this.setState({
+            discover,
+        });
         this.props.flowManager.selectDiscover(discover);
     }
 
     render() {
+        const title = 'Do you want to create a Pipeline for one repository or automatically discover?';
+        const option1Class = this.state.discover === false ? 'u-selected' : '';
+        const option2Class = this.state.discover === true ? 'u-selected' : '';
+
         return (
-            <FlowStep {...this.props} title="Do you want to create a Pipeline for one repository or automatically discover?">
-                <div>
-                    <button onClick={() => this.selectDiscover(false)}>Just One</button>
-                </div>
-                <div>
-                    <button onClick={() => this.selectDiscover(true)}>Auto Discover!!</button>
+            <FlowStep {...this.props} className="github-choose-discover-step" title={title}>
+                <div className="toggle layout-large">
+                    <button className={`monochrome ${option1Class}`} onClick={() => this.selectDiscover(false)}>
+                        <h1 className="title">Just one repository</h1>
+                        <p className="text">
+                            Recommended if you haven't created a Pipeline before or do
+                            have any repositories containing a <em>Jenkinsfile</em>
+                        </p>
+                    </button>
+
+                    <button className={`monochrome ${option2Class}`} onClick={() => this.selectDiscover(true)}>
+                        <h1 className="title">Automatically discover</h1>
+                        <p className="text">
+                            Actively discovers new <em>Jenkinsfiles</em> in this organization's
+                            repositories and creates Pipelines automatically.
+                        </p>
+                    </button>
                 </div>
             </FlowStep>
         );
