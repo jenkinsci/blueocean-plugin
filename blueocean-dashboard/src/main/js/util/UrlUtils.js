a783cdd687f397418b4391c75819f803ea1f82d2
Vivek Pandey <vivek.pandey@gmail.com>
2017-01-26 16:50:07 -0800

    Revert "JENKINS-41462# branch name encoding fix (#751)" (#755)
    This reverts commit 196d5fc2cd4f2a816553ba2014732f3fcae64ead.

--- blueocean-dashboard/src/main/js/util/UrlUtils.js
+++ blueocean-dashboard/src/main/js/util/UrlUtils.js
@@ -46,16 +46,8 @@ export const buildRunDetailsUrl = (organization, pipeline, branch, runId, tabNam
 /*
  * helper to clean the path replace(/%2F/g, '%252F')
  * @param input
- *
- * XXX: This was used where encodeURIComponent() should have been used. If it has not other uses,
- *      it should perhaps be removed.
  */
 export const uriString = (input) => encodeURIComponent(input).replace(/%2F/g, '%252F');
-
-/**
- * Double encode name, feature/test#1 is encoded as feature%252Ftest%25231
- */
-export const doubleUriEncode = (input) => escape(encodeURIComponent(input));
 
 // general fetchAllTrigger
 export const fetchAllSuffix = '?start=0';
@@ -120,7 +112,7 @@ export function calculateNodeBaseUrl(config) {
         `${_appURLBase}/rest/organizations/jenkins/` +
         `pipelines/${name}`;
     if (isMultiBranch) {
-        return `${baseUrl}/branches/${doubleUriEncode(branch)}/runs/${runId}/nodes/`;
+        return `${baseUrl}/branches/${uriString(branch)}/runs/${runId}/nodes/`;
     }
     return `${baseUrl}/runs/${runId}/nodes/`;
 }
@@ -137,7 +129,7 @@ export function calculateStepsBaseUrl(config) {
         `${_appURLBase}/rest/organizations/jenkins/` +
         `pipelines/${name}`;
     if (isMultiBranch) {
-        baseUrl = `${baseUrl}/branches/${doubleUriEncode(branch)}`;
+        baseUrl = `${baseUrl}/branches/${uriString(branch)}`;
     }
     if (node && node !== null) {
         return `${baseUrl}/runs/${runId}/nodes/${node}/steps/`;
@@ -156,7 +148,7 @@ export function calculateRunLogURLObject(config) {
     let url;
     let fileName;
     if (isMultiBranch) {
-        url = `${baseUrl}/branches/${doubleUriEncode(branch)}/runs/${runId}/log/`;
+        url = `${baseUrl}/branches/${uriString(branch)}/runs/${runId}/log/`;
         fileName = `${branch}-${runId}.txt`;
     } else {
         url = `${baseUrl}/runs/${runId}/log/`;
