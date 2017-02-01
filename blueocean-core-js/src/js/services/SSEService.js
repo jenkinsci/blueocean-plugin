39d7972962aa0882aabb713a5c92b349d39e89a7
Cliff Meyers <cliff.meyers@gmail.com>
2017-02-01 10:50:07 -0500

    Task/jenkins 38598 sse service remove handler (#754)
    * [JENKINS-38598] add randomId() generator to core-js; allow for handlers to be removed from SSEService
    
    * [JENKINS-38598] use beta core-js
    
    * tick version after publishing blueocean-core-js@0.0.57
    
    * use prod blueocean-core-js

--- blueocean-core-js/src/js/services/SSEService.js
+++ blueocean-core-js/src/js/services/SSEService.js
@@ -1,3 +1,5 @@
+import Utils from '../utils';
+
 export class SSEService {
     constructor(connection) {
         this.connection = connection;
@@ -12,10 +14,32 @@ export class SSEService {
         }
     }
 
+    /**
+     * Add a handler for job events.
+     *
+     * @param {Function} handlerFn
+     * @returns {string} id used by removeHandler
+     */
     registerHandler(handlerFn) {
-        this._handlers.push(handlerFn);
+        const id = Utils.randomId('sse');
+
+        this._handlers.push({
+            id,
+            handlerFn,
+        });
+
+        return id;
     }
+
+    /**
+     * Remove a previously-registered handler.
+     * @param id
+     */
+    removeHandler(id) {
+        this._handlers = this._handlers.filter(handler => handler.id !== id);
+    }
+
     _handleJobEvent(event) {
-        this._handlers.forEach(handler => handler(event));
+        this._handlers.forEach(handler => handler.handlerFn(event));
     }
 }
