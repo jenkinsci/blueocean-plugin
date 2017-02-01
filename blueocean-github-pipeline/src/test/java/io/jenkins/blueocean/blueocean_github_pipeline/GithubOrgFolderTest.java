cdd78782258bb936c235e0b2266bda818e707880
Vivek Pandey <Vivek Pandey vivek.pandey@gmail.com>
2017-01-31 17:32:28 -0800

    Added test

--- blueocean-github-pipeline/src/test/java/io/jenkins/blueocean/blueocean_github_pipeline/GithubOrgFolderTest.java
+++ blueocean-github-pipeline/src/test/java/io/jenkins/blueocean/blueocean_github_pipeline/GithubOrgFolderTest.java
@@ -1,20 +1,35 @@
 package io.jenkins.blueocean.blueocean_github_pipeline;
 
+import com.cloudbees.hudson.plugins.folder.AbstractFolderProperty;
+import com.cloudbees.plugins.credentials.CredentialsProvider;
+import com.cloudbees.plugins.credentials.CredentialsScope;
+import com.cloudbees.plugins.credentials.CredentialsStore;
+import com.cloudbees.plugins.credentials.common.StandardCredentials;
+import com.cloudbees.plugins.credentials.common.StandardUsernamePasswordCredentials;
+import com.cloudbees.plugins.credentials.domains.Domain;
+import com.cloudbees.plugins.credentials.domains.DomainSpecification;
+import com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import hudson.model.User;
 import hudson.tasks.Mailer;
 import io.jenkins.blueocean.rest.impl.pipeline.PipelineBaseTest;
+import io.jenkins.blueocean.rest.impl.pipeline.credential.BlueOceanCredentialsProvider;
+import jenkins.branch.OrganizationFolder;
 import jenkins.model.Jenkins;
 import org.acegisecurity.adapters.PrincipalAcegiUserToken;
 import org.acegisecurity.context.SecurityContextHolder;
 import org.acegisecurity.userdetails.UserDetails;
+import org.jenkinsci.plugins.github_branch_source.Connector;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.Map;
+
+import static org.junit.Assert.*;
 
 /**
  * @author Vivek Pandey
@@ -67,6 +82,50 @@ public class GithubOrgFolderTest extends PipelineBaseTest {
                 ))
                 .build(Map.class);
     }
+
+    @Test
+    public void shouldFindUserStoreCredential() throws IOException {
+        //add username password credential to user's credential store in user domain and in USER scope
+        User user = login();
+        CredentialsStore store=null;
+        for(CredentialsStore s: CredentialsProvider.lookupStores(user)){
+            if(s.hasPermission(CredentialsProvider.CREATE) && s.hasPermission(CredentialsProvider.UPDATE)){
+                store = s;
+                break;
+            }
+        }
+        store.addDomain(new Domain("github-domain",
+                "Github Domain to store personal access token",
+                Collections.<DomainSpecification>emptyList()
+        ));
+        Domain domain = store.getDomainByName("github-domain");
+        StandardUsernamePasswordCredentials credential = new UsernamePasswordCredentialsImpl(CredentialsScope.USER,
+                "github", "Github Access Token", user.getId(), "12345");
+        store.addCredentials(domain, credential);
+
+
+        //create org folder and attach user and credential id to it
+        OrganizationFolder organizationFolder = j.createProject(OrganizationFolder.class, "demo");
+        AbstractFolderProperty prop = new BlueOceanCredentialsProvider.FolderPropertyImpl(user.getId(), credential.getId());
+        organizationFolder.addProperty(prop);
+
+        // lookup for created credential id in system store, it should resolve to previously created user store credential
+        StandardCredentials c = Connector.lookupScanCredentials(organizationFolder, null, credential.getId());
+        assertNotNull(c);
+        assertTrue(c instanceof StandardUsernamePasswordCredentials);
+        StandardUsernamePasswordCredentials usernamePasswordCredentials = (StandardUsernamePasswordCredentials) c;
+        assertEquals(credential.getId(), usernamePasswordCredentials.getId());
+        assertEquals(credential.getPassword().getPlainText(),usernamePasswordCredentials.getPassword().getPlainText());
+        assertEquals(credential.getUsername(),usernamePasswordCredentials.getUsername());
+
+        //now remove this property
+        organizationFolder.getProperties().remove(prop);
+
+        //it must not be found
+        c = Connector.lookupScanCredentials(organizationFolder, null, credential.getId());
+        assertNull(c);
+    }
+
 
     private User login() throws IOException {
         j.jenkins.setSecurityRealm(j.createDummySecurityRealm());
