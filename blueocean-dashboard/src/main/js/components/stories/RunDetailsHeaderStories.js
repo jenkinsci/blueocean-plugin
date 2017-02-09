d1d841551e6293cc16ffe528bab42c0106f6c5e1
Josh McDonald <josh@joshmcdonald.info>
2017-02-09 11:59:07 +1000

    bug/JENKINS-41542-run-details-stuffs * Fix alignment issues

--- blueocean-dashboard/src/main/js/components/stories/RunDetailsHeaderStories.js
+++ blueocean-dashboard/src/main/js/components/stories/RunDetailsHeaderStories.js
@@ -16,11 +16,9 @@ import {RunRecord} from '../records';
 import {testData} from './data/changesData';
 
 const runJSON = JSON.stringify(testData.run);
-const temp = JSON.parse(runJSON);
-// temp.changeSet = new ChangeSetRecord(temp.changeSet);
-const run = new RunRecord(temp);
 
 const strings = {
+    "common.date.duration.display.format": "M[ month] d[ days] h[ hours] m[ minutes] s[ seconds]",
     "common.date.duration.format": "m[ minutes] s[ seconds]",
     "common.date.duration.hint.format": "M [month], d [days], h[h], m[m], s[s]",
     "common.date.readable.long": "MMM DD YYYY h:mma Z",
@@ -53,9 +51,70 @@ RunDetailsHeader.timeManager = {
 
 storiesOf('Run Details Header', module)
     .add('Basic', basic)
+    .add('No Commit', noCommit)
+    .add('Long Branch', longBranch)
 ;
 
 function basic() {
+
+    const temp = JSON.parse(runJSON);
+    const run = new RunRecord(temp);
+
+    const topNavLinks = [
+        <a href="#" className="selected">Pipeline</a>,
+        <a href="#">Changes</a>,
+        <a href="#">Tests</a>,
+        <a href="#">Artifacts</a>,
+    ];
+
+    return (
+        <WithContext context={ctx}>
+            <RunDetailsHeader
+                locale="en"
+                t={t}
+                pipeline={testData.pipeline}
+                data={run}
+                onOrganizationClick={ action('button-click')}
+                onNameClick={ action('button-click')}
+                onAuthorsClick={ action('button-click')}
+                topNavLinks={topNavLinks}/>
+        </WithContext>
+    );
+}
+
+function noCommit() {
+
+    const temp = JSON.parse(runJSON);
+    temp.commitId = null;
+    const run = new RunRecord(temp);
+
+    const topNavLinks = [
+        <a href="#" className="selected">Pipeline</a>,
+        <a href="#">Changes</a>,
+        <a href="#">Tests</a>,
+        <a href="#">Artifacts</a>,
+    ];
+
+    return (
+        <WithContext context={ctx}>
+            <RunDetailsHeader
+                locale="en"
+                t={t}
+                pipeline={testData.pipeline}
+                data={run}
+                onOrganizationClick={ action('button-click')}
+                onNameClick={ action('button-click')}
+                onAuthorsClick={ action('button-click')}
+                topNavLinks={topNavLinks}/>
+        </WithContext>
+    );
+}
+
+function longBranch() {
+
+    const temp = JSON.parse(runJSON);
+    temp.pipeline = "bug/JENKINS-007-license-to-kill-this-long-ass-branch-name";
+    const run = new RunRecord(temp);
 
     const topNavLinks = [
         <a href="#" className="selected">Pipeline</a>,
