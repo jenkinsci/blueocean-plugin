2f0d8483d1f80fa59c7d9d8ce79e80e0756e181a
Vivek Pandey <vivek.pandey@gmail.com>
2017-02-10 09:00:21 -0800

    JENKINS-41348# BlueOcean specific CredentialsProvider (#768)
    * JENKINS-41348# BlueOcean specific CredentialsProvider
    
    Provides User store/scoped credentials by using appropriate properties
    on OrganizationFolder.
    
    * BlueOceanCredentialProvider is not optional extension
    
    * i18n of blueocean credential provider message
    
    * Added test
    
    * credentials API to create credential in different stores (user and system)
    
    Also refactored helper credential methods at one place.
    
    * Git pipeline add user id and credential id property to MBP job
    
    * test fixes
    
    * Use StringUtils for null/balnk check
    
    * Store domain in folder property and other changes
    
    All changes are as per discussion on PR
    
    * Impersonation related fix
    
    * Added DomainSpecification/DomainRequirement checks
    
    * Removed redundant boolean
    
    * Checkpointing work in progress
    
    * Create domain specification for blueocean credential provider proxy
    
    * Fix related to credentials lookup in validate accesstoken apis
    
    - Also enabled github api tests that gets run if GITHUB_ACCESS_TOKEN jvm property is provided
    
    * Fixes as per PR comments
    
    - github enterprise domain name has hostname suffix
    - Fixed correct domain requirement
    
    * github api tests are enabled, to run them use this JVM property -DGITHUB_ACCESS_TOKEN=$YOUR_GITHUB_AACECSS_TOKEN
    
    * Fix to get BO folder property for child folders of org folder
    
    * Added test for github org folder

--- blueocean-github-pipeline/src/main/java/io/jenkins/blueocean/blueocean_github_pipeline/GithubEnterpriseScm.java
+++ blueocean-github-pipeline/src/main/java/io/jenkins/blueocean/blueocean_github_pipeline/GithubEnterpriseScm.java
@@ -1,18 +1,24 @@
 package io.jenkins.blueocean.blueocean_github_pipeline;
 
 import hudson.Extension;
+import io.jenkins.blueocean.commons.ErrorMessage;
+import io.jenkins.blueocean.commons.ServiceException;
 import io.jenkins.blueocean.rest.Reachable;
 import io.jenkins.blueocean.rest.impl.pipeline.scm.Scm;
 import io.jenkins.blueocean.rest.impl.pipeline.scm.ScmFactory;
 
 import javax.annotation.Nonnull;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 /**
  * @author Vivek Pandey
  */
 public class GithubEnterpriseScm extends GithubScm {
-    private static final String DEFAULT_ENTERPRISE_API_SUFFIX = "/api/v3";
-    private static final String ID = "github-enterprise";
+    static final String DEFAULT_ENTERPRISE_API_SUFFIX = "/api/v3";
+    static final String ID = "github-enterprise";
+    static final String DOMAIN_NAME="blueocean-github-enterprise-domain";
+
 
     public GithubEnterpriseScm(Reachable parent) {
         super(parent);
@@ -28,13 +34,24 @@ public class GithubEnterpriseScm extends GithubScm {
         return super.getUri()+DEFAULT_ENTERPRISE_API_SUFFIX;
     }
 
+    @Override
+    public String getCredentialDomainName() {
+        java.net.URI uri;
+        try {
+            uri = new URI(getUri());
+        } catch (URISyntaxException e) {
+            throw new ServiceException.UnexpectedErrorException(new ErrorMessage(400, "Invalid Github Enterprise URI: "+getUri()));
+        }
+        return DOMAIN_NAME + "-" + uri.getHost();
+    }
+
     @Extension
     public static class GithubScmFactory extends ScmFactory {
 
         @Override
         public Scm getScm(String id, Reachable parent) {
             if(id.equals(ID)){
-                return new GithubScm(parent);
+                return new GithubEnterpriseScm(parent);
             }
             return null;
         }
@@ -42,7 +59,7 @@ public class GithubEnterpriseScm extends GithubScm {
         @Nonnull
         @Override
         public Scm getScm(Reachable parent) {
-            return new GithubScm(parent);
+            return new GithubEnterpriseScm(parent);
         }
     }
 
