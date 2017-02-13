cebf80b846767a192725aab16b343e9a2a0c75d5
Ivan Meredith <ivan@ivan.net.nz>
2017-02-13 16:14:47 +1300

    [JENKINS-41242] Adjust footer to have more useful info

--- blueocean-web/src/main/js/DevelopmentFooter.jsx
+++ blueocean-web/src/main/js/DevelopmentFooter.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import moment from 'moment';
 // The following import target will be generate on build time, do not edit
 import revisionInfo from '../../../target/classes/io/jenkins/blueocean/revisionInfo';
+import { AppConfig } from '@jenkins-cd/blueocean-core-js';
 
 export class DevelopmentFooter extends Component {
     render() {
@@ -14,9 +15,12 @@ export class DevelopmentFooter extends Component {
         }
         return (
           <div className="development-footer">
-              <span>Built at {moment(revisionInfo.timestamp).format('Do MMMM YYYY hh:mm A')}&nbsp;</span>
+              <span> {AppConfig.getConfig().version}&nbsp;</span>
+              <span> &#183; Core {AppConfig.getJenkinsConfig().version}&nbsp;</span>
+              <span> &#183; {revisionInfo.sha.substring(0,7)}&nbsp; </span>
               <span> &#183; {revisionInfo.branch}&nbsp;</span>
-              <span> &#183; {revisionInfo.sha.substring(0,7)} </span>
+              <span> &#183; {moment(revisionInfo.timestamp).format('Do MMMM YYYY hh:mm A')}</span>
+              
           </div>
         );
     }
