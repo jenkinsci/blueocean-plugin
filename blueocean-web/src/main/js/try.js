7dca2440f4a9e3b03de9586777a74903831293b9
Tom Fennelly <tom.fennelly@gmail.com>
2017-02-14 16:40:59 +0000

    [FIX JENKINS-41638] "Open Blue Ocean" construction by replacing the classic object path with the blue ocean object path (#819)
    * Construct "Open Blue Ocean" URL by replacing the object url with the Blue Ocean URL
    
    * Fix "Open Blue Ocean" URL tests
    
    * Fix "Open Blue Ocean" URL for classic pages where there's no BO page e.g. folders
    
    * Fix "Open Blue Ocean" URL for Run instances (they're not Items)
    
    * Fix StatePreloaderTest
    
    * change the try.js path substitution
    
    * Tweak try.js

--- blueocean-web/src/main/js/try.js
+++ blueocean-web/src/main/js/try.js
@@ -1,25 +1,25 @@
 var $ = require('jquery-detached').getJQuery();
 
-function getAppUrl() {
-    const rootUrl = $('head').attr('data-rooturl');
-    if (!rootUrl) {
-        return '';
-    } else {
-        return rootUrl;
-    }
-}
-
 $(document).ready(() => {
-    var tryBlueOcean = $('<a id="open-blueocean-in-context" class="try-blueocean header-callout">Open Blue Ocean</a>');
     var contextUrlDiv = $('#blueocean-context-url');
     var tryBlueOceanUrl;
 
     if (contextUrlDiv.length === 1) {
-        tryBlueOceanUrl = contextUrlDiv.attr('data-context-url');
-    } else {
-        tryBlueOceanUrl = `${getAppUrl()}/blue`;
+        var blueUrl = contextUrlDiv.attr('data-context-url');
+        var classicUrl = contextUrlDiv.attr('data-classic-url');
+        if (classicUrl) {
+            var indexOfContext = window.location.href.indexOf(classicUrl);
+            if (indexOfContext !== -1) {
+                tryBlueOceanUrl = window.location.href.substring(0, indexOfContext) + blueUrl;
+            }
+        }
     }
-    tryBlueOcean.attr('href', tryBlueOceanUrl);
 
+    if (!tryBlueOceanUrl) {
+        tryBlueOceanUrl = `./blue`;
+    }
+
+    var tryBlueOcean = $('<a id="open-blueocean-in-context" class="try-blueocean header-callout">Open Blue Ocean</a>');
+    tryBlueOcean.attr('href', tryBlueOceanUrl);
     $('#page-head #header').append(tryBlueOcean);
 });
