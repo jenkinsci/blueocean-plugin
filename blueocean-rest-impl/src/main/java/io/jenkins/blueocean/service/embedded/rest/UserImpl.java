dd944378fe95363ffc27ceb3eba46e935d5155e5
Vivek Pandey <vivek.pandey@gmail.com>
2017-02-13 17:06:42 -0800

    JENKINS-41433# User permission api (#808)

--- blueocean-rest-impl/src/main/java/io/jenkins/blueocean/service/embedded/rest/UserImpl.java
+++ blueocean-rest-impl/src/main/java/io/jenkins/blueocean/service/embedded/rest/UserImpl.java
@@ -1,7 +1,9 @@
 package io.jenkins.blueocean.service.embedded.rest;
 
+import com.google.common.collect.ImmutableMap;
+import hudson.model.Item;
 import hudson.model.User;
-import hudson.plugins.favorite.user.FavoriteUserProperty;
+import hudson.security.ACL;
 import hudson.tasks.Mailer;
 import hudson.tasks.UserAvatarResolver;
 import io.jenkins.blueocean.commons.ServiceException;
@@ -9,11 +11,16 @@ import io.jenkins.blueocean.rest.ApiHead;
 import io.jenkins.blueocean.rest.Reachable;
 import io.jenkins.blueocean.rest.hal.Link;
 import io.jenkins.blueocean.rest.model.BlueFavoriteContainer;
+import io.jenkins.blueocean.rest.model.BluePipeline;
 import io.jenkins.blueocean.rest.model.BlueUser;
+import io.jenkins.blueocean.rest.model.BlueUserPermission;
 import jenkins.model.Jenkins;
-import org.kohsuke.stapler.Stapler;
+import org.acegisecurity.Authentication;
+import org.acegisecurity.userdetails.UsernameNotFoundException;
 
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * {@link BlueUser} implementation backed by in-memory {@link User}
@@ -77,8 +84,62 @@ public class UserImpl extends BlueUser {
     }
 
     @Override
+    public BlueUserPermission getPermission() {
+        Authentication authentication = Jenkins.getAuthentication();
+        String name = authentication.getName();
+        final boolean[] ad = new boolean[1];
+        final Map<String,Boolean> pipelinePermission = new HashMap<>();
+
+        if(name.equals(user.getId())){
+            ad[0] = isAdmin();
+            pipelinePermission.putAll(getPipelinePermissions());
+        }else if(!name.equals(user.getId()) && Jenkins.getInstance().hasPermission(Jenkins.ADMINISTER)) {
+            // authenticated user is different from requested user and authenticated user is admin,
+            // we will let it impersonate requested user
+            try {
+                ACL.impersonate(user.impersonate(), new Runnable() {
+                    @Override
+                    public void run() {
+                        ad[0] = isAdmin();
+                        pipelinePermission.putAll(getPipelinePermissions());
+                    }
+                });
+            }catch (UsernameNotFoundException e){
+                return null;
+            }
+        }else{ //different than the logged in user and logged in user is not admin
+            return null;
+        }
+
+        return new BlueUserPermission() {
+            @Override
+            public boolean isAdministration() {
+                return ad[0];
+            }
+
+            @Override
+            public Map<String, Boolean> getPipelinePermission() {
+                return pipelinePermission;
+            }
+        };
+    }
+
+    @Override
     public Link getLink() {
         return (parent != null)?parent.getLink().rel(getId()): ApiHead.INSTANCE().getLink().rel("users/"+getId());
+    }
+
+    private boolean isAdmin(){
+        return Jenkins.getInstance().hasPermission(Jenkins.ADMINISTER);
+    }
+
+    private Map<String, Boolean> getPipelinePermissions(){
+        return ImmutableMap.of(
+                BluePipeline.CREATE_PERMISSION, Jenkins.getInstance().hasPermission(Item.CREATE),
+                BluePipeline.READ_PERMISSION, Jenkins.getInstance().hasPermission(Item.READ),
+                BluePipeline.START_PERMISSION, Jenkins.getInstance().hasPermission(Item.BUILD),
+                BluePipeline.STOP_PERMISSION, Jenkins.getInstance().hasPermission(Item.CANCEL)
+        );
     }
 
 }
