f070ba6b1f6581781c65de9399288eed8ab7be8b
James William Dumay <jdumay@cloudbees.com>
2017-02-28 19:08:10 +1100

    Topic/include pr branch info in run (#858)
    * Include branch and pr info in run model
    
    * Add skip none = true
    
    * .

--- blueocean-pipeline-api-impl/src/main/java/io/jenkins/blueocean/rest/impl/pipeline/BranchImpl.java
+++ blueocean-pipeline-api-impl/src/main/java/io/jenkins/blueocean/rest/impl/pipeline/BranchImpl.java
@@ -20,16 +20,15 @@ import org.jenkinsci.plugins.workflow.job.WorkflowJob;
 import org.kohsuke.stapler.export.Exported;
 import org.kohsuke.stapler.export.ExportedBean;
 
-import static io.jenkins.blueocean.rest.model.KnownCapabilities.*;
+import static io.jenkins.blueocean.rest.model.KnownCapabilities.BLUE_BRANCH;
+import static io.jenkins.blueocean.rest.model.KnownCapabilities.JENKINS_WORKFLOW_JOB;
+import static io.jenkins.blueocean.rest.model.KnownCapabilities.PULL_REQUEST;
 
 /**
  * @author Vivek Pandey
  */
 @Capability({BLUE_BRANCH, JENKINS_WORKFLOW_JOB, PULL_REQUEST})
 public class BranchImpl extends PipelineImpl {
-
-    private static final String PULL_REQUEST = "pullRequest";
-    private static final String BRANCH = "branch";
 
     private final Link parent;
     protected final Job job;
@@ -40,25 +39,12 @@ public class BranchImpl extends PipelineImpl {
         this.parent = parent;
     }
 
-    @Exported(name = PULL_REQUEST, inline = true)
+    @Exported(name = PullRequest.PULL_REQUEST, inline = true)
     public PullRequest getPullRequest() {
-        // TODO probably want to be using SCMHeadCategory instances to categorize them instead of hard-coding for PRs
-        SCMHead head = SCMHead.HeadByItem.findHead(job);
-        if(head instanceof ChangeRequestSCMHead) {
-            ChangeRequestSCMHead cr = (ChangeRequestSCMHead)head;
-            ObjectMetadataAction om = job.getAction(ObjectMetadataAction.class);
-            ContributorMetadataAction cm = job.getAction(ContributorMetadataAction.class);
-            return new PullRequest(
-                cr.getId(),
-                om != null ? om.getObjectUrl() : null,
-                om != null ? om.getObjectDisplayName() : null,
-                cm != null ? cm.getContributor() : null
-            );
-        }
-        return null;
+        return PullRequest.get(job);
     }
 
-    @Exported(name = BRANCH, inline = true)
+    @Exported(name = Branch.BRANCH, inline = true)
     public Branch getBranch() {
         ObjectMetadataAction om = job.getAction(ObjectMetadataAction.class);
         PrimaryInstanceMetadataAction pima = job.getAction(PrimaryInstanceMetadataAction.class);
@@ -96,6 +82,8 @@ public class BranchImpl extends PipelineImpl {
 
     @ExportedBean
     public static class Branch {
+
+        public static final String BRANCH = "branch";
         private static final String BRANCH_URL = "url";
         private static final String BRANCH_PRIMARY = "isPrimary";
 
@@ -116,10 +104,22 @@ public class BranchImpl extends PipelineImpl {
         public boolean isPrimary() {
             return primary;
         }
+
+        public static Branch getBranch(Job job) {
+            ObjectMetadataAction om = job.getAction(ObjectMetadataAction.class);
+            PrimaryInstanceMetadataAction pima = job.getAction(PrimaryInstanceMetadataAction.class);
+            if (om == null && pima == null) {
+                return null;
+            }
+            String url = om != null && om.getObjectUrl() != null ? om.getObjectUrl() : null;
+            return new Branch(url, pima != null);
+        }
     }
 
     @ExportedBean
     public static class PullRequest {
+
+        public static final String PULL_REQUEST = "pullRequest";
         private static final String PULL_REQUEST_NUMBER = "id";
         private static final String PULL_REQUEST_AUTHOR = "author";
         private static final String PULL_REQUEST_TITLE = "title";
@@ -161,6 +161,23 @@ public class BranchImpl extends PipelineImpl {
         @Exported(name = PULL_REQUEST_AUTHOR)
         public String getAuthor() {
             return author;
+        }
+
+        public static PullRequest get(Job job) {
+            // TODO probably want to be using SCMHeadCategory instances to categorize them instead of hard-coding for PRs
+            SCMHead head = SCMHead.HeadByItem.findHead(job);
+            if(head instanceof ChangeRequestSCMHead) {
+                ChangeRequestSCMHead cr = (ChangeRequestSCMHead)head;
+                ObjectMetadataAction om = job.getAction(ObjectMetadataAction.class);
+                ContributorMetadataAction cm = job.getAction(ContributorMetadataAction.class);
+                return new PullRequest(
+                    cr.getId(),
+                    om != null ? om.getObjectUrl() : null,
+                    om != null ? om.getObjectDisplayName() : null,
+                    cm != null ? cm.getContributor() : null
+                );
+            }
+            return null;
         }
     }
 
