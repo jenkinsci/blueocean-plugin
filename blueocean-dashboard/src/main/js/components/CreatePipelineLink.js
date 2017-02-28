9273102818ee9a179510dd8bc07b25aac0781b41
Ivan Meredith <ivan@ivan.net.nz>
2017-02-28 15:23:57 +1300

    Show Creation link if security not enabled

--- blueocean-dashboard/src/main/js/components/CreatePipelineLink.js
+++ blueocean-dashboard/src/main/js/components/CreatePipelineLink.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-import { i18nTranslator, User } from '@jenkins-cd/blueocean-core-js';
+import { i18nTranslator, User, Security } from '@jenkins-cd/blueocean-core-js';
 
 const t = i18nTranslator('blueocean-dashboard');
 
 export default function CreatePipelineLink() {
     const user = User.current();
 
-    if (user && !user.permissions.pipeline.create()) {
+    if (Security.isSecurityEnabled() && user && !user.permissions.pipeline.create()) {
         return null;
     }
 
