324303bc4e16aac05b3c935d9d2da33f4d021b48
Ivan Meredith <ivan@ivan.net.nz>
2017-03-01 14:29:52 +1300

    [JENKINS-41434] Hide link when no create permission (#849)
    * [JENKINS-41434] Use user permissions to show/hide new pipeline link
    
    * Release beta of core-js
    
    * Fix type with _class
    
    * Show Creation link if security not enabled
    
    * Release new version of core-js

--- blueocean-core-js/src/js/User.js
+++ blueocean-core-js/src/js/User.js
@@ -4,14 +4,91 @@
 
 import { blueocean } from './scopes';
 
-export class User {
-
+class PipelinePermissions {
     constructor(blueUser = {}) {
-        this._class = blueUser._class;
-        this._links = blueUser._links;
-        this.email = blueUser.email;
-        this.fullName = blueUser.fullName;
-        this.id = blueUser.id || 'anonymous';
+        if (blueUser.permission && blueUser.permission.pipeline) {
+            this._permissions = blueUser.permission.pipeline;
+        } else {
+            this._permissions = {};
+        }
+    }
+
+    create() {
+        return !!this._permissions.create;
+    }
+
+    read() {
+        return !!this._permissions.read;
+    }
+
+    start() {
+        return !!this._permissions.start;
+    }
+
+    stop() {
+        return !!this._permissions.stop;
+    }
+
+    configure() {
+        return !!this._permissions.configure;
+    }
+}
+
+
+class CredentialPermissions {
+    constructor(blueUser = {}) {
+        if (blueUser.permission && blueUser.permission.credential) {
+            this._permissions = blueUser.permission.credential;
+        } else {
+            this._permissions = {};
+        }
+    }
+
+    create() {
+        return !!this._permissions.create;
+    }
+
+    view() {
+        return !!this._permissions.view;
+    }
+
+    delete() {
+        return !!this._permissions.delete;
+    }
+
+    update() {
+        return !!this._permissions.update;
+    }
+
+    manageDomains() {
+        return !!this._permissions.manageDomains;
+    }
+}
+
+export class User {
+     
+    constructor(blueUser = {}) {
+        this._blueUser = blueUser;
+    }
+
+    get _class() {
+        return this._blueUser._class;
+    }
+
+    get _links() {
+        return this._blueUser._links;
+    }
+
+    get email() {
+        return this._blueUser.email;
+    }
+
+    get fullName() {
+        return this._blueUser.fullName;
+    }
+
+    get id() {
+        return this._blueUser.id || 'anonymous';
     }
 
     isAnonymous() {
@@ -20,6 +97,18 @@ export class User {
 
     static current() {
         return CURRENT; // eslint-disable-line no-use-before-define
+    }
+
+    get isAdministrator() {
+        const permission = this._blueUser._permission || {};
+        return !!permission.administrator;
+    }
+
+    get permissions() {
+        return {
+            pipeline: new PipelinePermissions(this._blueUser),
+            credential: new CredentialPermissions(this._blueUser),
+        };
     }
 }
 
