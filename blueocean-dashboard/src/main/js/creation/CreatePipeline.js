e5eafd85069fe507697ef6f0c11ba9bf45b2c31d
Cliff Meyers <cmeyers@cloudbees.com>
2017-03-07 12:44:50 -0500

    [JENKINS-42375] if the user doesn't have permission to create pipelines, show an error step instead of the "scm selection" step

--- blueocean-dashboard/src/main/js/creation/CreatePipeline.js
+++ blueocean-dashboard/src/main/js/creation/CreatePipeline.js
@@ -1,18 +1,19 @@
 import React, { PropTypes } from 'react';
 import { Page } from '@jenkins-cd/design-language';
-import { ContentPageHeader, i18nTranslator } from '@jenkins-cd/blueocean-core-js';
+import { ContentPageHeader, i18nTranslator, Security, User } from '@jenkins-cd/blueocean-core-js';
+import Extensions from '@jenkins-cd/js-extensions';
 
 import { ClassicCreationLink } from './ClassicCreationLink';
 import { CreatePipelineScmListRenderer } from './CreatePipelineScmListRenderer';
 import { CreatePipelineStepsRenderer } from './CreatePipelineStepsRenderer';
 import VerticalStep from './flow2/VerticalStep';
-
-import Extensions from '@jenkins-cd/js-extensions';
-const Sandbox = Extensions.SandboxedComponent;
-
 import loadingIndicator from '../LoadingIndicator';
+import StepStatus from './flow2/FlowStepStatus';
 
+
+const Sandbox = Extensions.SandboxedComponent;
 const t = i18nTranslator('blueocean-dashboard');
+
 
 export default class CreatePipeline extends React.Component {
 
@@ -34,6 +35,11 @@ export default class CreatePipeline extends React.Component {
         }
     }
 
+    _isCreationDisabled() {
+        const user = User.current();
+        return Security.isSecurityEnabled() && user && !user.permissions.pipeline.create();
+    }
+
     _onSelection(selectedProvider) {
         if (this.state.selectedProvider) {
             this.state.selectedProvider.destroyFlowManager();
@@ -48,6 +54,10 @@ export default class CreatePipeline extends React.Component {
         this._onExit(path);
     }
 
+    _onNavigatePipelines() {
+        this._onExit({ url: '/pipelines' });
+    }
+
     _onExit({ url } = {}) {
         if (url) {
             this.context.router.replace(url);
@@ -59,7 +69,8 @@ export default class CreatePipeline extends React.Component {
     }
 
     render() {
-        const firstStepStatus = this.state.selectedProvider ? 'complete' : 'active';
+        const firstStepStatus = this.state.selectedProvider ? StepStatus.COMPLETE : StepStatus.ACTIVE;
+        const creationEnabled = !this._isCreationDisabled();
 
         return (
             <Page>
@@ -69,6 +80,7 @@ export default class CreatePipeline extends React.Component {
 
                         <ClassicCreationLink />
                     </ContentPageHeader>
+                    { creationEnabled &&
                     <main>
                         <article className="content-area">
                             <VerticalStep className="first-step" status={firstStepStatus}>
@@ -89,6 +101,20 @@ export default class CreatePipeline extends React.Component {
                             </Sandbox>
                         </article>
                     </main>
+                    }
+                    { !creationEnabled &&
+                    <main>
+                        <article className="content-area">
+                            <VerticalStep className="first-step" status={StepStatus.ERROR}>
+                                <h1>{t('creation.core.intro.invalid_permission_title')}</h1>
+
+                                <button onClick={() => this._onNavigatePipelines()}>
+                                    {t('creation.core.intro.invalid_permission_button')}
+                                </button>
+                            </VerticalStep>
+                        </article>
+                    </main>
+                    }
                 </div>
             </Page>
         );
