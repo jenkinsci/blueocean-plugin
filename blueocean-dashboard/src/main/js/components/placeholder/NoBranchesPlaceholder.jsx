7fd2f60c592d4491a29a140ff4320d4f03ed457e
Cliff Meyers <cmeyers@cloudbees.com>
2017-03-22 15:17:05 -0400

    [JENKINS-41740] sizing and alignment tweaks

--- blueocean-dashboard/src/main/js/components/placeholder/NoBranchesPlaceholder.jsx
+++ blueocean-dashboard/src/main/js/components/placeholder/NoBranchesPlaceholder.jsx
@@ -10,14 +10,14 @@ export function NoBranchesPlaceholder(props) {
     const { t } = props;
 
     const columns = [
-        { width: 50, head: { text: 40 }, cell: { icon: 20 } },
+        { width: 40, head: { text: 40 }, cell: { icon: 20 } },
         { width: 50, head: { text: 40 }, cell: { text: 30 } },
         { width: 50, head: { text: 50 }, cell: { text: 80 } },
         { width: 50, head: { text: 50 }, cell: { text: 60 } },
-        { width: 200, head: { text: 60 }, cell: { text: 150 } },
+        { width: 200, isFlexible: true, head: { text: 60 }, cell: { text: 150 } },
         { width: 100, head: { text: 60 }, cell: { text: 60 } },
         { width: 100, head: { text: 60 }, cell: { text: 60 } },
-        { width: 50, head: {}, cell: { icon: 20 } },
+        { width: 20, head: {}, cell: { icon: 20 } },
     ];
 
     const content = {
@@ -29,7 +29,7 @@ export function NoBranchesPlaceholder(props) {
     };
 
     return (
-        <PlaceholderContent className="u-fill u-fade-bottom" style={{ top: 72 }}>
+        <PlaceholderContent className="NoBranches u-fill u-fade-bottom" style={{ top: 72 }}>
             <PlaceholderTable columns={columns} rowCount={20} />
             <PlaceholderDialog width={375} content={content} />
         </PlaceholderContent>
