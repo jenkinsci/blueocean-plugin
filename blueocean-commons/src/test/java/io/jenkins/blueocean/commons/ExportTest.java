45e573d237ef5aa4d7769b1efd2095c1e1bdfb69
James Dumay <jdumay@cloudbees.com>
2017-04-10 17:11:35 +1000

    Fix test

--- blueocean-commons/src/test/java/io/jenkins/blueocean/commons/ExportTest.java
+++ blueocean-commons/src/test/java/io/jenkins/blueocean/commons/ExportTest.java
@@ -1,8 +1,6 @@
 package io.jenkins.blueocean.commons;
 
 import io.jenkins.blueocean.commons.stapler.Export;
-import io.jenkins.blueocean.commons.stapler.ModelObjectSerializerTest;
-import io.jenkins.blueocean.commons.stapler.ModelObjectSerializerTest.X;
 import net.sf.json.JSONObject;
 import org.junit.Assert;
 import org.junit.Test;
@@ -14,10 +12,10 @@ import java.io.IOException;
 public class ExportTest {
     @Test
     public void test_json() throws IOException {
-        String xJson = Export.toJson(new ModelObjectSerializerTest.X());
+        String xJson = Export.toJson(new X());
         JSONObject jsonObj = JSONObject.fromObject(xJson);
 
-        Assert.assertEquals(ModelObjectSerializerTest.X.class.getName(), jsonObj.getString("_class"));
+        Assert.assertEquals(X.class.getName(), jsonObj.getString("_class"));
         Assert.assertEquals("xVal", jsonObj.getString("val"));
     }
 
