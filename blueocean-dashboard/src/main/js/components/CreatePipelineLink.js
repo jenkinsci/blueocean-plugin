b8e44a327fdf88522b69640d5f3d6b15f2067d63
Cliff Meyers <cliff.meyers@gmail.com>
2017-04-11 14:23:10 -0400

    Bug/jenkins 43347 security disabled creation disabled (#955)
    * [JENKINS-43347] show an error message for Creation if the jenkins instance has security disabled
    
    * [JENKINS-43347] remove obsolete security util

--- blueocean-dashboard/src/main/js/components/CreatePipelineLink.js
+++ blueocean-dashboard/src/main/js/components/CreatePipelineLink.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { Link } from 'react-router';
 
 import { i18nTranslator } from '@jenkins-cd/blueocean-core-js';
-import securityUtils from '../util/security-utils';
+import creationUtils from '../creation/creation-status-utils';
 
 const t = i18nTranslator('blueocean-dashboard');
 
 export default function CreatePipelineLink() {
-    if (!securityUtils.isCreationEnabled()) {
+    if (creationUtils.isHidden()) {
         return null;
     }
 
