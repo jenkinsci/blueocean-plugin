acba3a346ca9f0ef7b5f7fc9144ea19d7370332f
Josh McDonald <josh@joshmcdonald.info>
2017-04-13 10:30:04 +1000

    task/libify-time-harmonizer * Remove wacky isRunning method

--- blueocean-core-js/src/js/components/TimeHarmonizer.jsx
+++ blueocean-core-js/src/js/components/TimeHarmonizer.jsx
@@ -19,6 +19,17 @@ function translate(...args) { // TODO: RM
     return 'NUTS';
 }
 
+function jobStillActive(status) {
+    switch (String(status).toUpperCase()) {
+        case 'RUNNING':
+        case 'PAUSED':
+        case 'QUEUED':
+            return true;
+        default:
+            return false;
+    }
+}
+
 export class TimeHarmonizerUtil {
     // Construct with the owning component for access to props / context
     constructor(owner) {
@@ -36,11 +47,11 @@ export class TimeHarmonizerUtil {
     };
 
     getDuration = (result) => {
-        const durationMillis = this.isRunningFunction(result)() ? this.durationMillis : this.getTimes().durationMillis;
+        const durationMillis = jobStillActive(result) ? this.durationMillis : this.getTimes().durationMillis;
         return durationMillis;
     };
 
-    getTimes = (props) => {
+    getTimes = (props) => { // TODO: Get rid of the props argument
         props = props || this.owner.props;
         const { result, startTime, durationInMillis, endTime } = props;
         if (!startTime) {
@@ -51,7 +62,7 @@ export class TimeHarmonizerUtil {
             startTime,
             endTime,
             durationInMillis,
-            isRunning: this.isRunningFunction(result)(),
+            isRunning: jobStillActive(result),
         }, this.getSkewMillis());
         return harmonizeTimes;
     };
@@ -64,18 +75,6 @@ export class TimeHarmonizerUtil {
 
         const title = translate(`common.state.${result.toLowerCase()}`, { 0: i18nDuration });
         return title;
-    };
-
-    isRunningFunction = (result) => {
-        // FIXME: Why on earth does this return a function? Why does it even exist?
-        switch (result) {
-            case 'RUNNING':
-            case 'PAUSED':
-            case 'QUEUED':
-                return () => true;
-            default:
-                return () => false;
-        }
     };
 }
 
@@ -120,90 +119,4 @@ export const TimeHarmonizer = ComposedComponent => {
     return NewComponent;
 };
 
-// const TimeHarmonizerOLD = ComposedComponent => {
-//     class NewComponent extends Component {
-//         constructor(props, context) {
-//             super(props, context);
-//             logger.warn(props, context);
-//             const { startTime } = this.props;
-//             this.skewMillis = this.context && this.context.config ? this.context.config.getServerBrowserTimeSkewMillis() : 0;
-//             this.durationMillis = startTime ? this.getTimes(props).durationMillis : 0;
-//             this.getI18nTitle = this.getI18nTitle.bind(this);
-//             this.getDuration = this.getDuration.bind(this);
-//             this.getTimes = this.getTimes.bind(this);
-//         }
-//
-//         getDuration(result) {
-//             const durationMillis = this.isRunningFunction(result)() ? this.durationMillis : this.getTimes().durationMillis;
-//             return durationMillis;
-//         }
-//
-//         getTimes(props = this.props) {
-//             const { result, startTime, durationInMillis, endTime } = props;
-//             if (!startTime) {
-//                 return {};
-//             }
-//             // we need to make sure that we calculate with the correct time offset
-//             const harmonizeTimes = timeManager.harmonizeTimes({
-//                 startTime,
-//                 endTime,
-//                 durationInMillis,
-//                 isRunning: this.isRunningFunction(result)(),
-//             }, this.skewMillis);
-//             logger.warn('Returning object', harmonizeTimes);
-//             return harmonizeTimes;
-//         }
-//
-//         getI18nTitle(result) {
-//             const durationMillis = this.getDuration(result);
-//             const i18nDuration = timeManager
-//                 .format(durationMillis,
-//                     translate('common.date.duration.hint.format', { defaultValue: 'M [month], d [days], h[h], m[m], s[s]' }));
-//
-//             logger.warn('times', { thisDuration: this.durationMillis, i18nDuration, durationMillis });
-//             const title = translate(`common.state.${result.toLowerCase()}`, { 0: i18nDuration });
-//             return title;
-//         }
-//
-//         isRunningFunction(result) {
-//             const isRunning = () => {
-//                 switch (result) {
-//                     case 'RUNNING':
-//                     case 'PAUSED':
-//                     case 'QUEUED':
-//                         return true;
-//                     default:
-//                         return false;
-//                 }
-//             };
-//             return isRunning;
-//         }
-//
-//         render() {
-//             const childProps = {
-//                 ...this.props,
-//                 ...this.state,
-//                 getTimes: this.getTimes,
-//                 getDuration: this.getDuration,
-//                 getI18nTitle: this.getI18nTitle,
-//                 isRunning: this.isRunning,
-//             };
-//
-//             // create a composedComponent and inject the functions we want to expose
-//             return (<ComposedComponent {...childProps}/>);
-//         }
-//     }
-//
-//     NewComponent.propTypes = {
-//         result: PropTypes.string,
-//         startTime: PropTypes.string,
-//         endTime: PropTypes.string,
-//         durationInMillis: PropTypes.number,
-//     };
-//
-//     NewComponent.contextTypes = {
-//         config: PropTypes.object.isRequired,
-//     };
-//     return NewComponent;
-// };
 
