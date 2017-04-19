c8383770a1ff203d9ce12ba676130bd222ebfa1a
Vivek Pandey <vivek.pandey@gmail.com>
2017-04-19 14:24:03 -0700

    Task/jenkins 43248 Remove hardcoding of the jenkins organization from the UI (#965)
    * JENKINS-43248# Credential search API requires organization
    
    * JENKINS-43248# Organization preloader
    
    * JENKINS-43248# Resolve organization correctly
    
    * JENKINS-43248# Resolve organization using OrganizationResolver
    
    * JENKINS-43248# Removed this method not used anywhere and never worked
    
    This is removed as there is OrganizationResolver with different ways to
    get BlueOrganization.
    
    * JENKINS-43248# Should be not found error
    
    * JENKINS-43248# expose organization from config
    
    This provides single way to access organization in context.
    
    * JENKINS-43248# Fetch favorites using organization
    
    * JENKINS-43248# Call creation and credentials API using organization
    
    * JENKINS-43248# Create Pipeline link includes organization
    
    * JENKINS-43248# Added organization specific route to create-pipeline
    
    * JENKINS-43248# Use prefetched organization to use pipeline search
    
    * Test fixes
    
    * Organization name could be picked from system property
    
    * core js version dance
    
    * JENKINS-43248# getOperationName() with encoded parameter
    
    * Fixed OrganizationResolver imports
    
    * blueocean-core-js version dance - published 0.0.98

--- blueocean-core-js/src/js/paths/rest.js
+++ blueocean-core-js/src/js/paths/rest.js
@@ -1,3 +1,5 @@
+import AppConfig from '../config';
+
 /**
  * This object defines rest paths
  */
@@ -15,7 +17,7 @@ export default {
     },
 
     allPipelines() {
-        return `${this.apiRoot()}/search/?q=type:pipeline;excludedFromFlattening:jenkins.branch.MultiBranchProject,hudson.matrix.MatrixProject&filter=no-folders`;
+        return `${this.apiRoot()}/search/?q=type:pipeline;organization:${AppConfig.getOrganizationName()};excludedFromFlattening:jenkins.branch.MultiBranchProject,hudson.matrix.MatrixProject&filter=no-folders`;
     },
 
     activities(organization, pipeline, branch) {
