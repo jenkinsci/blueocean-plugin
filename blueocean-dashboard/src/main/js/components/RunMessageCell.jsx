c46865529c2e2bf173361074168dccbaed45214c
James Dumay <jdumay@cloudbees.com>
2017-04-29 17:17:24 +1000

    Tighten up lozenge spacing and truncation

--- blueocean-dashboard/src/main/js/components/RunMessageCell.jsx
+++ blueocean-dashboard/src/main/js/components/RunMessageCell.jsx
@@ -12,19 +12,19 @@ export default class RunMessageCell extends Component {
         const t = this.props.t;
         let message;
         if (run && run.description) {
-            message = (<span className="message" title={run.description}>{run.description}</span>);
+            message = (<span className="RunMessageCell" title={run.description}>{run.description}</span>);
         } else if (run && run.changeSet && run.changeSet.length > 0) {
             const commitMsg = run.changeSet[run.changeSet.length - 1].msg;
             if (run.changeSet.length > 1) {
-                return (<span className="message" title={commitMsg}>{commitMsg} <Lozenge title={t('lozenge.commit', {0: run.changeSet.length})}/></span>);
+                return (<span className="RunMessageCell" title={commitMsg}><span className="RunMessageCellInner">{commitMsg}</span> <Lozenge title={t('lozenge.commit', {0: run.changeSet.length})}/></span>);
             } else {
-                return (<span className="message" title={commitMsg}>{commitMsg}</span>);
+                return (<span className="RunMessageCell" title={commitMsg}>{commitMsg}</span>);
             }
         } else if (run && run.causes.length > 0) {
             const cause = run.causes[0].shortDescription;
             return (<span className="cause" title={cause}>{cause}</span>)
         } else {
-            message = (<span className="message">–</span>);
+            message = (<span className="RunMessageCell">–</span>);
         }
         return message;
     }
