a0506f6944df464909a203b828f36a114feb524c
James Dumay <jdumay@cloudbees.com>
2017-04-29 11:24:36 +1000

    Oops

--- blueocean-dashboard/src/main/js/components/RunDetailsTests.jsx
+++ blueocean-dashboard/src/main/js/components/RunDetailsTests.jsx
@@ -32,7 +32,7 @@ export default class RunDetailsTests extends Component {
         const { t, locale } = this.props;
 
         let result;
-        if (this.props.pipeline.testSummary) {
+        if (this.props.result.testSummary) {
             result = (
                 <div className="test-results-container">
                     <TestResults
