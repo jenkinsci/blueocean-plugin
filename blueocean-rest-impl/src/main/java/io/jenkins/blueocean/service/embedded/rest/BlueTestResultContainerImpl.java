cdc0a640758713ef8204a973b51c33792edca154
James Dumay <jdumay@cloudbees.com>
2017-05-01 12:34:20 +1000

    Fix bug where tests where not being filtered by state correctly.

--- blueocean-rest-impl/src/main/java/io/jenkins/blueocean/service/embedded/rest/BlueTestResultContainerImpl.java
+++ blueocean-rest-impl/src/main/java/io/jenkins/blueocean/service/embedded/rest/BlueTestResultContainerImpl.java
@@ -1,5 +1,6 @@
 package io.jenkins.blueocean.service.embedded.rest;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Predicate;
 import com.google.common.base.Predicates;
 import com.google.common.collect.Iterables;
@@ -21,7 +22,7 @@ import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.util.Iterator;
 
-import static org.apache.commons.lang.StringUtils.isNotEmpty;
+import static org.apache.commons.lang.StringUtils.isEmpty;
 
 public class BlueTestResultContainerImpl extends BlueTestResultContainer {
     private final Run<?, ?> run;
@@ -35,7 +36,9 @@ public class BlueTestResultContainerImpl extends BlueTestResultContainer {
     @SuppressWarnings("unchecked")
     public BlueTestResult get(final String name) {
         Result resolved = BlueTestResultFactory.resolve(run, parent);
-        checkFoundTests(resolved);
+        if (resolved.summary == null || resolved.results == null) {
+            throw new NotFoundException("no tests");
+        }
         BlueTestResult testResult = Iterables.find(resolved.results, new Predicate<BlueTestResult>() {
             @Override
             public boolean apply(@Nullable BlueTestResult input) {
@@ -52,57 +55,66 @@ public class BlueTestResultContainerImpl extends BlueTestResultContainer {
     @Override
     public Iterator<BlueTestResult> iterator() {
         Result resolved = BlueTestResultFactory.resolve(run, parent);
-        checkFoundTests(resolved);
-        StaplerRequest request = Stapler.getCurrentRequest();
-        if (request != null) {
-            String status = request.getParameter("status");
-            String state = request.getParameter("state");
-
-            if (isNotEmpty(status) && isNotEmpty(state)) {
-                throw new BadRequestExpception("must provide either status or state");
-            }
-
-            if (isNotEmpty(status)) {
-                String[] statusAtoms = StringUtils.split(status, ',');
-                Predicate<BlueTestResult> predicate = Predicates.alwaysFalse();
-                if (statusAtoms != null && statusAtoms.length > 0) {
-                    for (String statusString : statusAtoms) {
-                        Status queryStatus;
-                        try {
-                            queryStatus = Status.valueOf(statusString.toUpperCase());
-                        } catch (IllegalArgumentException e) {
-                            throw new BadRequestExpception("bad status " + status, e);
-                        }
-                        predicate = Predicates.or(predicate, new StatusPredicate(queryStatus));
-                    }
-                    return Iterables.filter(resolved.results, predicate).iterator();
-                }
-            }
-
-            if (isNotEmpty(state)) {
-                String[] stateAtoms = StringUtils.split(status, ',');
-                Predicate<BlueTestResult> predicate = Predicates.alwaysFalse();
-                if (stateAtoms != null && stateAtoms.length > 0) {
-                    for (String stateString : stateAtoms) {
-                        State queryState;
-                        try {
-                            queryState = State.valueOf(stateString.toUpperCase());
-                        } catch (IllegalArgumentException e) {
-                            throw new BadRequestExpception("bad state " + status, e);
-                        }
-                        predicate = Predicates.or(predicate, new StatePredicate(queryState));
-                    }
-                    return Iterables.filter(resolved.results, predicate).iterator();
-                }
-            }
-        }
-        return resolved.results.iterator();
-    }
-
-    private void checkFoundTests(Result resolved) {
         if (resolved.summary == null || resolved.results == null) {
             throw new NotFoundException("no tests");
         }
+        StaplerRequest request = Stapler.getCurrentRequest();
+        Iterator<BlueTestResult> results;
+        if (request != null) {
+            String status = request.getParameter("status");
+            String state = request.getParameter("state");
+            if (isEmpty(status) && isEmpty(state)) {
+                results = resolved.results.iterator();
+            } else if (!isEmpty(status)) {
+                results = filterByStatus(resolved.results, status);
+            } else if (!isEmpty(state)) {
+                results = filterByState(resolved.results, state);
+            } else {
+                throw new BadRequestExpception("must provide either 'status' or 'state' params");
+            }
+        } else {
+            results = resolved.results.iterator();
+        }
+        return results;
+    }
+
+    @VisibleForTesting
+    public static Iterator<BlueTestResult> filterByStatus(Iterable<BlueTestResult> results, String status) {
+        String[] statusAtoms = StringUtils.split(status, ',');
+        Predicate<BlueTestResult> predicate = Predicates.alwaysFalse();
+        if (statusAtoms == null || statusAtoms.length == 0) {
+            throw new BadRequestExpception("status not provided");
+        }
+        for (String statusString : statusAtoms) {
+            Status queryStatus;
+            try {
+                queryStatus = Status.valueOf(statusString.toUpperCase());
+            } catch (IllegalArgumentException e) {
+                throw new BadRequestExpception("bad status " + status, e);
+            }
+            predicate = Predicates.or(predicate, new StatusPredicate(queryStatus));
+        }
+        return Iterables.filter(results, predicate).iterator();
+    }
+
+    @VisibleForTesting
+    public static Iterator<BlueTestResult> filterByState(Iterable<BlueTestResult> results, String state) {
+        String[] stateAtoms = StringUtils.split(state, ',');
+        Predicate<BlueTestResult> predicate = Predicates.alwaysFalse();
+        if (stateAtoms == null || stateAtoms.length == 0) {
+            throw new BadRequestExpception("state not provided");
+        }
+
+        for (String stateString : stateAtoms) {
+            State queryState;
+            try {
+                queryState = State.valueOf(stateString.toUpperCase());
+            } catch (IllegalArgumentException e) {
+                throw new BadRequestExpception("bad state " + state, e);
+            }
+            predicate = Predicates.or(predicate, new StatePredicate(queryState));
+        }
+        return Iterables.filter(results, predicate).iterator();
     }
 
     static class StatusPredicate implements Predicate<BlueTestResult> {
