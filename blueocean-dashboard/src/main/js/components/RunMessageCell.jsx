29e0e757af9856496adc8ca13fe751bb1fe50c77
James Dumay <jdumay@cloudbees.com>
2017-05-01 16:23:20 +1000

    fix

--- blueocean-dashboard/src/main/js/components/RunMessageCell.jsx
+++ blueocean-dashboard/src/main/js/components/RunMessageCell.jsx
@@ -1,5 +1,4 @@
 import React, { Component, PropTypes } from 'react';
-import Lozenge from './Lozenge';
 
 export default class RunMessageCell extends Component {
     propTypes = {
@@ -16,13 +15,13 @@ export default class RunMessageCell extends Component {
         } else if (run && run.changeSet && run.changeSet.length > 0) {
             const commitMsg = run.changeSet[run.changeSet.length - 1].msg;
             if (run.changeSet.length > 1) {
-                return (<span className="RunMessageCell" title={commitMsg}><span className="RunMessageCellInner">{commitMsg}</span> <Lozenge title={t('lozenge.commit', {0: run.changeSet.length})}/></span>);
+                return (<span className="RunMessageCell" title={commitMsg}></span>);
             } else {
                 return (<span className="RunMessageCell" title={commitMsg}>{commitMsg}</span>);
             }
         } else if (run && run.causes.length > 0) {
             const cause = run.causes[0].shortDescription;
-            return (<span className="cause" title={cause}>{cause}</span>)
+            return (<span className="RunMessageCell" title={cause}>{cause}</span>)
         } else {
             message = (<span className="RunMessageCell">–</span>);
         }
