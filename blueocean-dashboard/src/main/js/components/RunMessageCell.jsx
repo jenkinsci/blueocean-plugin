aba5afa1e812803362b9751286868a11382895ad
James Dumay <jdumay@cloudbees.com>
2017-05-02 10:26:04 +1000

    .

--- blueocean-dashboard/src/main/js/components/RunMessageCell.jsx
+++ blueocean-dashboard/src/main/js/components/RunMessageCell.jsx
@@ -18,13 +18,13 @@ export default class RunMessageCell extends Component {
             if (run.changeSet.length > 1) {
                 return (<span className="RunMessageCell" title={commitMsg}><span className="RunMessageCellInner">{commitMsg}</span> <Lozenge title={t('lozenge.commit', {0: run.changeSet.length})}/></span>);
             } else {
-                return (<span className="RunMessageCell" title={commitMsg}>{commitMsg}</span>);
+                return (<span className="RunMessageCell" title={commitMsg}><span className="RunMessageCellInner">{commitMsg}</span></span>);
             }
         } else if (run && run.causes.length > 0) {
             const cause = run.causes[0].shortDescription;
-            return (<span className="RunMessageCell" title={cause}>{cause}</span>)
+            return (<span className="RunMessageCell" title={cause}><span className="RunMessageCellInner">{cause}</span></span>)
         } else {
-            message = (<span className="RunMessageCell">–</span>);
+            message = (<span className="RunMessageCell"><span className="RunMessageCellInner">–</span></span>);
         }
         return message;
     }
