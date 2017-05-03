f8adbcd4bd9b78e94ffc0c3d257777c818df92cc
Josh McDonald <josh@joshmcdonald.info>
2017-05-03 15:20:30 +1000

    josh/jenkins-41833-new-table-migration-2 * moar cleanup

--- blueocean-dashboard/src/main/js/components/TestComponent1.jsx
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-import React, { Component, PropTypes } from 'react';
-import { Icon } from '@jenkins-cd/react-material-icons';
-
-// TODO: Remove this bogus testing crap :D
-
-const TestComponent1 = (props) => <Icon icon="settings" size={24} />;
-
-export default TestComponent1;
--- blueocean-dashboard/src/main/js/components/TestComponent2.jsx
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-import React, { Component, PropTypes } from 'react';
-import { Icon } from '@jenkins-cd/react-material-icons';
-
-// TODO: Remove this bogus testing crap :D
-
-const TestComponent2 = (props) => <Icon icon="file_download" size={24} />;
-
-export default TestComponent2;
