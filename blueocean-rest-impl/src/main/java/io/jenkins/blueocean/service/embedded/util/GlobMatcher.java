d53fa3c1b1180f7d0752782cb5ac271a8ed06b36
James Dumay <jdumay@cloudbees.com>
2017-05-11 14:35:12 +1000

    javadoc

--- blueocean-rest-impl/src/main/java/io/jenkins/blueocean/service/embedded/util/GlobMatcher.java
+++ blueocean-rest-impl/src/main/java/io/jenkins/blueocean/service/embedded/util/GlobMatcher.java
@@ -5,6 +5,10 @@ import org.apache.oro.text.regex.MalformedPatternException;
 import org.apache.oro.text.regex.Pattern;
 import org.apache.oro.text.regex.Perl5Matcher;
 
+/**
+ * Matches glob string patters
+ * See https://en.wikipedia.org/wiki/Glob_(programming)
+ */
 public final class GlobMatcher {
 
     private static final GlobCompiler GLOB_COMPILER = new GlobCompiler();
@@ -19,6 +23,10 @@ public final class GlobMatcher {
         }
     }
 
+    /**
+     * @param input to check
+     * @return matches pattern or not
+     */
     public boolean matches(String input) {
         return MATCHER.matches(input, pattern);
     }
