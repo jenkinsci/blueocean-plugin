ce750b90eaea2bb5d4a83ac1261ddb420f947cbb
Nicolae Pascu <contact@nicupascu.ro>
2017-05-16 18:06:50 +1000

    decodeURI text in branch filter that is being applied from the url get param

--- blueocean-dashboard/src/main/js/components/Activity.jsx
+++ blueocean-dashboard/src/main/js/components/Activity.jsx
@@ -111,8 +111,9 @@ export class Activity extends Component {
         const duration = t(`${head}.duration`, { defaultValue: 'Duration' });
         const completed = t(`${head}.completed`, { defaultValue: 'Completed' });
         const branchText = t(`${head}.branch`, { defaultValue: 'Branch' });
+        const decodedBranchName = decodeURIComponent(branch);
 
-        const branchFilter = isMultiBranchPipeline && (<ColumnFilter placeholder={branchText} value={branch}
+        const branchFilter = isMultiBranchPipeline && (<ColumnFilter placeholder={branchText} value={decodedBranchName}
             onChange={b => this.navigateToBranch(b)}
             options={pipeline.branchNames.map(b => decodeURIComponent(b))}
         />);
