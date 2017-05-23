c941d63de1d3f6fd803d08bc107d818c0bc8f418
Josh McDonald <josh@joshmcdonald.info>
2017-05-23 14:54:28 +1000

    josh/jenkins-41833-new-table-migration-2 * Fixes for pipelineRUnning and a little cleanup in freestyleRunning tests

--- acceptance-tests/src/test/js/notMultibranch/freestyleRunning.js
+++ acceptance-tests/src/test/js/notMultibranch/freestyleRunning.js
@@ -9,22 +9,27 @@ module.exports = {
         const freestyleCreate = browser.page.freestyleCreate().navigate();
         freestyleCreate.createFreestyle(jobName, 'freestyle.sh');
     },
-    
+
     /** Build freestyle Job*/
     'Step 02': function (browser) {
         var blueActivityPage = browser.page.bluePipelineActivity().forJob(jobName, 'jenkins');
         blueActivityPage.waitForElementVisible('.run-button');
-        
+
         // run the job
         blueActivityPage.click('.run-button');
         blueActivityPage.waitForElementVisible('@toastOpenButton');
-        
+
         //check it spins and then is done  
         blueActivityPage.waitForElementVisible('.run-button.btn-secondary');
-        blueActivityPage.waitForElementVisible(`[data-pipeline=${jobName}][data-runid='1']`);
+        const rowSelector = `[data-pipeline='${jobName}'][data-runid='1']`;
+        blueActivityPage.waitForElementVisible(rowSelector);
         blueActivityPage.waitForElementVisible('.progress-spinner');
-        blueActivityPage.waitForElementVisible('.success');         
-        blueActivityPage.waitForElementNotPresent('.progress-spinner');       
+        blueActivityPage.waitForElementVisible('.success');
+        blueActivityPage.waitForElementNotPresent('.progress-spinner');
+
+        browser.elements('css selector', rowSelector, function (res) {
+            browser.assert.equal(1, res.value.length, 'Correct number of runs started');
+        })
     },
 
 };
