02dae8506c8f74e1f2f48fa4cbf9ecd75e8fecfa
Ivan Meredith <ivan@ivan.net.nz>
2017-05-27 02:02:28 +1200

    Enable batch mode

--- acceptance-tests/run.sh
+++ acceptance-tests/run.sh
@@ -6,23 +6,23 @@
 
 # ------------------------------------------------------------------------------------------------------------------
 # Run acceptance tests.
-# 
+#
 # args:
-# 
+#
 #   -a  Blue Ocean aggregator plugin path
 #       e.g. ./run.sh -a=/Users/tfennelly/projects/blueocean/blueocean-plugin
 #   -v  Jenkins version
-#       e.g. ./run.sh -v=2.5 
-#   -s  Install local SNAPSHOTS. See https://github.com/jenkinsci/acceptance-test-harness/blob/master/docs/SUT-VERSIONS.md#install-plugins-from-local-maven-repository  
+#       e.g. ./run.sh -v=2.5
+#   -s  Install local SNAPSHOTS. See https://github.com/jenkinsci/acceptance-test-harness/blob/master/docs/SUT-VERSIONS.md#install-plugins-from-local-maven-repository
 #       e.g.  ./run.sh -s
 #   -d  Run a clean dev instance of Jenkins (with blueocean plugins) and keep it running. Allows you to do fast dev of tests in your IDE without having to
 #       constanly wait for Jenkins to startup etc. Need to ensure that your test does not use JenkinsAcceptanceTestRule while you are developing
 #       (e.g. via AbstractJUnitTest), or you'll be wasting your time.
 #       e.g.  ./run.sh -d
-# 
+#
 #   All other args are assumed to be customs plugins ala https://github.com/jenkinsci/acceptance-test-harness/blob/master/docs/SUT-VERSIONS.md#use-custom-plugin-file
 #   e.g. ./run.sh blueocean-plugin.jpi=/Users/tfennelly/projects/blueocean/blueocean-plugin/target/blueocean-plugin.hpi
-# 
+#
 # ------------------------------------------------------------------------------------------------------------------
 npm install
 assemble-plugins $AGGREGATOR_DIR
@@ -35,7 +35,7 @@ if [ "${RUN_SELENIUM}" == "true" ]; then
 fi
 
 
-EXECUTION="env JENKINS_JAVA_OPTS=\"${JENKINS_JAVA_OPTS}\" ${ATH_SERVER_HOST} ${ATH_SERVER_PORT} BROWSER=phantomjs LOCAL_SNAPSHOTS=${LOCAL_SNAPSHOTS} ${PLUGINS} PLUGINS_DIR=../runtime-plugins/runtime-deps/target/plugins PATH=./node:./node/npm/bin:./node_modules/.bin:${PATH} JENKINS_WAR=../bin/jenkins-${JENKINS_VERSION}.war mvn ${MAVEN_SETTINGS} test ${PROFILES} ${TEST_TO_RUN}"
+EXECUTION="env JENKINS_JAVA_OPTS=\"${JENKINS_JAVA_OPTS}\" ${ATH_SERVER_HOST} ${ATH_SERVER_PORT} BROWSER=phantomjs LOCAL_SNAPSHOTS=${LOCAL_SNAPSHOTS} ${PLUGINS} PLUGINS_DIR=../runtime-plugins/runtime-deps/target/plugins PATH=./node:./node/npm/bin:./node_modules/.bin:${PATH} JENKINS_WAR=../bin/jenkins-${JENKINS_VERSION}.war mvn -B ${MAVEN_SETTINGS} test ${PROFILES} ${TEST_TO_RUN}"
 
 echo ""
 echo "> ${EXECUTION}"
