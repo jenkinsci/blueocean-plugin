118380612ba580329bb7b5a9fa662e7609984a98
Cliff Meyers <cliff.meyers@gmail.com>
2017-05-31 12:44:20 -0400

    Bug/jenkins 44571 show more log broken (#1107)
    * make the "show complete log" button open the raw log in a new browser tab (temporary workaround for render performance issue)
    
    * TODO comment

--- blueocean-dashboard/src/main/js/components/karaoke/urls/prefixIfNeeded.js
+++ blueocean-dashboard/src/main/js/components/karaoke/urls/prefixIfNeeded.js
@@ -1,5 +1,6 @@
 import { AppConfig } from '@jenkins-cd/blueocean-core-js';
 
+// TODO: rename this as the name is misleading - it *always* adds a prefix
 export function prefixIfNeeded(url) {
     return `${AppConfig.getJenkinsRootURL().replace(/\/$/, '')}${url}`;
 }
