bbaec7d3a3611982215518400951993005618b8d
Josh McDonald <josh@joshmcdonald.info>
2017-06-16 12:42:23 +1000

    bug/JENKINS-44759-remove-search-query-param-2 * Import Cliff's test updates

--- acceptance-tests/src/main/java/io/blueocean/ath/pages/blue/DashboardPage.java
+++ acceptance-tests/src/main/java/io/blueocean/ath/pages/blue/DashboardPage.java
@@ -4,11 +4,12 @@ import io.blueocean.ath.BaseUrl;
 import io.blueocean.ath.WaitUtil;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -41,7 +42,7 @@ public class DashboardPage {
 
     public boolean isFavorite(String job) {
         WebElement favorite = wait.until(driver -> {
-            WebElement tr = driver.findElement(By.xpath("//*[@data-pipeline=\"" + job + "\"]"));
+            WebElement tr = driver.findElement(By.xpath("//tr[@data-name=\"" + job + "\"]"));
             return tr.findElement(By.cssSelector(".Checkbox.Favorite > label > input"));
         });
         return favorite.isSelected();
@@ -49,7 +50,7 @@ public class DashboardPage {
 
     public void toggleFavorite(String job) {
         WebElement favorite = wait.until(driver -> {
-            WebElement tr = driver.findElement(By.xpath("//*[@data-pipeline=\"" + job + "\"]"));
+            WebElement tr = driver.findElement(By.xpath("//tr[@data-name=\"" + job + "\"]"));
             return tr.findElement(By.cssSelector(".Checkbox.Favorite > label"));
         });
 
@@ -60,5 +61,48 @@ public class DashboardPage {
         } else {
             logger.info(String.format("Pipeline %s was unfavorited", job));
         }
+    }
+
+    public void findJob(String jobName) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(
+            By.cssSelector(".pipelines-table tr[data-name='"+jobName+"']")
+        ));
+    }
+
+    public int getJobCount() {
+        return driver.findElements(By.cssSelector(".pipelines-table tbody tr")).size();
+    }
+
+    public void testJobCountEqualTo(int numberOfJobs) {
+        wait.until(ExpectedConditions.numberOfElementsToBe(
+            By.cssSelector(".pipelines-table tbody tr"),
+            numberOfJobs
+        ));
+        logger.info("found job count = " + numberOfJobs);
+    }
+
+    public void testJobCountAtLeast(int numberOfJobs) {
+        wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(
+            By.cssSelector(".pipelines-table tbody tr"),
+            numberOfJobs - 1
+        ));
+        logger.info("found job count >= " + numberOfJobs);
+    }
+
+    public void enterSearchText(String searchText) {
+        WebElement element = wait.until(
+            ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".search-pipelines-input input"))
+        );
+        element.sendKeys(searchText);
+        logger.info("entered search text =" + searchText);
+    }
+
+    public void clearSearchText() {
+        WebElement element = wait.until(
+            ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".search-pipelines-input input"))
+        );
+        element.sendKeys(Keys.chord(Keys.CONTROL, "a"));
+        element.sendKeys(Keys.BACK_SPACE);
+        logger.info("cleared search text");
     }
 }
