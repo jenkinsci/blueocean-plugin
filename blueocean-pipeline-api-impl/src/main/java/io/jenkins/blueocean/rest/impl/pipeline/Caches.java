811ec32d0f6b6e126fc447df0318b08ccccb4aa1
James Dumay <jdumay@cloudbees.com>
2017-06-18 10:36:50 +1000

    add tests

--- blueocean-pipeline-api-impl/src/main/java/io/jenkins/blueocean/rest/impl/pipeline/Caches.java
+++ blueocean-pipeline-api-impl/src/main/java/io/jenkins/blueocean/rest/impl/pipeline/Caches.java
@@ -5,6 +5,7 @@ import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 import hudson.Extension;
 import hudson.model.Item;
+import hudson.model.ItemGroup;
 import hudson.model.Job;
 import hudson.model.listeners.ItemListener;
 import io.jenkins.blueocean.rest.impl.pipeline.BranchImpl.Branch;
@@ -16,52 +17,18 @@ import jenkins.scm.api.metadata.ObjectMetadataAction;
 import jenkins.scm.api.metadata.PrimaryInstanceMetadataAction;
 import jenkins.scm.api.mixin.ChangeRequestSCMHead;
 
-import javax.annotation.Nonnull;
 import java.util.concurrent.TimeUnit;
 
 class Caches {
 
-    public static final LoadingCache<String, PullRequest> PULL_REQUEST_METADATA = CacheBuilder.newBuilder().expireAfterAccess(1, TimeUnit.DAYS).build(new CacheLoader<String, PullRequest>() {
-        @Override
-        public PullRequest load(@Nonnull String key) throws Exception {
-            Job job = Jenkins.getInstance().getItemByFullName(key, Job.class);
-            if (job == null) {
-                return null;
-            }
-            // TODO probably want to be using SCMHeadCategory instances to categorize them instead of hard-coding for PRs
-            SCMHead head = SCMHead.HeadByItem.findHead(job);
-            if (head instanceof ChangeRequestSCMHead) {
-                ChangeRequestSCMHead cr = (ChangeRequestSCMHead) head;
-                ObjectMetadataAction om = job.getAction(ObjectMetadataAction.class);
-                ContributorMetadataAction cm = job.getAction(ContributorMetadataAction.class);
-                return new PullRequest(
-                    cr.getId(),
-                    om != null ? om.getObjectUrl() : null,
-                    om != null ? om.getObjectDisplayName() : null,
-                    cm != null ? cm.getContributor() : null
-                );
-            }
-            return null;
-        }
-    });
+    static final LoadingCache<String, PullRequest> PULL_REQUEST_METADATA = CacheBuilder.newBuilder()
+        .expireAfterAccess(1, TimeUnit.DAYS)
+        .build(new PullRequestCacheLoader(Jenkins.getInstance().getItemGroup()));
 
 
-    public static final LoadingCache<String, Branch> BRANCH_METADATA = CacheBuilder.newBuilder().expireAfterAccess(1, TimeUnit.DAYS).build(new CacheLoader<String, Branch>() {
-        @Override
-        public Branch load(@Nonnull String key) throws Exception {
-            Job job = Jenkins.getInstance().getItemByFullName(key, Job.class);
-            if (job == null) {
-                return null;
-            }
-            ObjectMetadataAction om = job.getAction(ObjectMetadataAction.class);
-            PrimaryInstanceMetadataAction pima = job.getAction(PrimaryInstanceMetadataAction.class);
-            if (om == null && pima == null) {
-                return null;
-            }
-            String url = om != null && om.getObjectUrl() != null ? om.getObjectUrl() : null;
-            return new Branch(url, pima != null);
-        }
-    });
+    static final LoadingCache<String, Branch> BRANCH_METADATA = CacheBuilder.newBuilder()
+        .expireAfterAccess(1, TimeUnit.DAYS)
+        .build(new BranchCacheLoader(Jenkins.getInstance().getItemGroup()));
 
     @Extension
     public static class ListenerImpl extends ItemListener {
@@ -94,6 +61,61 @@ class Caches {
             }
             PULL_REQUEST_METADATA.invalidate(item.getFullName());
             BRANCH_METADATA.invalidate(item.getFullName());
+        }
+    }
+
+    static class BranchCacheLoader extends CacheLoader<String, Branch> {
+        private ItemGroup<? extends Item> itemGroup;
+
+        BranchCacheLoader(ItemGroup<? extends Item> itemGroup) {
+            this.itemGroup = itemGroup;
+        }
+
+        @Override
+        public Branch load(String key) throws Exception {
+            Item item = itemGroup.getItem(key);
+            if (item == null) {
+                return null;
+            }
+            Job job = (Job)item;
+            ObjectMetadataAction om = job.getAction(ObjectMetadataAction.class);
+            PrimaryInstanceMetadataAction pima = job.getAction(PrimaryInstanceMetadataAction.class);
+            if (om == null && pima == null) {
+                return null;
+            }
+            String url = om != null && om.getObjectUrl() != null ? om.getObjectUrl() : null;
+            return new Branch(url, pima != null);
+        }
+    }
+
+    static class PullRequestCacheLoader extends CacheLoader<String, PullRequest> {
+        private ItemGroup<? extends Item> itemGroup;
+
+        PullRequestCacheLoader(ItemGroup<? extends Item> itemGroup) {
+            this.itemGroup = itemGroup;
+        }
+
+        @Override
+        public PullRequest load(String key) throws Exception {
+            Item item = itemGroup.getItem(key);
+            if (item == null) {
+                return null;
+            }
+            Job job = (Job)item;
+            // TODO probably want to be using SCMHeadCategory instances to categorize them instead of hard-coding for PRs
+            SCMHead head = SCMHead.HeadByItem.findHead(job);
+            if (head instanceof ChangeRequestSCMHead) {
+                ChangeRequestSCMHead cr = (ChangeRequestSCMHead) head;
+                ObjectMetadataAction om = job.getAction(ObjectMetadataAction.class);
+                ContributorMetadataAction cm = job.getAction(ContributorMetadataAction.class);
+                return new PullRequest(
+                    cr.getId(),
+                    om != null ? om.getObjectUrl() : null,
+                    om != null ? om.getObjectDisplayName() : null,
+                    cm != null ? cm.getContributor() : null
+                );
+            }
+            return null;
         }
     }
 
