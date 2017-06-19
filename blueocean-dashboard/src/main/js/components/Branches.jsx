242de6ab6f681fe8ffb03141fdb30cd5559429f3
Ivan Meredith <ivan@ivan.net.nz>
2017-06-19 16:07:25 +1200

    Reapply changes to branches

--- blueocean-dashboard/src/main/js/components/Branches.jsx
+++ blueocean-dashboard/src/main/js/components/Branches.jsx
@@ -4,11 +4,8 @@ import { LiveStatusIndicator, RunButton } from '@jenkins-cd/blueocean-core-js';
 import Extensions from '@jenkins-cd/js-extensions';
 import { observer } from 'mobx-react';
 import { CellRow, CellLink } from './CellLink';
-import { Icon } from '@jenkins-cd/react-material-icons';
-import { buildPipelineUrl } from '../util/UrlUtils';
-import { Link } from 'react-router';
 import RunMessageCell from './RunMessageCell';
-
+import RunHistoryButton from './RunHistoryButton';
 import { buildRunDetailsUrl } from '../util/UrlUtils';
 
 function sortByOrdinal(extensions, done) {
@@ -72,7 +69,6 @@ export default class Branches extends Component {
         }
         const cleanBranchName = decodeURIComponent(branch.name);
         const runDetailsUrl = buildRunDetailsUrl(branch.organization, pipeline.fullName, cleanBranchName, latestRun.id, 'pipeline');
-        const historyButtonUrl = `${buildPipelineUrl(branch.organization, pipeline.fullName)}/activity?branch=${encodeURIComponent(branch.name)}`;
         return (
             <CellRow linkUrl={runDetailsUrl} id={`${cleanBranchName}-${latestRun.id}`}>
                 <CellLink disableDefaultPadding>
@@ -107,11 +103,7 @@ export default class Branches extends Component {
                       onNavigation={openRunDetails}
                     />
 
-                    <div className="history-button-component">
-                        <Link to={historyButtonUrl} className="materials-icons history-button">
-                            <Icon size={24} icon="history" />
-                        </Link>
-                    </div>
+                   <RunHistoryButton pipeline={pipeline} branchName={branch.name} />
 
                     <Extensions.Renderer
                       extensionPoint="jenkins.pipeline.branches.list.action"
