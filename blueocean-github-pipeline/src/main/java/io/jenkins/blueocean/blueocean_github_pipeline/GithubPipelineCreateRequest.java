d57630573cbcb6c1b9ff42e368620f150d3a8ef0
Stephen Connolly <stephen.alan.connolly@gmail.com>
2017-06-22 09:39:04 +0100

    [FIXED JENKINS-45058] Now that we are using traits we can just copy them along

--- blueocean-github-pipeline/src/main/java/io/jenkins/blueocean/blueocean_github_pipeline/GithubPipelineCreateRequest.java
+++ blueocean-github-pipeline/src/main/java/io/jenkins/blueocean/blueocean_github_pipeline/GithubPipelineCreateRequest.java
@@ -148,18 +148,22 @@ public class GithubPipelineCreateRequest extends AbstractPipelineCreateRequest {
                 GitHubSCMNavigator gitHubSCMNavigator = organizationFolder.getNavigators().get(GitHubSCMNavigator.class);
 
                 StringBuilder sb = new StringBuilder();
+                List<SCMTrait<?>> traits = new ArrayList<>();
                 if (gitHubSCMNavigator != null) {
                     // currently, we are setting a series of regular expressions to match the repositories
                     // so we need to extract the current set for incoming create requests to keep them
                     // see a few lines below for the pattern being used
-                    String pattern = ".*";
+                    RegexSCMSourceFilterTrait regexTrait = null;
                     for (SCMTrait<?> t : gitHubSCMNavigator.getTraits()) {
                         if (t instanceof RegexSCMSourceFilterTrait) {
-                            pattern = ((RegexSCMSourceFilterTrait) t).getRegex();
-                            break;
+                            regexTrait = (RegexSCMSourceFilterTrait) t;
+                        } else {
+                            traits.add(t);
                         }
                     }
-                    Matcher matcher = Pattern.compile("\\((.*?)\\\\b\\)\\?").matcher(pattern);
+                    Matcher matcher = Pattern.compile("\\((.*?)\\\\b\\)\\?").matcher(
+                        regexTrait == null ? ".*" : regexTrait.getRegex()
+                    );
 
                     while (matcher.find()) {
                         String existingRepo = matcher.group(1);
@@ -178,22 +182,22 @@ public class GithubPipelineCreateRequest extends AbstractPipelineCreateRequest {
                     if (credentialId == null) {
                         credentialId = gitHubSCMNavigator.getCredentialsId();
                     }
-                }
-
-                gitHubSCMNavigator = new GitHubSCMNavigator(orgName);
-
-                List<SCMTrait<?>> traits = new ArrayList<>();
-                traits.add(new BranchDiscoveryTrait(true, true));
-                traits.add(new ForkPullRequestDiscoveryTrait(
+                } else {
+                    // these are the legacy defaults for discovery, TBD whether to change
+                    traits.add(new BranchDiscoveryTrait(true, true));
+                    traits.add(new ForkPullRequestDiscoveryTrait(
                         EnumSet.of(ChangeRequestCheckoutStrategy.MERGE),
                         new ForkPullRequestDiscoveryTrait.TrustContributors()
                     ));
+                }
                 for (String r : repos) {
                     sb.append(String.format("(%s\\b)?", r));
                 }
                 if (sb.length() > 0) {
                     traits.add(new RegexSCMSourceFilterTrait(sb.toString()));
                 }
+
+                gitHubSCMNavigator = new GitHubSCMNavigator(orgName); // technically only needed if new org folder
                 gitHubSCMNavigator.setTraits(traits);
                 gitHubSCMNavigator.setApiUri(apiUrl);
                 gitHubSCMNavigator.setCredentialsId(credentialId);
