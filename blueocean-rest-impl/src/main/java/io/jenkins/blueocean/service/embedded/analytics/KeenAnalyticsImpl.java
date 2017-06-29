f8b2524bca95c7aa7620d9a5cb30fe7269fe6fba
James William Dumay <jdumay@cloudbees.com>
2017-06-29 10:26:16 +1000

    Configure Keen impl via extension and add extension to enhance props (#1207)
    * Configure Keen impl via extension and add extension to enhance props
    
    * Move to own test class
    
    * Update keys

--- blueocean-rest-impl/src/main/java/io/jenkins/blueocean/service/embedded/analytics/KeenAnalyticsImpl.java
+++ blueocean-rest-impl/src/main/java/io/jenkins/blueocean/service/embedded/analytics/KeenAnalyticsImpl.java
@@ -20,17 +20,13 @@ import java.util.Map;
 @Restricted(NoExternalUse.class)
 public class KeenAnalyticsImpl extends AbstractAnalytics {
 
-    private static final String PROJECT_ID = "5950934f3d5e150f5ab9d7be";
-    private static final String WRITE_KEY = "E6C1FA3407AF4DD3115DBC186E40E9183A90069B1D8BBA78DB3EA6B15EA6182C881E8C55B4D7A48F55D5610AD46F36E65093227A7490BF7A56307047903BCCB16D05B9456F18A66849048F100571FDC91888CAD94F2A271A8B9E5342D2B9404E";
-    private static final KeenClient CLIENT = new JavaKeenClientBuilder().build();
+    static final KeenClient CLIENT = new JavaKeenClientBuilder().build();
 
     static {
         KeenClient.initialize(CLIENT);
-        KeenClient.client().setDefaultProject(new KeenProject(PROJECT_ID, WRITE_KEY, null));
     }
 
-    public KeenAnalyticsImpl() {
-    }
+    public KeenAnalyticsImpl() {}
 
     @Override
     public boolean isEnabled() {
@@ -43,6 +39,10 @@ public class KeenAnalyticsImpl extends AbstractAnalytics {
         ProxyConfiguration proxyConfig = Jenkins.getInstance().proxy;
         Proxy proxy = proxyConfig == null ? null : proxyConfig.createProxy(null);
         CLIENT.setProxy(proxy);
+        // Ensure that we are using the right project info
+        KeenProject project = KeenConfiguration.get().project();
+        CLIENT.setDefaultProject(project);
+        // Send the event
         CLIENT.addEventAsync(name, allProps);
     }
 }
