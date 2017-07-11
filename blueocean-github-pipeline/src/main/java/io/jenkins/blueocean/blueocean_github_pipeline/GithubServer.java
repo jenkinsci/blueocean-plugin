974856d4efdca2c11e8349b50399126123a69acc
James William Dumay <jdumay@cloudbees.com>
2017-07-11 14:35:42 +1000

    Move to addEndpoint() to retain binary compatibility (#1241)

--- blueocean-github-pipeline/src/main/java/io/jenkins/blueocean/blueocean_github_pipeline/GithubServer.java
+++ blueocean-github-pipeline/src/main/java/io/jenkins/blueocean/blueocean_github_pipeline/GithubServer.java
@@ -1,5 +1,8 @@
 package io.jenkins.blueocean.blueocean_github_pipeline;
 
+import com.google.common.base.Charsets;
+import com.google.common.hash.Hashing;
+import com.google.common.io.BaseEncoding;
 import io.jenkins.blueocean.rest.hal.Link;
 import io.jenkins.blueocean.rest.model.Resource;
 import org.jenkinsci.plugins.github_branch_source.Endpoint;
@@ -11,6 +14,7 @@ import static hudson.Util.rawEncode;
 @ExportedBean
 public class GithubServer extends Resource {
 
+    public static final String ID = "ID";
     public static final String NAME = "name";
     public static final String API_URL = "apiUrl";
 
@@ -20,6 +24,11 @@ public class GithubServer extends Resource {
     GithubServer(Endpoint endpoint, Link parent) {
         this.endpoint = endpoint;
         this.parent = parent;
+    }
+
+    @Exported(name = ID)
+    public String getId() {
+        return Hashing.sha256().hashString(endpoint.getApiUri(), Charsets.UTF_8).toString();
     }
 
     @Exported(name = NAME)
@@ -34,6 +43,6 @@ public class GithubServer extends Resource {
 
     @Override
     public Link getLink() {
-        return parent.rel(rawEncode(endpoint.getName()));
+        return parent.rel(getId());
     }
 }
