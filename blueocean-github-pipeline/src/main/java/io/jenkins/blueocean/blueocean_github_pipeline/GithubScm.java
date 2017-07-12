68afc950062c87317c5551058d15506482f3444d
James Dumay <jdumay@cloudbees.com>
2017-07-13 09:16:44 +1000

    Improve message

--- blueocean-github-pipeline/src/main/java/io/jenkins/blueocean/blueocean_github_pipeline/GithubScm.java
+++ blueocean-github-pipeline/src/main/java/io/jenkins/blueocean/blueocean_github_pipeline/GithubScm.java
@@ -304,7 +304,7 @@ public class GithubScm extends Scm {
             throw new ServiceException.PreconditionRequired("Github accessToken does not have required scopes. Expected scopes 'user:email, repo'");
         }
         if(status == 404){
-            throw new ServiceException.NotFoundException("Github could not be found. Remote server sent 404.");
+            throw new ServiceException.NotFoundException(String.format("Remote server at %s responded with code 404.", apiUrl));
         }
         if(status != 200) {
             throw new ServiceException.BadRequestException(String.format("Github Api returned error: %s. Error message: %s.", connection.getResponseCode(), connection.getResponseMessage()));
