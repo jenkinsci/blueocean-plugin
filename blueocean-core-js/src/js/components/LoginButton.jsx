4793188f3210535b28aea5ab6a10f83986c2c9a9
Martin Nowak <code@dawg.eu>
2017-07-19 01:05:42 +0200

    provide a login/logout button in result header
    - having to close the result page, login, go back, and click retry is
      quite a non-obvious roundtrip coming from a GH status link
    - instead add a login/logout button to RunDetailsHeader

---
+++ blueocean-core-js/src/js/components/LoginButton.jsx
@@ -0,0 +1,38 @@
+import React, { Component, PropTypes } from 'react';
+import {
+    AppConfig, Security, UrlConfig, Utils,
+} from '../index';
+
+/**
+ * LoginButton to login/logout.
+ */
+export class LoginButton extends Component {
+
+    render() {
+        if (!Security.isSecurityEnabled()) {
+            return (null);
+        }
+
+        let action;
+        let url;
+        if (Security.isAnonymousUser()) {
+            action = 'login';
+            url = `${UrlConfig.getJenkinsRootURL()}/${AppConfig.getLoginUrl()}?from=${encodeURIComponent(Utils.windowOrGlobal().location.pathname)}`;
+        } else {
+            action = 'logout';
+            url = `${UrlConfig.getJenkinsRootURL()}/logout`;
+        }
+        return (
+            <div className={ this.props.className }>
+                <a href={url} className="btn-link">
+                    { this.props.translate(action, { defaultValue: action }) }
+                </a>
+            </div>
+        );
+    }
+}
+
+LoginButton.propTypes = {
+    className: PropTypes.string,
+    translate: PropTypes.func,
+};
