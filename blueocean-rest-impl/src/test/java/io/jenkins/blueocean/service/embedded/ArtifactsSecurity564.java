0177c643de59252cb8dd8ac93d42cdcbecc2d740
Ivan Meredith <ivan@ivan.net.nz>
2017-07-19 14:21:15 +1200

    Use a custom builder to generate files to archive

--- blueocean-rest-impl/src/test/java/io/jenkins/blueocean/service/embedded/ArtifactsSecurity564.java
+++ blueocean-rest-impl/src/test/java/io/jenkins/blueocean/service/embedded/ArtifactsSecurity564.java
@@ -1,5 +1,10 @@
 package io.jenkins.blueocean.service.embedded;
 
+import hudson.FilePath;
+import hudson.Launcher;
+import hudson.model.AbstractBuild;
+import hudson.model.Build;
+import hudson.model.BuildListener;
 import hudson.model.FreeStyleProject;
 import hudson.model.Hudson;
 import hudson.model.Item;
@@ -7,30 +12,45 @@ import hudson.model.Run;
 import hudson.security.GlobalMatrixAuthorizationStrategy;
 import hudson.security.HudsonPrivateSecurityRealm;
 import hudson.tasks.ArtifactArchiver;
+import hudson.tasks.Builder;
 import hudson.tasks.Shell;
+import org.junit.AfterClass;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.jvnet.hudson.test.Issue;
+import org.jvnet.hudson.test.TouchBuilder;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.Serializable;
+import java.net.URLEncoder;
 import java.util.List;
 import java.util.Map;
 
 public class ArtifactsSecurity564 extends BaseTest {
-    static {
+    @BeforeClass
+    public static void beforeClass() {
         System.setProperty("hudson.security.ArtifactsPermission", "true");
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        System.setProperty("hudson.security.ArtifactsPermission", "false");
     }
 
     /**
      * Uses matrix-auth to provide artifacts permission.
      *
      * If hudson.security.ArtifactsPermission is set then the user must have Run.ARTIFACTS set.
-     * 
+     *
      * @throws Exception
      */
     @Issue("SECURITY-564")
     @Test
     public void testArtifactsWithPermissions() throws Exception {
         String JOB_NAME = "artifactPermissions";
+        String artfictPath = "a/b/c";
         HudsonPrivateSecurityRealm realm = new HudsonPrivateSecurityRealm(false);
         realm.createAccount("alice","alice");
         realm.createAccount("bob","bob");
@@ -41,12 +61,11 @@ public class ArtifactsSecurity564 extends BaseTest {
         as.add(Hudson.READ,"alice");
         as.add(Item.READ,"alice");
         as.add(Run.ARTIFACTS,"alice");
-
         as.add(Hudson.READ,"bob");
         as.add(Item.READ,"bob");
 
         FreeStyleProject p = j.createFreeStyleProject(JOB_NAME);
-        p.getBuildersList().add(new Shell("#!/bin/bash\nmkdir -p test/me/out; cd test/me/out; touch {0..105}.txt"));
+        p.getBuildersList().add(new ArtifactBuilder(artfictPath, 100));
         p.getPublishersList().add(new ArtifactArchiver("**/*"));
         Run r = p.scheduleBuild2(0).waitForStart();
 
@@ -56,12 +75,32 @@ public class ArtifactsSecurity564 extends BaseTest {
 
         Assert.assertEquals(100, artifacts.size());
         Assert.assertEquals(0, ((Map) artifacts.get(0)).get("size"));
-        Assert.assertEquals("test/me/out/0.txt", ((Map) artifacts.get(0)).get("path"));
-        Assert.assertEquals("/job/artifactPermissions/1/artifact/test/me/out/0.txt", ((Map) artifacts.get(0)).get("url"));
+        Assert.assertEquals(artfictPath + "/0.txt", ((Map) artifacts.get(0)).get("path"));
+        Assert.assertEquals("/job/artifactPermissions/1/artifact/"+ artfictPath +"/0.txt", ((Map) artifacts.get(0)).get("url"));
 
         List artifactsBob = request().auth("bob", "bob").get("/organizations/jenkins/pipelines/"+JOB_NAME+"/runs/"+r.getId()+"/artifacts").build(List.class);
 
         Assert.assertEquals(0, artifactsBob.size());
     }
 
+    public static class ArtifactBuilder extends Builder implements Serializable {
+        private String path;
+        private int numberOfFiles;
+
+        public ArtifactBuilder(String path, int numberOfFiles) {
+            this.path = path;
+            this.numberOfFiles = numberOfFiles;
+        }
+
+        @Override
+        public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener) throws InterruptedException, IOException {
+            FilePath f = new FilePath(build.getWorkspace(), path);
+            f.mkdirs();
+            for (int i = 0; i < numberOfFiles; i++) {
+                new FilePath(f, i + ".txt").touch(System.currentTimeMillis());
+            }
+
+            return true;
+        }
+    }
 }
