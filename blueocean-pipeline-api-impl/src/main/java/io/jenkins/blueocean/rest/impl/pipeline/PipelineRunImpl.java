0e16f0b264e1cc37c965eca221d0f700e24791c7
Nicolae Pascu <contact@nicupascu.ro>
2017-07-21 22:21:38 +0300

    JENKINS-45011 make commit id in run result header clickable (#1212)
    * make commit id in run result header clickable
    
    * re-use the CommitHash component from the JDL
    
    * release beta version of corejs
    
    * fix broken test affected by the re-factoring
    
    * release beta version of core-js
    
    * move changes from https://github.com/jenkinsci/jenkins-design-language/pull/180/files
    
    * make the CommitId component consistent
    
    * publish new core-js and jdl versions
    
    * use the last commit in the changeset as the commit in the header
    
    * remove doubled check that was not needed
    
    * change the dash "type" so it is consistent
    
    * add commitUrl field in the run obj
    
    * use commitID and commitUrl instead of changeset ids and urls, remove test code
    
    * fix '—' char in test
    
    * release beta ver of core-js and jdl
    
    * release new ver of core-js and jdl

--- blueocean-pipeline-api-impl/src/main/java/io/jenkins/blueocean/rest/impl/pipeline/PipelineRunImpl.java
+++ blueocean-pipeline-api-impl/src/main/java/io/jenkins/blueocean/rest/impl/pipeline/PipelineRunImpl.java
@@ -27,6 +27,7 @@ import jenkins.model.Jenkins;
 import jenkins.scm.api.SCMRevisionAction;
 import org.jenkinsci.plugins.workflow.cps.replay.ReplayAction;
 import org.jenkinsci.plugins.workflow.cps.replay.ReplayCause;
+import org.jenkinsci.plugins.workflow.job.WorkflowJob;
 import org.jenkinsci.plugins.workflow.job.WorkflowRun;
 import org.jenkinsci.plugins.workflow.support.steps.ExecutorStepExecution;
 import org.jenkinsci.plugins.workflow.support.steps.input.InputAction;
@@ -175,6 +176,29 @@ public class PipelineRunImpl extends AbstractRunImpl<WorkflowRun> {
         SCMRevisionAction data = run.getAction(SCMRevisionAction.class);
         if (data != null){
             return data.getRevision().toString();
+        }
+        return null;
+    }
+
+    @Exported(name = "commitUrl")
+    public String getCommitUrl() {
+        String commitId = getCommitId();
+        if (commitId != null) {
+            Container<BlueChangeSetEntry> changeSets = getChangeSet();
+            int buildNumber = this.run.number;
+            WorkflowJob job = this.run.getParent();
+            while (buildNumber > 0) {
+                BlueChangeSetEntry entry = changeSets.get(commitId);
+                if (entry != null) {
+                    return entry.getUrl();
+                }
+                buildNumber--;
+                Run<?,?> run = job.getBuildByNumber(buildNumber);
+                if (run == null) {
+                    continue;
+                }
+                changeSets = AbstractRunImpl.getBlueRun(run, parent).getChangeSet();
+            }
         }
         return null;
     }
