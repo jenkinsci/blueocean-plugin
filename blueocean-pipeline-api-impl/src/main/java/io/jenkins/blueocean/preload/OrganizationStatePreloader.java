a4fdb665ff6599140e618f22c04b3511bafe0509
Alvaro Lobato <alvarolobato@gmail.com>
2017-07-25 00:35:50 +0200

    [JENKINS-45703][JENKINS-45442] [JENKINS-45482] [JENKINS-45360] Several fixes around organizations (#1272)
    * [JENKINS-45482] unit test showing incorrect behaviour.
    
    If a custom blue ocean organisation is in use and it uses a root item
    that is not Jenkins then modifying existing GitHub organistaions fails.
    
    * [JENKINS-45442] unit test showing incorrect permission check.
    
    When a custom organisation is in use pipeline creation checks
    permissions on jenkins rather than the organistion root.
    
    * Though shalt not set system properties in tests without restting them
    
    * [JENKINS-45360] unit test for duplicate MBP with custom org.
    
    Improve the test coverage and demonstrate the issue with MBP and custom
    organizations.
    shouldFailOnValidation4 is the test that fails in this case.
    
    * Just increasing test coverage of custom orgs.
    
    No issue was observed around this area but given I was working in the
    area seemed logical to add it at the same time to prevent any
    regreessions.
    
    * [JENKINS-45422] the ACL check should be on the org folder.
    
    The ACL check was checking the wrong ACL leading to a failure to create
    pipelines when a custom organisation was in use.
    
    * [JENKINS-45482] Use the correct Item.
    
    When obraining the Item to see if it already exists the check used the
    incorrect path if a custom Organisation was in use.
    
    * [JENKINS-45360] check the organisation root not Jenkins for duplicates.
    
    When we are checking for existing items we need to do it in the root of the organisation and not the root of Jenkins.
    
    * Credentials uses an invalid org when no authentication in use
    
    * [JENKINS-45703] - Check the organization from the url and the factory instead of jenkins root
    
    * Adress comment.
    
    * change version to steel
    
    * [JENKINS-45703] Use better regex
    
    * revert to normal snapshotting names

--- blueocean-pipeline-api-impl/src/main/java/io/jenkins/blueocean/preload/OrganizationStatePreloader.java
+++ blueocean-pipeline-api-impl/src/main/java/io/jenkins/blueocean/preload/OrganizationStatePreloader.java
@@ -1,21 +1,28 @@
 package io.jenkins.blueocean.preload;
 
+import java.io.StringWriter;
+import java.util.Iterator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.annotation.Nonnull;
+
+import org.kohsuke.stapler.Stapler;
+import org.kohsuke.stapler.StaplerRequest;
+
 import hudson.Extension;
 import io.jenkins.blueocean.commons.PageStatePreloader;
 import io.jenkins.blueocean.rest.factory.organization.AbstractOrganization;
 import io.jenkins.blueocean.rest.factory.organization.OrganizationFactory;
 import io.jenkins.blueocean.rest.model.BlueOrganization;
-import jenkins.model.Jenkins;
 import net.sf.json.util.JSONBuilder;
-
-import javax.annotation.Nonnull;
-import java.io.StringWriter;
 
 /**
  * @author Vivek Pandey
  */
 @Extension
 public class OrganizationStatePreloader extends PageStatePreloader {
+
     @Nonnull
     @Override
     public String getStatePropertyPath() {
@@ -24,12 +31,27 @@ public class OrganizationStatePreloader extends PageStatePreloader {
 
     @Override
     public String getStateJson() {
-        BlueOrganization organization = OrganizationFactory.getInstance().getContainingOrg(Jenkins.getInstance());
-        String organizationGroup="/"; //default is root group
-        if(organization instanceof AbstractOrganization){
-            organizationGroup = "/"+((AbstractOrganization) organization).getGroup().getFullName();
+        String orgName = getOrganizationFromURL();
+        BlueOrganization organization = null;
+        
+        OrganizationFactory orgFactory = OrganizationFactory.getInstance();
+        if (orgName != null) {
+            organization = orgFactory.get(orgName);
         }
+
+        if (organization == null) {
+            Iterator<BlueOrganization> iterator = orgFactory.list().iterator();
+            if (iterator.hasNext()) {
+                organization = iterator.next();
+            }
+        }
+
         if(organization != null) {
+            String organizationGroup = "/"; //default is root group
+            if (organization instanceof AbstractOrganization) {
+                organizationGroup = "/" + ((AbstractOrganization) organization).getGroup().getFullName();
+            }
+
             StringWriter writer = new StringWriter();
             new JSONBuilder(writer)
                     .object()
@@ -43,5 +65,27 @@ public class OrganizationStatePreloader extends PageStatePreloader {
         }else{
             return "{}";// if will happen only when there is no implementation of BlueOrganization found.
         }
+    }
+
+    private static final Pattern pattern = Pattern.compile("/blue/organizations/([^/]*)/");
+
+    private String getOrganizationFromURL() {
+        StaplerRequest currentRequest = Stapler.getCurrentRequest();
+        if (currentRequest == null) {
+            return null;
+        }
+
+        String requestURI = currentRequest.getRequestURI();
+
+        if (requestURI == null) {
+            return null;
+        }
+
+        Matcher matcher = pattern.matcher(requestURI);
+
+        if (matcher.find()) {
+            return matcher.group(1);
+        }
+        return null;
     }
 }
