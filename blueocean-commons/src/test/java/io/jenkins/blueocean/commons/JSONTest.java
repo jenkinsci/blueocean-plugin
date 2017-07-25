987ca5fa02f909be01ff4f31e62df709d99fe7ff
James Dumay <jdumay@cloudbees.com>
2017-07-25 13:42:35 +1000

    JENKINS-45099 escape step description so that it is ECMA script safe

--- blueocean-commons/src/test/java/io/jenkins/blueocean/commons/JSONTest.java
+++ blueocean-commons/src/test/java/io/jenkins/blueocean/commons/JSONTest.java
@@ -8,4 +8,9 @@ public class JSONTest {
     public void sanitize() throws Exception {
         Assert.assertEquals("hello vivek :)", JSON.sanitizeString("hello\r vivek\n\t :)"));
     }
+
+    @Test
+    public void escape() throws Exception {
+        Assert.assertEquals("\\\\033[32m some text \\\\033[0m", JSON.escape("\\033[32m some text \\033[0m"));
+    }
 }
