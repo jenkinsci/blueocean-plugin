7f6769ea16da63d834201ab836c7e321a154234e
James Dumay <jdumay@cloudbees.com>
2017-08-09 11:12:28 +1000

    Improve test

--- blueocean-pipeline-api-impl/src/test/java/io/jenkins/blueocean/htmlpublisher/HTMLArtifactTest.java
+++ blueocean-pipeline-api-impl/src/test/java/io/jenkins/blueocean/htmlpublisher/HTMLArtifactTest.java
@@ -33,8 +33,9 @@ public class HTMLArtifactTest extends PipelineBaseTest {
         BluePipeline  bluePipeline = (BluePipeline) BluePipelineFactory.resolve(p);
         BlueArtifactContainer artifacts = bluePipeline.getLatestRun().getArtifacts();
 
-        BlueArtifact artifact = Iterators.getOnlyElement(artifacts.iterator());
+        Assert.assertEquals(1, Iterators.size(artifacts.iterator()));
 
+        BlueArtifact artifact = Iterators.getOnlyElement(artifacts.iterator());
         Assert.assertEquals("/blue/rest/organizations/jenkins/pipelines/project/runs/1/artifacts/io.jenkins.blueocean.htmlpublisher.HTMLArtifact%3AMy%2520Cool%2520report/", artifact.getLink().getHref());
         Assert.assertEquals("My Cool report", artifact.getName());
         Assert.assertEquals("My Cool report", artifact.getPath());
