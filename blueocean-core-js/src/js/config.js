6c1264984945e95147b325e7ad6e4c619ffced7f
Keith Zantow <kzantow@gmail.com>
2017-08-09 03:42:28 -0400

    Don't be dumb

--- blueocean-core-js/src/js/config.js
+++ blueocean-core-js/src/js/config.js
@@ -12,7 +12,7 @@ const pfx = 'features=';
 const pfxlen = pfx.length;
 (window.location.href.split('?')[1] || '').split('&')
         .map(p => p.startsWith(pfx)
-            && (p.substring(pfxlen).split(',').map(f => features[f] = true && 'nice')));
+            && (p.substring(pfxlen).split(',').forEach(f => features[f] = true)));
 
 export default {
     loadUrls() {
