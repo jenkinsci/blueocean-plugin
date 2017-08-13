affcc67d412fbb7c0c3d1a0ea1768e0bdc042d50
Ivan Meredith <ivan@ivan.net.nz>
2017-08-14 11:14:56 +1200

    JENKINS-44761: Fastsearch ATH coverage

--- acceptance-tests/src/test/java/io/blueocean/ath/offline/dashboard/search/SearchTest.java
+++ acceptance-tests/src/test/java/io/blueocean/ath/offline/dashboard/search/SearchTest.java
@@ -1,8 +1,10 @@
 package io.blueocean.ath.offline.dashboard.search;
 
 import com.mashape.unirest.http.exceptions.UnirestException;
+import com.offbytwo.jenkins.model.FolderJob;
 import io.blueocean.ath.ATHJUnitRunner;
 import io.blueocean.ath.api.classic.ClassicJobApi;
+import io.blueocean.ath.model.Folder;
 import io.blueocean.ath.pages.blue.DashboardPage;
 import org.apache.log4j.Logger;
 import org.junit.Test;
@@ -23,16 +25,46 @@ public class SearchTest {
 
     @Test
     public void testSearch() throws InterruptedException, UnirestException, IOException {
+        int totalJobs = 4;
         String alpha = "search-test-freestyle-alpha";
         String bravo = "search-test-freestyle-bravo";
         jobApi.createFreeStyleJob(alpha, "echo alpha");
         jobApi.createFreeStyleJob(bravo, "echo bravo");
+        FolderJob jobFolder = jobApi.getFolder(Folder.folders("folder1", "folder2"), true);
+        jobApi.createFreeStyleJob(jobFolder,"zz", "echo zz");
+        jobApi.createFreeStyleJob(jobFolder,"zx", "echo zx");
 
         dashboardPage.open();
         dashboardPage.enterSearchText(alpha);
         dashboardPage.testJobCountEqualTo(1);
         dashboardPage.findJob(alpha);
         dashboardPage.clearSearchText();
-        dashboardPage.testJobCountAtLeast(2);
+        dashboardPage.testJobCountEqualTo(4);
+        dashboardPage.enterSearchText("folder1/");
+        dashboardPage.testJobCountEqualTo(2);
+        dashboardPage.clearSearchText();
+        dashboardPage.testJobCountEqualTo(totalJobs);
+
+        dashboardPage.enterSearchText("f*/z*");
+        dashboardPage.testJobCountEqualTo(2);
+        dashboardPage.clearSearchText();
+        dashboardPage.testJobCountEqualTo(totalJobs);
+
+
+        dashboardPage.enterSearchText("z*");
+        dashboardPage.testJobCountEqualTo(2);
+        dashboardPage.clearSearchText();
+        dashboardPage.testJobCountEqualTo(totalJobs);
+
+        dashboardPage.enterSearchText("zz*");
+        dashboardPage.testJobCountEqualTo(1);
+        dashboardPage.clearSearchText();
+        dashboardPage.testJobCountEqualTo(totalJobs);
+
+        dashboardPage.enterSearchText("*freestyle*");
+        dashboardPage.testJobCountEqualTo(2);
+        dashboardPage.clearSearchText();
+        dashboardPage.testJobCountEqualTo(totalJobs);
+
     }
 }
