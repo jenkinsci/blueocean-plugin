f87ba06d5cae5975020ce86a4a950d8e3030b329
Keith Zantow <kzantow@gmail.com>
2017-08-14 04:19:06 -0400

    Typo fun

--- blueocean-git-pipeline/src/main/java/io/jenkins/blueocean/blueocean_git_pipeline/GitUtils.java
+++ blueocean-git-pipeline/src/main/java/io/jenkins/blueocean/blueocean_git_pipeline/GitUtils.java
@@ -128,7 +128,7 @@ class GitUtils {
         final SshSessionFactory sshSessionFactory = new JschConfigSessionFactory() {
             @Override
             protected void configure(OpenSshConfig.Host hc, com.jcraft.jsch.Session session) {
-                session.setConfig("StrictHostKeyChecking", "no"); // jenkins user doesn't likely have the hos key
+                session.setConfig("StrictHostKeyChecking", "no"); // jenkins user doesn't likely have the host key
             }
 
             @Override
