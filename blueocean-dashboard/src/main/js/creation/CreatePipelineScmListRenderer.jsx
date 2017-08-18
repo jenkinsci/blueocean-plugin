e6e1a9cf616d36af41d174147f87e337d4c8855d
Cliff Meyers <cmeyers@cloudbees.com>
2017-08-18 12:43:00 -0400

    sort the scm providers via ordinal

--- blueocean-dashboard/src/main/js/creation/CreatePipelineScmListRenderer.jsx
+++ blueocean-dashboard/src/main/js/creation/CreatePipelineScmListRenderer.jsx
@@ -5,6 +5,7 @@ import React, { PropTypes } from 'react';
 import Extensions from '@jenkins-cd/js-extensions';
 
 const Sandbox = Extensions.SandboxedComponent;
+const { sortByOrdinal } = Extensions.Utils;
 
 /**
  * Displays the initial set of options to begin a creation flow from a SCM Provider.
@@ -23,23 +24,9 @@ export class CreatePipelineScmListRenderer extends React.Component {
         this._initialize();
     }
 
-    customSortProviders(providers, sortedProviders, providerToSearch) {
-        const providerKeys = Object.keys(providers);
-
-        for (let i = 0; i <= providerKeys.length; i++) {
-            if (providers[i] && ((providers[i].constructor.name === providerToSearch) || !providerToSearch)) {
-                sortedProviders.push(providers[i]);
-
-                // eslint-disable-next-line
-                delete providers[i];
-                break;
-            }
-        }
-    }
-
     _initialize() {
         // load and store the SCM providers that contributed the specified extension point
-        Extensions.store.getExtensions(this.props.extensionPoint, (extensions) => {
+        Extensions.store.getExtensions(this.props.extensionPoint, sortByOrdinal, (extensions) => {
             let providers = extensions.map(Provider => {
                 try {
                     return new Provider();
@@ -51,18 +38,8 @@ export class CreatePipelineScmListRenderer extends React.Component {
 
             providers = providers.filter(provider => !!provider);
 
-            // eslint-disable-next-line
-            let sortedProviders = [];
-
-            this.customSortProviders(providers, sortedProviders, 'BbCloudScmProvider');
-            this.customSortProviders(providers, sortedProviders, 'BbServerScmProvider');
-            this.customSortProviders(providers, sortedProviders, 'GithubScmProvider');
-            this.customSortProviders(providers, sortedProviders, 'GithubEnterpriseScmProvider');
-            this.customSortProviders(providers, sortedProviders, 'GitScmProvider');
-            this.customSortProviders(providers, sortedProviders); // add all other providers
-
             this.setState({
-                providers: sortedProviders,
+                providers,
             });
         });
     }
