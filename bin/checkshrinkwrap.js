b9c0b636f96a1e6006131c3690e83ec79e823db3
Nicolae Pascu <contact@nicupascu.ro>
2017-08-23 16:04:19 +1000

    add the pipeline editor to the cleanInstall.js and checkshrinkwrap.js scripts

--- bin/checkshrinkwrap.js
+++ bin/checkshrinkwrap.js
@@ -28,6 +28,7 @@ checkProject('../blueocean-personalization');
 checkProject('../blueocean-web');
 checkProject('../blueocean-config');
 checkProject('../blueocean-core-js');
+checkProject('../blueocean-pipeline-editor');
 
 const ellapsed = new Date().getTime() - start;
 console.log(`all dependencies look good! took ${ellapsed}ms`);
