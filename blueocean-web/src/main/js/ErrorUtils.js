3ebe72c7175fd2c87f40aee4b20552319c3d8c37
Cliff Meyers <cmeyers@cloudbees.com>
2017-09-05 15:15:53 -0400

    log a message to the browser console about bogus HTTP errors for Chrome and Firefox

--- blueocean-web/src/main/js/ErrorUtils.js
+++ blueocean-web/src/main/js/ErrorUtils.js
@@ -1,8 +1,28 @@
+import { i18nTranslator } from '@jenkins-cd/blueocean-core-js';
+
+
 let initialized = false;
+let warningLogged = false;
+
+function isGoogleChrome() {
+    const isChromium = window.chrome,
+        winNav = window.navigator,
+        vendorName = winNav.vendor,
+        isOpera = winNav.userAgent.indexOf("OPR") > -1,
+        isIEedge = winNav.userAgent.indexOf("Edge") > -1,
+        isIOSChrome = winNav.userAgent.match("CriOS");
+
+    return isIOSChrome ||
+        (isChromium !== null && typeof isChromium !== "undefined" && vendorName === "Google Inc." &&  !isOpera && !isIEedge);
+}
+
+function isFirefox() {
+    return window.navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
+}
 
 function logApplicationError(messageOrEvent) {
     const message = messageOrEvent.error || messageOrEvent;
-    console.error('unhandled error: ', message);
+    console.error('Hnhandled Error: ', message);
 
     if (messageOrEvent.preventDefault) {
         messageOrEvent.preventDefault();
@@ -13,7 +33,7 @@ function logUnhandledPromiseRejection(errorEvent) {
     const { reason } = errorEvent.detail || errorEvent;
 
     if (reason) {
-        console.error('unhandled rejection: ', reason);
+        console.error('Unhandled Rejection: ', reason);
         errorEvent.preventDefault();
     }
     // otherwise we'll fall back to the default rejection handler
@@ -27,6 +47,21 @@ function initializeErrorHandling() {
     }
 }
 
+function logBrowserConsoleWarning() {
+    if (!warningLogged) {
+        const translate = i18nTranslator('blueocean-web');
+
+        if (isGoogleChrome()) {
+            console.log(translate('common.logging.warning.chrome'));
+        } else if (isFirefox()) {
+            console.log(translate('common.logging.warning.firefox'));
+        }
+
+        warningLogged = true;
+    }
+}
+
 export default {
     initializeErrorHandling,
+    logBrowserConsoleWarning,
 };
