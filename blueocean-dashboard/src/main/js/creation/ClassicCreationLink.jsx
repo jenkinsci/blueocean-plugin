22f6cd30197a967f5327a19b728ade65a87f8b9d
Thorsten Scherler <scherler@gmail.com>
2017-09-12 15:26:46 +0200

    [JENKINS-45475_go_classic] Fix merge gone wrong

--- blueocean-dashboard/src/main/js/creation/ClassicCreationLink.jsx
+++ blueocean-dashboard/src/main/js/creation/ClassicCreationLink.jsx
@@ -1,12 +1,7 @@
 import React from 'react';
 
-<<<<<<< HEAD
 import { i18nTranslator, buildClassicCreateJobUrl } from '@jenkins-cd/blueocean-core-js';
-import { Icon } from '@jenkins-cd/react-material-icons';
-=======
-import { i18nTranslator, UrlConfig } from '@jenkins-cd/blueocean-core-js';
 import { Icon } from '@jenkins-cd/design-language';
->>>>>>> upstream/master
 
 const t = i18nTranslator('blueocean-dashboard');
 
