47d83e71913429428dd7d92da84779588d8ce21c
Nicolae Pascu <contact@nicupascu.ro>
2017-09-15 17:49:28 +1000

    add another test for LinkifiedText when 2 partial links are added to the text

--- blueocean-dashboard/src/test/js/components/LinkifiedText-spec.js
+++ blueocean-dashboard/src/test/js/components/LinkifiedText-spec.js
@@ -59,4 +59,26 @@ describe("LinkifiedText", () => {
         assert.equal(wrapper.find('a').prop('href'), 'https://issue.url');
         assert.equal(wrapper.find('Link').first().prop('to'), 'link');
     });
+
+    it("renders linked text with 2 linked issues in text", () => {
+        const mockPartialTextLinks = [
+            {
+                'id': 'JIRA-XXXX',
+                'url': 'https://issue.url'
+            },
+            {
+                'id': 'Link-XXXX',
+                'url': 'https://link.url'
+            }
+        ];
+        const wrapper = shallow(<LinkifiedText text="test JIRA-XXXX text Link-XXXX test" textLink="link" partialTextLinks={mockPartialTextLinks} />);
+
+        assert.equal(wrapper.find('a').first().text(), 'JIRA-XXXX');
+        assert.equal(wrapper.find('a').first().prop('href'), 'https://issue.url');
+
+        assert.equal(wrapper.find('a').at(1).text(), 'Link-XXXX');
+        assert.equal(wrapper.find('a').at(1).prop('href'), 'https://link.url');
+
+        assert.equal(wrapper.find('Link').first().prop('to'), 'link');
+    });
 });
