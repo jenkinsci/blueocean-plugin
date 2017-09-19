1998a06001a97d9a39e7e94351ee4818f87824de
Cliff Meyers <cliff.meyers@gmail.com>
2017-09-19 18:36:03 -0400

    Task/jenkins 42283 fetch promise error handlers (#1363)
    * add unit tests for fetch using nock
    
    * bring back the logic that prefixes jenkins path onto a URL, unless it's an absolute URL (required for fetch-spec with nock)
    
    * use bluebird promises in core-js Fetch; improve error message for non 2xx responses
    
    * add default logging for uncaught errors and unhandled Promise rejections
    
    * swap out es6-promise for bluebird in remainder of core-js
    
    * release beta core-js
    
    * use beta core-js
    
    * port dashboard from es6-promise to bluebird
    
    * rename Errors -> ErrorUtils
    
    * log a message to the browser console about bogus HTTP errors for Chrome and Firefox
    
    * install beta blueocean-core-js in editor project
    
    * fix shrinkwrap inconsistency in blueocean-core-js, republish beta
    
    * use new beta core-js in all modules
    
    * release blueocean-core-js 0.0.160
    
    * tick core-js version
    
    * use prod release of blueocean-core-js

--- blueocean-web/src/main/js/init.js
+++ blueocean-web/src/main/js/init.js
@@ -1,5 +1,11 @@
+// this function is executed before all other code during bundle's onStartup
+// see gulpfile, builder.bundle().onStartup()
+
+import ErrorUtils from './ErrorUtils';
 
 export function execute(done, bundleConfig) {
+    ErrorUtils.initializeErrorHandling();
+
     // Get the extension list metadata from Jenkins.
     // Might want to do some flux fancy-pants stuff for this.
     const appRoot = document.getElementsByTagName("head")[0].getAttribute("data-appurl");
