04b872f9ac608c00af4aac57f040d0a8417d01aa
Michael Neale <michael.neale@gmail.com>
2017-09-26 18:30:04 +1000

    Disable JWT for ssh test (#1424)

--- blueocean-commons/src/main/java/io/jenkins/blueocean/commons/BlueOceanConfigProperties.java
+++ blueocean-commons/src/main/java/io/jenkins/blueocean/commons/BlueOceanConfigProperties.java
@@ -10,6 +10,6 @@ public class BlueOceanConfigProperties {
     public static final String BLUEOCEAN_ROLLBAR_ENABLED_PROPERTY = "BLUEOCEAN_ROLLBAR_ENABLED";
 
     public static final boolean ROLLBAR_ENABLED = Boolean.getBoolean(BLUEOCEAN_ROLLBAR_ENABLED_PROPERTY);
-    public static final boolean BLUEOCEAN_FEATURE_JWT_AUTHENTICATION = Boolean.getBoolean(BLUEOCEAN_FEATURE_JWT_AUTHENTICATION_PROPERTY);
+    public static boolean BLUEOCEAN_FEATURE_JWT_AUTHENTICATION = Boolean.getBoolean(BLUEOCEAN_FEATURE_JWT_AUTHENTICATION_PROPERTY);
 
 }
