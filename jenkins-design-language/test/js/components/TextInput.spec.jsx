75764af2b3cb446369e87df563da8d9029401720
Thorsten Scherler <tscherler@cloudbees.com>
2017-09-27 09:21:22 -0700

    [FIX JENKINS-47148] Add a11y support for textInput (#1428)
    * [a11ySupportTextinput] Add a11y support for textInput
    
    * [a11ySupportTextinput] Rename property as asked by @sophistifunk

---
+++ jenkins-design-language/test/js/components/TextInput.spec.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { assert } from 'chai';
+import { mount } from 'enzyme';
+
+import { TextInput } from '../../../src/js/components';
+
+
+describe("TextInput", () => {
+    it('TextInput should render ok but with undefined props.', () => {
+        const wrapper = mount(<TextInput />);
+        assert.ok(wrapper);
+        assert.equal(wrapper.find('input').props().placeholder, undefined);
+        assert.equal(wrapper.find('input').props()['aria-label'], undefined);
+    });
+    it('TextInput should render ok with placeholder.', () => {
+        const wrapper = mount(<TextInput placeholder="placeholder"/>);
+        assert.ok(wrapper);
+        assert.equal(wrapper.find('input').props().placeholder, "placeholder");
+    });
+    it('TextInput should render aria props.', () => {
+        const wrapper = mount(<TextInput aria-label="placeholder"/>);
+        assert.ok(wrapper);
+        assert.equal(wrapper.find('input').props()['aria-label'], "placeholder");
+    });
+    it('TextInput should render aria props with placeholder value as fallback.', () => {
+        const wrapper = mount(<TextInput placeholder="placeholder"/>);
+        assert.ok(wrapper);
+        assert.equal(wrapper.find('input').props()['aria-label'], "placeholder");
+    });
+});
