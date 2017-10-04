d63d8cf1fd5f7bf59f80034f0d76c55237bd2480
Nicolae Pascu <contact@nicupascu.ro>
2017-10-04 20:37:22 +1100

    fix icon import

--- blueocean-dashboard/src/main/js/components/karaoke/components/LogToolbar.jsx
+++ blueocean-dashboard/src/main/js/components/karaoke/components/LogToolbar.jsx
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from 'react';
-import { Icon } from '@jenkins-cd/react-material-icons';
+import { Icon } from '@jenkins-cd/design-language';
 import { fetchAllSuffix as suffix } from '../../../util/UrlUtils';
 
 import moment from 'moment';
