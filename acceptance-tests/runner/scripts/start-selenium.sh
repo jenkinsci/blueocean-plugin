4b4edd86bf0ac9b4c497395c59d4c0a411b80c76
Cliff Meyers <cmeyers@cloudbees.com>
2017-10-06 16:47:39 -0400

    Revert "use chrome instead of firefox and enable console logs integration"
    This reverts commit 7ace9debd3aca4788e94df630afe580aa98969ef.

--- acceptance-tests/runner/scripts/start-selenium.sh
+++ acceptance-tests/runner/scripts/start-selenium.sh
@@ -6,7 +6,7 @@ $SCRIPT_DIR/stop-selenium.sh
 echo ""
 echo " Starting Selenium Docker container..."
 echo ""
-docker run -d --name blueo-selenium -p 15900:5900 -p 4444:4444 -e no_proxy=localhost selenium/standalone-chrome-debug:2.53.0 > /dev/null
+docker run -d --name blueo-selenium -p 15900:5900 -p 4444:4444 -e no_proxy=localhost selenium/standalone-firefox-debug:2.53.0 > /dev/null
 
 # Output the containers bridge network IP to file
 SELENIUM_IP=`docker inspect -f '{{ .NetworkSettings.IPAddress }}' blueo-selenium`
