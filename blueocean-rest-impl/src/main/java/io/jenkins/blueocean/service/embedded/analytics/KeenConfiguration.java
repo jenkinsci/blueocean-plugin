79548a14b6f69635aef255970aa2539a1b669a39
James Dumay <jdumay@cloudbees.com>
2017-10-25 09:07:38 +1100

    Make keen keys innner private variables

--- blueocean-rest-impl/src/main/java/io/jenkins/blueocean/service/embedded/analytics/KeenConfiguration.java
+++ blueocean-rest-impl/src/main/java/io/jenkins/blueocean/service/embedded/analytics/KeenConfiguration.java
@@ -33,14 +33,18 @@ public abstract class KeenConfiguration implements ExtensionPoint {
 
     @Extension(ordinal = -1)
     public static class DefaultKeenConfiguration extends KeenConfiguration {
+
+        private static final String PROJECT_ID = "5950934f3d5e150f5ab9d7be";
+        private static final String WRITE_KEY = "E6C1FA3407AF4DD3115DBC186E40E9183A90069B1D8BBA78DB3EA6B15EA6182C881E8C55B4D7A48F55D5610AD46F36E65093227A7490BF7A56307047903BCCB16D05B9456F18A66849048F100571FDC91888CAD94F2A271A8B9E5342D2B9404E";
+
         @Override
         public String getProjectId() {
-            return "5950934f3d5e150f5ab9d7be";
+            return PROJECT_ID;
         }
 
         @Override
         public String getWriteKey() {
-            return "E6C1FA3407AF4DD3115DBC186E40E9183A90069B1D8BBA78DB3EA6B15EA6182C881E8C55B4D7A48F55D5610AD46F36E65093227A7490BF7A56307047903BCCB16D05B9456F18A66849048F100571FDC91888CAD94F2A271A8B9E5342D2B9404E";
+            return WRITE_KEY;
         }
     }
 }
