e31f5ca934c308a5d899fa5ed7ba82e4eee3fa1d
Cliff Meyers <cmeyers@cloudbees.com>
2017-11-10 12:08:57 -0500

    avoid nesting of AcceptanceTestException by unwrapping the cause

--- acceptance-tests/src/main/java/io/blueocean/ath/AcceptanceTestException.java
+++ acceptance-tests/src/main/java/io/blueocean/ath/AcceptanceTestException.java
@@ -1,7 +1,15 @@
 package io.blueocean.ath;
 
 public class AcceptanceTestException extends RuntimeException {
+
     public AcceptanceTestException(String message, Throwable cause) {
-        super(message, cause);
+        super(message);
+
+        if (cause instanceof AcceptanceTestException) {
+            initCause(cause.getCause());
+        } else {
+            initCause(cause);
+        }
     }
+
 }
