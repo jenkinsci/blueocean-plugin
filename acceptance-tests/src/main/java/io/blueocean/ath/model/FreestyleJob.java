f822f70897a057183b1a9b189a04b97122133d98
Cliff Meyers <cmeyers@cloudbees.com>
2017-11-14 15:44:04 -0500

    add "create" method to FreestyleJob

--- acceptance-tests/src/main/java/io/blueocean/ath/model/FreestyleJob.java
+++ acceptance-tests/src/main/java/io/blueocean/ath/model/FreestyleJob.java
@@ -1,9 +1,16 @@
 package io.blueocean.ath.model;
 
+import com.google.inject.Inject;
 import com.google.inject.assistedinject.Assisted;
 import com.google.inject.assistedinject.AssistedInject;
+import io.blueocean.ath.api.classic.ClassicJobApi;
+
+import java.io.IOException;
 
 public class FreestyleJob extends AbstractPipeline {
+    @Inject
+    ClassicJobApi jobApi;
+
     @AssistedInject
     public FreestyleJob(@Assisted String name) {
         super(name);
@@ -12,5 +19,10 @@ public class FreestyleJob extends AbstractPipeline {
     @AssistedInject
     public FreestyleJob(@Assisted Folder folder, @Assisted String name) {
         super(folder, name);
+    }
+
+    public FreestyleJob create(String command) throws IOException {
+        jobApi.createFreeStyleJob(jobApi.getFolder(getFolder(), true), getName(), command);
+        return this;
     }
 }
