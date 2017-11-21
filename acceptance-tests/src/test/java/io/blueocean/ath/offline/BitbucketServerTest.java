c145fe63361b3ce9809bf49c7aad6e21822930ec
Cliff Meyers <cmeyers@cloudbees.com>
2017-11-21 11:42:04 -0500

    ensure the job created by Bitbucket creation ATH is deleted beforehand

--- acceptance-tests/src/test/java/io/blueocean/ath/offline/BitbucketServerTest.java
+++ acceptance-tests/src/test/java/io/blueocean/ath/offline/BitbucketServerTest.java
@@ -6,10 +6,10 @@ import com.google.inject.Inject;
 import io.blueocean.ath.ATHJUnitRunner;
 import io.blueocean.ath.BaseUrl;
 import io.blueocean.ath.CustomJenkinsServer;
-import io.blueocean.ath.Locate;
 import io.blueocean.ath.Login;
 import io.blueocean.ath.WaitUtil;
 import io.blueocean.ath.WebDriverMixin;
+import io.blueocean.ath.api.classic.ClassicJobApi;
 import io.blueocean.ath.pages.blue.DashboardPage;
 import io.blueocean.ath.pages.blue.GithubCreationPage;
 import io.jenkins.blueocean.util.HttpRequest;
@@ -28,6 +28,9 @@ public class BitbucketServerTest implements WebDriverMixin {
 
     private static Logger LOGGER = Logger.getLogger(BitbucketServerTest.class);
     private static final String ENDPOINT_URL = "http://127.0.0.1:7990";
+    private static final String BB_PROJECT_KEY = "BLUE";
+    private static final String BB_PROJECT_NAME = "BlueOcean";
+    private static final String BB_REPO_NAME = "bitbucket-no-jenkinsfile";
 
     @Inject
     WaitUtil wait;
@@ -44,10 +47,14 @@ public class BitbucketServerTest implements WebDriverMixin {
     @Inject
     CustomJenkinsServer jenkins;
 
+    @Inject
+    ClassicJobApi jobApi;
+
     @Before
     public void setUp() throws IOException {
         cleanupEndpoint(ENDPOINT_URL);
         cleanupCredentials(ENDPOINT_URL);
+        jobApi.deletePipeline(BB_REPO_NAME);
     }
 
     @Test
@@ -56,10 +63,8 @@ public class BitbucketServerTest implements WebDriverMixin {
             .endPoint(ENDPOINT_URL)
             .credentials("admin:admin").build();
 
-        String project = "BLUE";
-        String repo = "testJenkinsfileCreate";
-        client.api().repositoryApi().delete(project, repo);
-        client.api().repositoryApi().create(project, CreateRepository.create(repo, true));
+        client.api().repositoryApi().delete(BB_PROJECT_KEY, BB_REPO_NAME);
+        client.api().repositoryApi().create(BB_PROJECT_KEY, CreateRepository.create(BB_REPO_NAME, true));
 
         dashboardPage.clickNewPipelineBtn();
 
@@ -79,8 +84,8 @@ public class BitbucketServerTest implements WebDriverMixin {
         wait.until(By.cssSelector(".text-password input")).sendKeys("admin");
         click(".button-create-credental");
 
-        creationPage.selectOrganization("BlueOcean");
-        creationPage.selectPipelineToCreate("testjenkinsfilecreate");
+        creationPage.selectOrganization(BB_PROJECT_NAME);
+        creationPage.selectPipelineToCreate(BB_REPO_NAME);
         creationPage.clickCreatePipelineButton();
     }
 
