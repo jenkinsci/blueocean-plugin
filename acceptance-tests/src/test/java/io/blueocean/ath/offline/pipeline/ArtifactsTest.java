013a7a13e0d2476a01800628efa981ce9c4037a0
Ivan Meredith <ivan@ivan.net.nz>
2018-01-29 11:23:10 +1300

    [JENKINS-45037] Remove download link if there are no artifacts (#1615)
    * [JENKINS-45037] Remove download link if there are no artifacts
    
    * Add ATH test

--- acceptance-tests/src/test/java/io/blueocean/ath/offline/pipeline/ArtifactsTest.java
+++ acceptance-tests/src/test/java/io/blueocean/ath/offline/pipeline/ArtifactsTest.java
@@ -5,7 +5,9 @@ import io.blueocean.ath.ATHJUnitRunner;
 import io.blueocean.ath.BlueOceanAcceptanceTest;
 import io.blueocean.ath.WaitUtil;
 import io.blueocean.ath.factory.ClassicPipelineFactory;
+import io.blueocean.ath.factory.FreestyleJobFactory;
 import io.blueocean.ath.model.ClassicPipeline;
+import io.blueocean.ath.model.FreestyleJob;
 import io.blueocean.ath.sse.SSEClientRule;
 import io.blueocean.ath.sse.SSEEvents;
 import org.apache.log4j.Logger;
@@ -13,6 +15,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.io.IOException;
 
@@ -21,6 +24,9 @@ public class ArtifactsTest extends BlueOceanAcceptanceTest {
     private Logger logger = Logger.getLogger(this.getClass());
     @Inject
     ClassicPipelineFactory pipelineFactory;
+
+    @Inject
+    FreestyleJobFactory freestyleJobFactory;
 
     @Rule
     @Inject
@@ -40,6 +46,16 @@ public class ArtifactsTest extends BlueOceanAcceptanceTest {
         pipeline.getRunDetailsArtifactsPage().open(1);
 
         wait.until(By.className("artifacts-info"));
+        wait.until(By.cssSelector("a[title='Download all artifact as zip'"));
         logger.info("Found artifacts table");
+    }
+
+    @Test
+    public void testNoArtifacts() throws IOException {
+        FreestyleJob testNoArtifacts = freestyleJobFactory.pipeline("testNoArtifacts").create("echo hi").build();
+
+        sseClientRule.untilEvents(SSEEvents.activityComplete("testNoArtifacts"));
+        testNoArtifacts.getRunDetailsArtifactsPage().open(1);
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector("a[title='Download all artifact as zip'")));
     }
 }
