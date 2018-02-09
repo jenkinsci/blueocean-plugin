4b82098484ae9c58e2bbab32e02984f5e222ec88
Nicolae Pascu <contact@nicupascu.ro>
2018-02-09 17:29:21 +1100

    remove erroneous title tag from the favourite/remove favourite icon

--- jenkins-design-language/src/js/components/favorite/Favorite.jsx
+++ jenkins-design-language/src/js/components/favorite/Favorite.jsx
@@ -50,7 +50,6 @@ function FavoriteStarSvg(checkedObj) {
             <svg className="star-icon" width="288" height="24" viewBox="0 0 288 24" xmlns="http://www.w3.org/2000/svg"
                 focusable="false"
             >
-                <title>stars for light background</title>
                 <g fill="none" fillRule="evenodd">
                     { /* this star is used for the default 'unchecked' state */ }
                     <path className="Favorite-fill star-empty" d="M22 9.24l-7.19-.62L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21 12 17.27 18.18 21l-1.63-7.03L22 9.24zM12 15.4l-3.76 2.27 1-4.28-3.32-2.88 4.38-.38L12 6.1l1.71 4.04 4.38.38-3.32 2.88 1 4.28L12 15.4z"/>
