1e05a5cf487cdb85112cbbb61d6ebf1f9cb889b4
Vivek Pandey <vivek.pandey@gmail.com>
2018-02-18 16:08:14 -0800

    JENKINS-48868# Dashboard page load performance fix (#1635)
    * JENKINS-48868# Dashboard page load performance fix
    
    Exclude expensive and extra information that UI doesn't need from
    response.
    
    * Return pipeline specific latest run instance
    
    * Latest run should be returned with correct capabilities
    
    * Add missing code coverage
    
    * Added missing replay property and some refactoring

--- blueocean-pipeline-api-impl/src/main/java/io/jenkins/blueocean/rest/impl/pipeline/PipelineImpl.java
+++ blueocean-pipeline-api-impl/src/main/java/io/jenkins/blueocean/rest/impl/pipeline/PipelineImpl.java
@@ -3,32 +3,48 @@ package io.jenkins.blueocean.rest.impl.pipeline;
 import hudson.Extension;
 import hudson.model.Item;
 import hudson.model.Job;
+import hudson.model.Run;
 import io.jenkins.blueocean.rest.Reachable;
 import io.jenkins.blueocean.rest.annotation.Capability;
 import io.jenkins.blueocean.rest.factory.BluePipelineFactory;
 import io.jenkins.blueocean.rest.factory.organization.OrganizationFactory;
 import io.jenkins.blueocean.rest.model.BlueOrganization;
 import io.jenkins.blueocean.rest.model.BluePipeline;
+import io.jenkins.blueocean.rest.model.BlueRun;
 import io.jenkins.blueocean.rest.model.BlueTrendContainer;
 import io.jenkins.blueocean.rest.model.Resource;
+import io.jenkins.blueocean.service.embedded.rest.AbstractBlueRunSummary;
 import io.jenkins.blueocean.service.embedded.rest.AbstractPipelineImpl;
 import io.jenkins.blueocean.service.embedded.rest.BlueTrendContainerImpl;
 import org.jenkinsci.plugins.workflow.job.WorkflowJob;
-
+import org.jenkinsci.plugins.workflow.job.WorkflowRun;
 import static io.jenkins.blueocean.rest.model.KnownCapabilities.JENKINS_WORKFLOW_JOB;
+import static io.jenkins.blueocean.rest.model.KnownCapabilities.JENKINS_WORKFLOW_RUN;
 
 /**
  * @author Kohsuke Kawaguchi
  */
 @Capability({JENKINS_WORKFLOW_JOB})
 public class PipelineImpl extends AbstractPipelineImpl {
+    private final Job job;
     protected PipelineImpl(BlueOrganization organization, Job job) {
         super(organization, job);
+        this.job = job;
     }
 
     @Override
     public BlueTrendContainer getTrends() {
         return new BlueTrendContainerImpl(this);
+    }
+
+    @Override
+    public BlueRun getLatestRun() {
+        Run run = job.getLastBuild();
+        if(run instanceof WorkflowRun){
+            BlueRun blueRun = new PipelineRunImpl((WorkflowRun) run, this, organization);
+            return new PipelineRunSummary(blueRun, run, this, organization);
+        }
+        return super.getLatestRun();
     }
 
     @Extension(ordinal = 1)
@@ -50,6 +66,13 @@ public class PipelineImpl extends AbstractPipelineImpl {
             }
 
             return null;
+        }
+    }
+
+    @Capability(JENKINS_WORKFLOW_RUN)
+    static class PipelineRunSummary extends AbstractBlueRunSummary {
+        public PipelineRunSummary(BlueRun blueRun, Run run, Reachable parent, BlueOrganization organization) {
+            super(blueRun, run, parent, organization);
         }
     }
 }
--- blueocean-rest-impl/src/main/java/io/jenkins/blueocean/service/embedded/rest/FreeStylePipeline.java
+++ blueocean-rest-impl/src/main/java/io/jenkins/blueocean/service/embedded/rest/FreeStylePipeline.java
@@ -1,23 +1,46 @@
 package io.jenkins.blueocean.service.embedded.rest;
 
 import hudson.Extension;
+import hudson.model.FreeStyleBuild;
 import hudson.model.FreeStyleProject;
 import hudson.model.Item;
 import hudson.model.Job;
+import hudson.model.Run;
 import io.jenkins.blueocean.rest.Reachable;
 import io.jenkins.blueocean.rest.annotation.Capability;
 import io.jenkins.blueocean.rest.factory.BluePipelineFactory;
 import io.jenkins.blueocean.rest.model.BlueOrganization;
 import io.jenkins.blueocean.rest.model.BluePipeline;
+import io.jenkins.blueocean.rest.model.BlueRun;
 import io.jenkins.blueocean.rest.model.Resource;
-
 import static io.jenkins.blueocean.rest.model.KnownCapabilities.JENKINS_FREESTYLE_PROJECT;
+import static io.jenkins.blueocean.rest.model.KnownCapabilities.JENKINS_FREE_STYLE_BUILD;
 
 @Capability(JENKINS_FREESTYLE_PROJECT)
 public class FreeStylePipeline extends AbstractPipelineImpl {
+    private final Job job;
     private FreeStylePipeline(BlueOrganization organization, Job job) {
         super(organization, job);
+        this.job = job;
     }
+
+    @Override
+    public BlueRun getLatestRun() {
+        Run run = job.getLastBuild();
+        if(run instanceof FreeStyleBuild){
+            BlueRun blueRun = new FreeStyleRunImpl((FreeStyleBuild) run, this, organization);
+            return new FreeStyleRunSummary(blueRun, run, this, organization);
+        }
+        return super.getLatestRun();
+    }
+
+    @Capability(JENKINS_FREE_STYLE_BUILD)
+    static class FreeStyleRunSummary extends AbstractBlueRunSummary{
+        public FreeStyleRunSummary(BlueRun blueRun, Run run, Reachable parent, BlueOrganization organization) {
+            super(blueRun, run, parent, organization);
+        }
+    }
+
 
     @Extension(ordinal = 1)
     public static class FactoryImpl extends BluePipelineFactory {
