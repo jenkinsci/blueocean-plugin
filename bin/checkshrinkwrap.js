1280ec0fe4ddaa3d4121a2060cfb1f8a70771ea0
Josh Soref <jsoref@users.noreply.github.com>
2018-02-19 20:59:42 +0000

    spelling: elapsed

--- bin/checkshrinkwrap.js
+++ bin/checkshrinkwrap.js
@@ -31,8 +31,8 @@ checkProject('../blueocean-core-js');
 checkProject('../blueocean-pipeline-editor');
 checkProject('../jenkins-design-language');
 
-const ellapsed = new Date().getTime() - start;
-console.log(`all dependencies look good! took ${ellapsed}ms`);
+const elapsed = new Date().getTime() - start;
+console.log(`all dependencies look good! took ${elapsed}ms`);
 // done!
 
 function checkProject(pathToProject) {
