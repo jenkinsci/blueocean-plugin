622013d1585f24eb2c25d6c1ebf3936b403f7089
Josh Soref <jsoref@users.noreply.github.com>
2018-02-19 21:07:00 +0000

    spelling: focus

--- blueocean-dashboard/src/main/js/util/logDisplayHelper.js
+++ blueocean-dashboard/src/main/js/util/logDisplayHelper.js
@@ -96,7 +96,7 @@ export const getNodesInformation = (nodes) => {
         }
         return modelItem;
     });
-    // in case we have all null we will focuse the first node since we assume that this would
+    // in case we have all null we will focus the first node since we assume that this would
     // be the next node to be started
     if (queuedNodes.length === nodes.length && !wasFocused && model[0]) {
         model[0].isFocused = true;
