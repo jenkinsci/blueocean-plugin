1a7d7d207bb8df066d47b73de26661bbe049cbda
Josh Soref <jsoref@users.noreply.github.com>
2018-02-19 20:53:47 +0000

    spelling: credential

--- blueocean-pipeline-api-impl/src/main/java/io/jenkins/blueocean/rest/impl/pipeline/credential/CredentialApi.java
+++ blueocean-pipeline-api-impl/src/main/java/io/jenkins/blueocean/rest/impl/pipeline/credential/CredentialApi.java
@@ -171,9 +171,9 @@ public class CredentialApi extends Resource {
 
         @Override
         public Credential get(String name) {
-            CredentialsStoreAction.CredentialsWrapper credetialsWrapper = domainWrapper.getCredential(name);
-            if(credetialsWrapper != null) {
-                return new Credential(credetialsWrapper, self);
+            CredentialsStoreAction.CredentialsWrapper credentialsWrapper = domainWrapper.getCredential(name);
+            if(credentialsWrapper != null) {
+                return new Credential(credentialsWrapper, self);
             }
             throw new ServiceException.NotFoundException(String.format("Credential %s not found in domain %s", name,
                     domainWrapper.getFullName()));
