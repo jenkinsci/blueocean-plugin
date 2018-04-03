0c8267d4995908448b28bf20480588ec882c9095
Josh McDonald <josh@joshmcdonald.info>
2018-04-03 13:10:49 +1000

    task/cleanup-url-utils * Good progress, tests green

--- blueocean-core-js/src/js/UrlBuilder.js
+++ blueocean-core-js/src/js/UrlBuilder.js
@@ -40,29 +40,24 @@ const trimEmptyTokens = tokens => {
     return copy;
 };
 
-/**
- * Builds the proper URL to view Run Details for the specified run.
- * Run is either a run object with "_links.self.href" property, or the URL itself.
- *
- * @param {object|string} run
- * @returns {string}
- *
- * FIXME-JM: Unify this with buildRunDetailsUrl in blueocean-core-js/src/js/utils/UrlUtils.js - this is madness.
- */
-export const buildRunDetailsUrl = run => {
-    const restUrl = extractRestUrl(run);
+// TODO: TS
+
+// TODO: Docs or remove
+function parseRestRunUrl(restUrl) {
+    // TODO: If keeping this, replace it with a regexp or a more sensible token handler, once we have tests that take into account the variable tokens length
+
     const tokens = trimEmptyTokens(restUrl.split('/'));
 
     // given the following URL '/blue/rest/organizations/jenkins/pipelines/folder1/pipelines/folder2/pipelines/folder3/pipelines/jdl-2
     // /branches/experiment%252Fbuild-locally-docker/runs/21/
 
-    const organizationName = tokens[3];
+    const organizationName = decodeURIComponent(tokens[3]);
     const isMultiBranch = tokens[tokens.length - 4] === 'branches';
 
     const fullNameStart = 4;
-    const fullNameEnd = !isMultiBranch ? tokens.length - 2 : tokens.length - 4;
+    const fullNameEnd = isMultiBranch ? tokens.length - 4 : tokens.length - 2;
     // grab the tokens that make up the full name, then filter out the even values ('/pipelines')
-    // so the clean folder path is returned, e.g. folder1/folder2/folder3/jdl-2
+    // so the clean folder path is returned, e.g. f1/pipelines/f2/pipelines/f3/pipelines/foo => f1/f2/f3/foo
     const fullName = tokens
         .slice(fullNameStart, fullNameEnd)
         .filter((name, index) => index % 2 === 1)
@@ -72,16 +67,79 @@ export const buildRunDetailsUrl = run => {
     const branchName = isMultiBranch ? tokens[tokens.length - 3] : '';
     const runId = tokens[tokens.length - 1];
 
-    const detailName = isMultiBranch ? decodeURIComponent(branchName) : pipelineName;
+    const detailName = decodeURIComponent(isMultiBranch ? decodeURIComponent(branchName) : pipelineName);
+    // TODO: Look into why branch names are double-encoded, if I dare
 
     // fail fast
     if (!organizationName || !fullName || !detailName || !runId) {
         throw new Error('Could not extract URI components');
     }
 
-    return `/organizations/${organizationName}` + `/${encodeURIComponent(fullName)}/detail` + `/${detailName}/${runId}/pipeline`;
-};
+    // TODO: Create a type for this once in TS
+    return {
+        organizationName,
+        fullName,
+        detailName,
+        runId,
+    };
+}
 
-export default {
-    buildRunDetailsUrl,
-};
+// TODO: Docs - Run object to run details url
+export function badName001(runDetails) {
+    const restUrl = runDetails._links.self.href;
+
+    const { organizationName, fullName, detailName, runId } = parseRestRunUrl(restUrl);
+
+    return badName003(organizationName, fullName, detailName, runId);
+}
+
+// TODO: Docs - Run link to run details url
+export function badName002(restUrl) {
+    const { organizationName, fullName, detailName, runId } = parseRestRunUrl(restUrl);
+
+    return badName003(organizationName, fullName, detailName, runId);
+}
+
+// TODO: Rename, docs, check param names
+// TODO: Add some unit tests, ensuring the individual components are URLEncoded correctly
+function badName003(organizationName, pipelineFullName, branchOrPipelineName, runId, tabName = 'pipeline') {
+    //   return `/organizations/${organizationName}` + `/${encodeURIComponent(fullName)}/detail` + `/${detailName}/${runId}/pipeline`;
+
+    return (
+        `/organizations/${encodeURIComponent(organizationName)}` +
+        `/${encodeURIComponent(pipelineFullName)}` +
+        `/detail/${encodeURIComponent(branchOrPipelineName)}` +
+        `/${encodeURIComponent(runId)}` +
+        `/${tabName}`
+    );
+}
+
+// /**
+//  * Builds the proper URL to view Run Details for the specified run.
+//  * Run is either a run object with "_links.self.href" property, or the URL itself.
+//  *
+//  * @param {object|string} run
+//  * @returns {string}
+//  *
+//  * FIXME-JM: Unify this with buildRunDetailsUrl in blueocean-core-js/src/js/utils/UrlUtils.js - this is madness.
+//  */
+// export const buildRunDetailsUrl = run => {
+//
+//     // TODO: Remove this
+//     throw new Error("should not still be calling this");
+//
+//     const restUrl = extractRestUrl(run);
+//
+//     const {
+//         organizationName,
+//         fullName,
+//         detailName,
+//         runId
+//     } = parseRestRunUrl(restUrl);
+//
+//     return `/organizations/${organizationName}` + `/${encodeURIComponent(fullName)}/detail` + `/${detailName}/${runId}/pipeline`;
+// };
+//
+// export default {
+//     buildRunDetailsUrl,
+// };
--- blueocean-core-js/test/js/UrlBuilder-spec.js
+++ blueocean-core-js/test/js/UrlBuilder-spec.js
@@ -3,79 +3,65 @@
  */
 import { assert } from 'chai';
 
-import { buildRunDetailsUrl } from '../../src/js/UrlBuilder';
+import { badName001, badName002 } from '../../src/js/UrlBuilder';
 
-const createObjectFromLink = (url) => (
-    { _links: { self: { href: url } } }
-);
+const createObjectFromLink = url => ({ _links: { self: { href: url } } });
 
 describe('UrlBuilder', () => {
-    describe('buildRunDetailsUrl', () => {
+    describe('badName002', () => {
+        it('throws sensible error for bad url', () => {
+            assert.throws(() => badName002('/a/b/c/d'), 'Could not extract URI components');
+        });
+
+        it('handles a URL', () => {
+            const freestyle = '/blue/rest/organizations/jenkins/pipelines/freestyle-success-10m/runs/28/';
+            const url = badName002(freestyle);
+            assert.equal(url, '/organizations/jenkins/freestyle-success-10m/detail/freestyle-success-10m/28/pipeline');
+        });
+    });
+
+    describe('badName001', () => {
         describe('handling input', () => {
             it('handles an object', () => {
-                const freestyle = createObjectFromLink(
-                    '/blue/rest/organizations/jenkins/pipelines/freestyle-success-10m/runs/28/'
-                );
+                const freestyle = createObjectFromLink('/blue/rest/organizations/jenkins/pipelines/freestyle-success-10m/runs/28/');
 
-                const url = buildRunDetailsUrl(freestyle);
+                const url = badName001(freestyle);
                 assert.equal(url, '/organizations/jenkins/freestyle-success-10m/detail/freestyle-success-10m/28/pipeline');
-            });
-
-            it('handles a URL', () => {
-                const freestyle = '/blue/rest/organizations/jenkins/pipelines/freestyle-success-10m/runs/28/';
-                const url = buildRunDetailsUrl(freestyle);
-                assert.equal(url, '/organizations/jenkins/freestyle-success-10m/detail/freestyle-success-10m/28/pipeline');
-            });
-
-            it('throws sensible error for bogus object', () => {
-                assert.throws(() => buildRunDetailsUrl({}), 'Could not find input URL');
             });
 
             it('throws sensible error for bad url', () => {
-                assert.throws(() => buildRunDetailsUrl('/a/b/c/d'), 'Could not extract URI components');
-            });
-
-            it('throws sensible error for a string of chars', () => {
-                assert.throws(() => buildRunDetailsUrl('abcdefghijklmnopqrstuvwxyz'), 'Could not extract URI components');
+                assert.throws(() => badName001(createObjectFromLink('/a/b/c/d')), 'Could not extract URI components');
             });
         });
 
         describe('freestyle', () => {
             it('handles top-level job', () => {
-                const freestyle = createObjectFromLink(
-                    '/blue/rest/organizations/jenkins/pipelines/freestyle-success-10m/runs/28/'
-                );
+                const freestyle = createObjectFromLink('/blue/rest/organizations/jenkins/pipelines/freestyle-success-10m/runs/28/');
 
-                const url = buildRunDetailsUrl(freestyle);
+                const url = badName001(freestyle);
                 assert.equal(url, '/organizations/jenkins/freestyle-success-10m/detail/freestyle-success-10m/28/pipeline');
             });
 
             it('handles nested job', () => {
-                const freestyle = createObjectFromLink(
-                    '/blue/rest/organizations/jenkins/pipelines/myfolder/pipelines/pipeline-2/runs/103/'
-                );
+                const freestyle = createObjectFromLink('/blue/rest/organizations/jenkins/pipelines/myfolder/pipelines/pipeline-2/runs/103/');
 
-                const url = buildRunDetailsUrl(freestyle);
+                const url = badName001(freestyle);
                 assert.equal(url, '/organizations/jenkins/myfolder%2Fpipeline-2/detail/pipeline-2/103/pipeline');
             });
         });
 
         describe('pipeline', () => {
             it('handles top-level job', () => {
-                const pipeline = createObjectFromLink(
-                    '/blue/rest/organizations/jenkins/pipelines/pipeline-failure-15s/runs/42/'
-                );
+                const pipeline = createObjectFromLink('/blue/rest/organizations/jenkins/pipelines/pipeline-failure-15s/runs/42/');
 
-                const url = buildRunDetailsUrl(pipeline);
+                const url = badName001(pipeline);
                 assert.equal(url, '/organizations/jenkins/pipeline-failure-15s/detail/pipeline-failure-15s/42/pipeline');
             });
 
             it('handles nested job', () => {
-                const pipeline = createObjectFromLink(
-                    '/blue/rest/organizations/jenkins/pipelines/pipeline-failure-15s/runs/42/'
-                );
+                const pipeline = createObjectFromLink('/blue/rest/organizations/jenkins/pipelines/pipeline-failure-15s/runs/42/');
 
-                const url = buildRunDetailsUrl(pipeline);
+                const url = badName001(pipeline);
                 assert.equal(url, '/organizations/jenkins/pipeline-failure-15s/detail/pipeline-failure-15s/42/pipeline');
             });
         });
@@ -86,17 +72,17 @@ describe('UrlBuilder', () => {
                     '/blue/rest/organizations/jenkins/pipelines/jdl1/branches/experiment%252Fbuild-locally-docker/runs/41/'
                 );
 
-                const url = buildRunDetailsUrl(multibranch);
+                const url = badName001(multibranch);
                 assert.equal(url, '/organizations/jenkins/jdl1/detail/experiment%2Fbuild-locally-docker/41/pipeline');
             });
 
             it('handles nested job', () => {
                 const multibranch = createObjectFromLink(
                     '/blue/rest/organizations/jenkins/pipelines/folder1/pipelines/folder2/pipelines/folder3/pipelines' +
-                    '/jdl-2/branches/experiment%252Fbuild-locally-docker/runs/21/'
+                        '/jdl-2/branches/experiment%252Fbuild-locally-docker/runs/21/'
                 );
 
-                const url = buildRunDetailsUrl(multibranch);
+                const url = badName001(multibranch);
                 assert.equal(url, '/organizations/jenkins/folder1%2Ffolder2%2Ffolder3%2Fjdl-2/detail/experiment%2Fbuild-locally-docker/21/pipeline');
             });
         });
