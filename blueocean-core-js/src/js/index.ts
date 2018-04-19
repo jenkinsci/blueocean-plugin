f6060c1d500c21c52d635b2429357a920c310cb7
Ivan Meredith <ivan@ivan.net.nz>
2018-04-19 20:35:42 +1200

    f

--- blueocean-core-js/src/js/index.ts
+++ blueocean-core-js/src/js/index.ts
@@ -94,6 +94,7 @@ sseService.registerHandler(defaultSSEhandler.handleEvents);
 // Export some debugging stuff client code may need
 
 import { enableMocksForI18n, disableMocksForI18n } from './i18n/i18n';
+export { execute as i18nBundleStartup } from './i18n/bundle-startup';
 
 export const DEBUG = {
     enableMocksForI18n,
