88a9864271896e9e878c90b480b410b4837ea27f
kshultzCB <kshultz@cloudbees.com>
2018-04-24 08:39:32 -0400

    Remove unused imports, add

--- acceptance-tests/src/test/java/io/blueocean/ath/offline/multibranch/StartStopTest.java
+++ acceptance-tests/src/test/java/io/blueocean/ath/offline/multibranch/StartStopTest.java
@@ -3,34 +3,26 @@ package io.blueocean.ath.offline.multibranch;
 import com.google.common.io.Files;
 import com.google.common.io.Resources;
 
-import io.blueocean.ath.ATHJUnitRunner;
-import io.blueocean.ath.BlueOceanAcceptanceTest;
-import io.blueocean.ath.GitRepositoryRule;
+import io.blueocean.ath.*;
 import io.blueocean.ath.factory.MultiBranchPipelineFactory;
 import io.blueocean.ath.model.MultiBranchPipeline;
 import io.blueocean.ath.pages.blue.ActivityPage;
 import io.blueocean.ath.pages.blue.BranchPage;
 import io.blueocean.ath.sse.SSEClientRule;
-import io.blueocean.ath.WaitUtil;
 import org.apache.log4j.Logger;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.junit.JGitTestUtil;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.openqa.selenium.WebElement;
 
 import javax.inject.Inject;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
-import java.util.List;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
-
+@Login
 @RunWith(ATHJUnitRunner.class)
-
 public class StartStopTest extends BlueOceanAcceptanceTest {
     private Logger logger = Logger.getLogger(CommitMessagesTest.class);
 
