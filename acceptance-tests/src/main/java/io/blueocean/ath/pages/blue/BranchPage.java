5096540c6121ecf5dabc509c14e4c29820bd33c5
kshultzCB <kshultz@cloudbees.com>
2018-04-23 20:31:53 -0400

    Lower the amount of time our Jenkinsfile sleeps

--- acceptance-tests/src/main/java/io/blueocean/ath/pages/blue/BranchPage.java
+++ acceptance-tests/src/main/java/io/blueocean/ath/pages/blue/BranchPage.java
@@ -71,14 +71,6 @@ public class BranchPage implements WebDriverMixin {
     public BranchPage clickRunButton(String branch) {
         wait.click(By.cssSelector("div[data-branch='" + branch + "'] a.run-button"));
         logger.info("Clicked run button of branch " + branch);
-        // return activityPageFactory.withPipeline(pipeline).checkUrl(branch);
-        return this;
-    }
-
-    public BranchPage open() {
-        driver.get(pipeline.getUrl() + "/branches");
-        checkUrl();
-        logger.info("Opened branch page for " + pipeline);
         return this;
     }
 
@@ -88,6 +80,12 @@ public class BranchPage implements WebDriverMixin {
         return this;
     }
 
+    public BranchPage open() {
+        driver.get(pipeline.getUrl() + "/branches");
+        checkUrl();
+        logger.info("Opened branch page for " + pipeline);
+        return this;
+    }
 
     public EditorPage openEditor(String branch) {
         wait.until(By.cssSelector("div[data-branch='" + branch + "'] a.pipeline-editor-link")).click();
--- acceptance-tests/src/test/java/io/blueocean/ath/offline/multibranch/StartStopTest.java
+++ acceptance-tests/src/test/java/io/blueocean/ath/offline/multibranch/StartStopTest.java
@@ -2,6 +2,7 @@ package io.blueocean.ath.offline.multibranch;
 
 import com.google.common.io.Files;
 import com.google.common.io.Resources;
+
 import io.blueocean.ath.ATHJUnitRunner;
 import io.blueocean.ath.BlueOceanAcceptanceTest;
 import io.blueocean.ath.GitRepositoryRule;
@@ -10,6 +11,7 @@ import io.blueocean.ath.model.MultiBranchPipeline;
 import io.blueocean.ath.pages.blue.ActivityPage;
 import io.blueocean.ath.pages.blue.BranchPage;
 import io.blueocean.ath.sse.SSEClientRule;
+import io.blueocean.ath.WaitUtil;
 import org.apache.log4j.Logger;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.junit.JGitTestUtil;
@@ -44,7 +46,7 @@ public class StartStopTest extends BlueOceanAcceptanceTest {
     MultiBranchPipelineFactory mbpFactory;
 
     /**
-     * This tests the commit messages are being picked up from git and displayed on the run in activity.
+     * This tests the ability to start and stop builds of a pipeline.
      */
     @Test
     public void startStopTest() throws IOException, GitAPIException {
@@ -72,28 +74,14 @@ public class StartStopTest extends BlueOceanAcceptanceTest {
         stopStartPipeline.rescanThisPipeline();
         sseClientRule.untilEvents(stopStartPipeline.buildsFinished);
 
-        // This is where the browser actually goes to the UI
-        // ActivityPage activityPage = stopStartPipeline.getActivityPage().open();
         BranchPage branchPage = stopStartPipeline.getBranchPage().open();
         logger.info("Should have two branches now");
-
-        logger.info("OPEN BRANCH PAGE DOES THIS WORK");
-        // BranchPage branchPage = stopStartPipeline.getBranchPage().open();
         branchPage.clickRunButton(firstBranch);
         branchPage.clickRunButton(secondBranch);
-        // next we want to stop them.
         branchPage.clickStopButton(firstBranch);
         branchPage.clickStopButton(secondBranch);
-        // sseClientRule.untilEvents(stopStartPipeline.buildsFinished);
-        // This or activityPage.open.whatever?
-        // branchPage.clickActivityTab();
         ActivityPage activityPage = stopStartPipeline.getActivityPage().open();
-        // Now we need to verify that the builds were stopped. So need to add
-        // methods to activityPage to do that. Maybe with an assert. Example
-        // From elsewhere:
-        // activityPage.assertIsDuration(cells.get(5).getText());
-        // so maybe this?
-        // activityPage.assertIsStoppedRun(firstBranch);
-        logger.info("We did it.");
+        activityPage.checkPipeline();
+        logger.info("Branches started and stopped successfully.");
     }
 }
