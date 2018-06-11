1247c555028fb64532d62bda97e22889bf11acc7
olivier lamy <olamy@apache.org>
2018-06-11 20:16:38 +1000

    move js test to ath java
    Signed-off-by: olivier lamy <olamy@apache.org>

--- blueocean-rest-impl/src/main/java/io/jenkins/blueocean/service/embedded/rest/junit/BlueJUnitTestResult.java
+++ blueocean-rest-impl/src/main/java/io/jenkins/blueocean/service/embedded/rest/junit/BlueJUnitTestResult.java
@@ -1,22 +1,17 @@
 package io.jenkins.blueocean.service.embedded.rest.junit;
 
-import com.google.common.base.Function;
 import com.google.common.collect.Iterables;
 import hudson.Extension;
 import hudson.model.Run;
 import hudson.tasks.junit.CaseResult;
-import hudson.tasks.junit.TestResult;
 import hudson.tasks.junit.TestResultAction;
 import io.jenkins.blueocean.commons.ServiceException.NotFoundException;
 import io.jenkins.blueocean.rest.Reachable;
 import io.jenkins.blueocean.rest.factory.BlueTestResultFactory;
 import io.jenkins.blueocean.rest.hal.Link;
-import io.jenkins.blueocean.rest.model.BluePipelineNode;
 import io.jenkins.blueocean.rest.model.BlueTestResult;
 import org.kohsuke.accmod.Restricted;
 import org.kohsuke.accmod.restrictions.NoExternalUse;
-
-import javax.annotation.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,11 +24,11 @@ import static org.apache.commons.lang.StringUtils.isEmpty;
 @Restricted(NoExternalUse.class)
 public class BlueJUnitTestResult extends BlueTestResult {
 
-    protected final CaseResult testResult;
+    private final BlueCaseResult testResult;
 
     public BlueJUnitTestResult(CaseResult testResult, Link parent) {
         super(parent);
-        this.testResult = testResult;
+        this.testResult = new BlueCaseResult( testResult );
     }
 
     @Override
@@ -96,7 +91,7 @@ public class BlueJUnitTestResult extends BlueTestResult {
 
     @Override
     protected String getUniqueId() {
-        return testResult.getId();
+        return testResult.getClassName() + ":" + testResult.getId();
     }
 
     @Override
@@ -139,12 +134,43 @@ public class BlueJUnitTestResult extends BlueTestResult {
             testsToTransform.addAll(action.getFailedTests());
             testsToTransform.addAll(action.getSkippedTests());
             testsToTransform.addAll(action.getPassedTests());
-            return Result.of(Iterables.transform(testsToTransform, new Function<CaseResult, BlueTestResult>() {
-                @Override
-                public BlueTestResult apply(@Nullable CaseResult input) {
-                    return new BlueJUnitTestResult(input, parent.getLink());
-                }
-            }));
+            return Result.of(Iterables.transform(testsToTransform, //
+                                                 input ->  new BlueJUnitTestResult(input, parent.getLink())));
+        }
+    }
+
+
+    protected static class BlueCaseResult extends CaseResult {
+
+        private CaseResult caseResult;
+
+        public BlueCaseResult( CaseResult caseResult )
+        {
+            super( caseResult.getSuiteResult(), caseResult.getName(),
+                   caseResult.getErrorStackTrace(), caseResult.getErrorDetails() );
+            this.caseResult = caseResult;
+        }
+
+        public String getStdOut()
+        {
+            return caseResult.getStdout();
+        }
+
+        @Override
+        public String getStdout()
+        {
+            return caseResult.getStdout();
+        }
+
+        public String getStdErr()
+        {
+            return caseResult.getStderr();
+        }
+
+        @Override
+        public String getStderr()
+        {
+            return caseResult.getStderr();
         }
     }
 }
