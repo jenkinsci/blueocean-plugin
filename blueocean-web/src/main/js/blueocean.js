1c720ecbee8a6165eff26e15095e81ada1e3b704
Nicolae Pascu <contact@nicupascu.ro>
2018-09-12 12:21:20 +1000

    add core-js css to the webpack bundle

--- blueocean-web/src/main/js/blueocean.js
+++ blueocean-web/src/main/js/blueocean.js
@@ -1,6 +1,7 @@
 import jdlCSS from '@jenkins-cd/design-language/dist/assets/css/jenkins-design-language.css';
 import style from '../less/blueocean.less';
 import dashboardCSS from '@jenkins-cd/blueocean-dashboard/dist/assets/css/blueocean-dashboard.css';
+import coreCSS from '@jenkins-cd/blueocean-core-js/dist/assets/css/blueocean-core-js.css';
 
 try {
     // start the App
