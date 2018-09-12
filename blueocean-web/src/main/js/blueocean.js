06af08919ac9677420b5372f8690abadd9450141
Nicolae Pascu <contact@nicupascu.ro>
2018-09-12 11:39:23 +1000

    add jdl css to webpack css bundle

--- blueocean-web/src/main/js/blueocean.js
+++ blueocean-web/src/main/js/blueocean.js
@@ -1,3 +1,4 @@
+import jdlCSS from '@jenkins-cd/design-language/dist/assets/css/jenkins-design-language.css';
 import style from '../less/blueocean.less';
 import dashboardCSS from '@jenkins-cd/blueocean-dashboard/dist/assets/css/blueocean-dashboard.css';
 
