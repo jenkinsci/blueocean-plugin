a0c094bc9cc0c95e1b0250db9392ffb6cd81b5ef
Ivan Meredith <ivan@ivan.net.nz>
2018-09-19 11:43:37 +1200

    Getting ready for pull

--- blueocean-pipeline-editor/gulpfile.js
+++
@@ -1,8 +0,0 @@
-const jsb = require('@jenkins-cd/js-builder');
-
-jsb.gulp.on('stop', () => {
-    // force exit when in debug mode
-    if (typeof v8debug === 'object') {
-        process.exit();
-    }
-});
