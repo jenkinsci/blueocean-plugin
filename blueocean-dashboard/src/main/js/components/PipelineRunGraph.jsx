bb41b84d9c03e97cb84a31b6cf225a46bc6aa6e0
Josh McDonald <josh@joshmcdonald.info>
2018-12-13 17:07:35 +1000

    task/replace-graph * Successfully published and imported widgetised pipeline graph module

--- blueocean-dashboard/src/main/js/components/PipelineRunGraph.jsx
+++ blueocean-dashboard/src/main/js/components/PipelineRunGraph.jsx
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import { PipelineGraph } from '@jenkins-cd/design-language';
+import * as pg2 from '@jenkins-cd/pipeline-graph-widget';
 import { TimeManager, i18nTranslator, logging } from '@jenkins-cd/blueocean-core-js';
 
 const timeManager = new TimeManager();
@@ -271,6 +272,8 @@ export default class PipelineRunGraph extends Component {
     };
 
     render() {
+        console.log('Imported from widget', pg2); // TODO: RM
+
         const { graphNodes, t } = this.state;
 
         if (!graphNodes) {
