57fe2d05737989c528a8cca71168ca01e070e6fb
Gavin Mogan <gmogan@cloudbees.com>
2019-03-22 13:40:00 -0700

    Cleanup of url generation as per code review. Don't even create nulls in the first place

--- blueocean-dashboard/src/main/js/components/testing/TestService.js
+++ blueocean-dashboard/src/main/js/components/testing/TestService.js
@@ -3,18 +3,6 @@ import TestLogService from './TestLogService';
 
 const PAGE_SIZE = 100;
 
-// Source: https://stackoverflow.com/questions/286141/remove-blank-attributes-from-an-object-in-javascript/38340730#38340730
-const stripNullAndUndefined = obj =>
-    Object.keys(obj)
-        .filter(k => obj[k] !== null && obj[k] !== undefined) // Remove undef. and null.
-        .reduce(
-            (newObj, k) =>
-                typeof obj[k] === 'object'
-                    ? Object.assign(newObj, { [k]: stripNullAndUndefined(obj[k]) }) // Recurse.
-                    : Object.assign(newObj, { [k]: obj[k] }), // Copy value.
-            {}
-        );
-
 export default class TestService extends BunkerService {
     constructor(pagerService) {
         super(pagerService);
@@ -24,47 +12,44 @@ export default class TestService extends BunkerService {
     newRegressionsPager(pipeline, run) {
         return this.pagerService.getPager({
             key: `tests/regressions/${pipeline.organization}-${pipeline.name}-${run.id}/`,
-            lazyPager: () => new Pager(TestService.createURL({ run, status: null, state: 'REGRESSION' }), PAGE_SIZE, this),
+            lazyPager: () => new Pager(TestService.createURL(run, { state: 'REGRESSION' }), PAGE_SIZE, this),
         });
     }
 
     newExistingFailedPager(pipeline, run) {
         return this.pagerService.getPager({
             key: `tests/existingFailures/${pipeline.organization}-${pipeline.name}-${run.id}/`,
-            lazyPager: () => new Pager(TestService.createURL({ run, status: 'FAILED', state: '!REGRESSION' }), PAGE_SIZE, this),
+            lazyPager: () => new Pager(TestService.createURL(run, { status: 'FAILED', state: '!REGRESSION' }), PAGE_SIZE, this),
         });
     }
 
     newSkippedPager(pipeline, run) {
         return this.pagerService.getPager({
             key: `tests/skipped/${pipeline.organization}-${pipeline.name}-${run.id}/`,
-            lazyPager: () => new Pager(TestService.createURL({ run, status: 'SKIPPED', state: null }), PAGE_SIZE, this),
+            lazyPager: () => new Pager(TestService.createURL(run, { status: 'SKIPPED' }), PAGE_SIZE, this),
         });
     }
 
     newFixedPager(pipeline, run) {
         return this.pagerService.getPager({
             key: `tests/fixed/${pipeline.organization}-${pipeline.name}-${run.id}/`,
-            lazyPager: () => new Pager(TestService.createURL({ run, status: null, state: 'FIXED' }), PAGE_SIZE, this),
+            lazyPager: () => new Pager(TestService.createURL(run, { state: 'FIXED' }), PAGE_SIZE, this),
         });
     }
 
     newPassedPager(pipeline, run) {
         return this.pagerService.getPager({
             key: `tests/passed/${pipeline.organization}-${pipeline.name}-${run.id}/`,
-            lazyPager: () => new Pager(TestService.createURL({ run, status: 'PASSED', state: null }), PAGE_SIZE, this),
+            lazyPager: () => new Pager(TestService.createURL(run, { status: 'PASSED' }), PAGE_SIZE, this),
         });
     }
     testLogs() {
         return this._logs;
     }
 
-    static createURL({ run, status, state, age }) {
+    static createURL(run, searchParams) {
         const testBaseUrl = run._links.tests.href;
-        const params = new URLSearchParams(stripNullAndUndefined({ status, state, age }));
-        if (!params) {
-            throw new Error('must provide either stage, status or age');
-        }
-        return `${testBaseUrl}?${params}`;
+        const params = new URLSearchParams(searchParams);
+        return `${testBaseUrl}?${new URLSearchParams(params)}`;
     }
 }
