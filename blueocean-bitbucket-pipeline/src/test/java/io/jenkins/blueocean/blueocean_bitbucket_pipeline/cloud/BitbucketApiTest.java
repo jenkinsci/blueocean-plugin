88848d17c75d8c552b1fee7e67fb55a8f5d4588f
Gavin Mogan <gmogan@cloudbees.com>
2019-04-11 15:29:13 -0700

    down with usernames!

--- blueocean-bitbucket-pipeline/src/test/java/io/jenkins/blueocean/blueocean_bitbucket_pipeline/cloud/BitbucketApiTest.java
+++ blueocean-bitbucket-pipeline/src/test/java/io/jenkins/blueocean/blueocean_bitbucket_pipeline/cloud/BitbucketApiTest.java
@@ -3,7 +3,6 @@ package io.jenkins.blueocean.blueocean_bitbucket_pipeline.cloud;
 import com.cloudbees.plugins.credentials.CredentialsDescriptor;
 import com.cloudbees.plugins.credentials.CredentialsScope;
 import com.cloudbees.plugins.credentials.common.StandardUsernamePasswordCredentials;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import edu.umd.cs.findbugs.annotations.NonNull;
 import hudson.util.Secret;
 import io.jenkins.blueocean.blueocean_bitbucket_pipeline.BitbucketApi;
@@ -21,8 +20,6 @@ import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
-import java.io.UnsupportedEncodingException;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.powermock.api.mockito.PowerMockito.when;
@@ -33,9 +30,11 @@ import static org.powermock.api.mockito.PowerMockito.when;
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({Secret.class})
 @PowerMockIgnore({"javax.crypto.*", "javax.security.*", "javax.net.ssl.*", "com.sun.org.apache.xerces.*", "javax.xml.*", "org.xml.*", "org.w3c.dom.*"})
-public class BitbucketApiTest  extends BbCloudWireMock{
+public class BitbucketApiTest extends BbCloudWireMock {
 
     private BitbucketApi api;
+    private static String USER_KEY = "{1c5c9255-d59f-47e2-b5c7-52269c0332b9}"; // vivekp7;
+    private static String TEAM_KEY = "{47cd7cf2-ca31-4c90-bc0e-4c7ef67f9dfe}"; // vivektestteam
 
     @Override
     public void setup() throws Exception {
@@ -44,71 +43,71 @@ public class BitbucketApiTest  extends BbCloudWireMock{
     }
 
     @Test
-    public void getUser(){
+    public void getUser() {
         BbUser user = api.getUser();
         assertNotNull(user);
-        assertEquals(getUserName(), user.getSlug());
-        assertEquals("https://bitbucket.org/account/vivekp7/avatar/50/", user.getAvatar());
+        assertEquals(USER_KEY, user.getSlug());
+        assertEquals("https://bitbucket.org/account/" + USER_KEY + "/avatar/50/", user.getAvatar());
     }
 
     @Test
-    public void getTeams() throws JsonProcessingException {
+    public void getTeams() {
         BbPage<BbOrg> teams = api.getOrgs(1, 100);
         assertEquals(2, teams.getSize());
-        assertEquals("vivekp7", teams.getValues().get(0).getKey());
+        assertEquals(USER_KEY, teams.getValues().get(0).getKey());
         assertEquals("Vivek Pandey", teams.getValues().get(0).getName());
-        assertEquals("https://bitbucket.org/account/vivekp7/avatar/50/", teams.getValues().get(0).getAvatar());
+        assertEquals("https://bitbucket.org/account/" + USER_KEY + "/avatar/50/", teams.getValues().get(0).getAvatar());
 
-        assertEquals("vivektestteam", teams.getValues().get(1).getKey());
+        assertEquals(TEAM_KEY, teams.getValues().get(1).getKey());
         assertEquals("Vivek's Team", teams.getValues().get(1).getName());
-        assertEquals("https://bitbucket.org/account/vivektestteam/avatar/50/", teams.getValues().get(1).getAvatar());
+        assertEquals("https://bitbucket.org/account/" + TEAM_KEY + "/avatar/50/", teams.getValues().get(1).getAvatar());
     }
 
     @Test
-    public void getUserTeam() throws JsonProcessingException {
-        BbOrg team = api.getOrg("vivekp7");
-        assertEquals("vivekp7", team.getKey());
+    public void getUserTeam() {
+        BbOrg team = api.getOrg(USER_KEY);
+        assertEquals(USER_KEY, team.getKey());
         assertEquals("Vivek Pandey", team.getName());
-        assertEquals("https://bitbucket.org/account/vivekp7/avatar/50/", team.getAvatar());
+        assertEquals("https://bitbucket.org/account/" + USER_KEY + "/avatar/50/", team.getAvatar());
     }
 
     @Test
-    public void getUserTeamUsingEmail() throws JsonProcessingException {
+    public void getUserTeamUsingEmail() {
         api = new BitbucketCloudApi(apiUrl, getMockedCredentials("x.y@gmail.com"));
-        BbOrg team = api.getOrg("vivekp7");
-        assertEquals("vivekp7", team.getKey());
+        BbOrg team = api.getOrg(USER_KEY);
+        assertEquals(USER_KEY, team.getKey());
         assertEquals("Vivek Pandey", team.getName());
-        assertEquals("https://bitbucket.org/account/vivekp7/avatar/50/", team.getAvatar());
+        assertEquals("https://bitbucket.org/account/" + USER_KEY + "/avatar/50/", team.getAvatar());
     }
 
 
     @Test
-    public void getTeam() throws JsonProcessingException {
-        BbOrg team = api.getOrg("vivektestteam");
-        assertEquals("vivektestteam", team.getKey());
+    public void getTeam() {
+        BbOrg team = api.getOrg(TEAM_KEY);
+        assertEquals(TEAM_KEY, team.getKey());
         assertEquals("Vivek's Team", team.getName());
-        assertEquals("https://bitbucket.org/account/vivektestteam/avatar/50/", team.getAvatar());
+        assertEquals("https://bitbucket.org/account/" + TEAM_KEY + "/avatar/50/", team.getAvatar());
     }
 
     @Test
-    public void getRepos(){
-        BbPage<BbRepo> repos = api.getRepos("vivektestteam", 1, 100);
+    public void getRepos() {
+        BbPage<BbRepo> repos = api.getRepos(TEAM_KEY, 1, 100);
         assertEquals(2, repos.getSize());
         assertEquals("pipeline-demo-test", repos.getValues().get(0).getSlug());
         assertEquals("emptyrepo", repos.getValues().get(1).getSlug());
     }
 
     @Test
-    public void getRepo(){
-        BbRepo repo = api.getRepo("vivektestteam", "pipeline-demo-test");
+    public void getRepo() {
+        BbRepo repo = api.getRepo(TEAM_KEY, "pipeline-demo-test");
         assertEquals("pipeline-demo-test", repo.getSlug());
     }
 
     @Test
-    public void getRepoContent() throws JsonProcessingException, UnsupportedEncodingException {
-        BbBranch branch = api.getDefaultBranch("vivekp7","demo1");
+    public void getRepoContent() {
+        BbBranch branch = api.getDefaultBranch(USER_KEY, "demo1");
         assertNotNull(branch);
-        String content = api.getContent("vivekp7", "demo1", "Jenkinsfile", branch.getLatestCommit());
+        String content = api.getContent(USER_KEY, "demo1", "Jenkinsfile", branch.getLatestCommit());
         assertEquals("node{\n" +
                 "  echo 'hello world!'\n" +
                 "}", content);
@@ -116,41 +115,43 @@ public class BitbucketApiTest  extends BbCloudWireMock{
 
 
     @Test
-    public void createNewRepoContent() throws JsonProcessingException, UnsupportedEncodingException {
+    public void createNewRepoContent() {
         //create new file
-        BbSaveContentResponse saveResponse = api.saveContent("vivekp7","demo1","foo",
+        BbSaveContentResponse saveResponse = api.saveContent(USER_KEY, "demo1", "foo",
                 "This is test content in new file",
-                "first commit", "null",null,null);
+                "first commit", "null", null, null);
         assertNotNull(saveResponse.getCommitId());
-        String content = api.getContent("vivekp7", "demo1", "foo", (String) saveResponse.getCommitId());
+        String content = api.getContent(USER_KEY, "demo1", "foo", (String) saveResponse.getCommitId());
         assertEquals("This is test content in new file", content);
     }
 
     //This is duplicated code from server, we can't move to base class as inheritance won't work with @RunWith annotation
     //from subclasses
-    private StandardUsernamePasswordCredentials getMockedCredentials(){
+    private StandardUsernamePasswordCredentials getMockedCredentials() {
         return getMockedCredentials(getUserName());
     }
 
-    private StandardUsernamePasswordCredentials getMockedCredentials(final String username){
+    private StandardUsernamePasswordCredentials getMockedCredentials(final String username) {
         final Secret secret = Mockito.mock(Secret.class);
         when(secret.getPlainText()).thenReturn(getPassword());
 
         PowerMockito.mockStatic(Secret.class);
         when(Secret.toString(secret)).thenReturn(getPassword());
 
-        return new StandardUsernamePasswordCredentials(){
+        return new StandardUsernamePasswordCredentials() {
             @Override
             public CredentialsScope getScope() {
                 return CredentialsScope.SYSTEM;
             }
+
             @NonNull
             @Override
             public CredentialsDescriptor getDescriptor() {
-                return new CredentialsDescriptor(){
+                return new CredentialsDescriptor() {
 
                 };
             }
+
             @NonNull
             @Override
             public String getUsername() {
