3927f46ee6d953cb5af7b699b249784634d3ce19
p4charu <cbopardikar@perforce.com>
2019-06-14 11:58:17 +0100

    Changed local variable name to cred to avoid ambiguity between class variable named credentials.

--- blueocean-dashboard/src/main/js/credentials/perforce/PerforceCredentialsManager.js
+++ blueocean-dashboard/src/main/js/credentials/perforce/PerforceCredentialsManager.js
@@ -18,17 +18,17 @@ class PerforceCredentialsManager {
     }
 
     @action
-    _onfindCredSuccess(credentials) {
+    _onfindCredSuccess(creds) {
         // We need only perforce credentials, so filter the non Perforce credentials out
         //TODO Is there a better way of doing this?
-        const length = credentials.length;
+        const length = creds.length;
         for (let i = 0; i < length; i++) {
-            const obj = credentials[i];
+            const obj = creds[i];
             if (obj.typeName.startsWith("Perforce")) {
                 this.credentials.push(obj);
             }
         }
-        return credentials;
+        return this.credentials;
     }
 
 }
