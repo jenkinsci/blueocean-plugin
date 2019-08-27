db8d55d3ea7a23ae80d5cdc40c2db9ead02d3833
Gavin Mogan <github@gavinmogan.com>
2019-08-27 10:25:15 -0700

    JENKINS-55605 - Remove restriction that prevented clicking/url updating on unfinished nodes, or parallel nodes (#2018)
    Removed the restriction on clicking on various nodes.
    The system seems to redirect to a random node if the node id is invalid (so kinda the same behaviour as now)
    This way whatever the state is, we try to show the logs for it.
    
    Was not able to find any existing reason why this restriction is in place

--- blueocean-dashboard/src/main/js/components/karaoke/components/Pipeline.jsx
+++ blueocean-dashboard/src/main/js/components/karaoke/components/Pipeline.jsx
@@ -270,15 +270,11 @@ export default class Pipeline extends Component {
                 pathArray.shift();
                 nextPath = `/${pathArray.join('/')}`;
             }
-            // check whether we have a parallel node or seq parallel nodes
-            const isParallel = nextNode.isParallel || (nextNode.firstParent && nextNode.parent != nextNode.firstParent);
 
-            // see whether we need to update the state
-            if (nextNode.state === 'FINISHED' || isParallel) {
-                nextPath = `${nextPath}/${id}`; // only allow node param in finished nodes
-            }
+            nextPath = `${nextPath}/${id}`;
+
             // see whether we need to update the karaoke mode
-            if ((nextNode.state === 'FINISHED' || isParallel) && this.karaoke) {
+            if (nextNode.state === 'FINISHED' && this.karaoke) {
                 logger.debug('turning off karaoke since we do not need it anymore because focus is on a finished node.');
                 this.stopKaraoke();
             }
