857168d2a92f7e0a2e7703f05f00f8877f4c57b6
olivier lamy <olamy@apache.org>
2020-10-19 14:31:53 +1000

    [JENKINS-63979] tombstone blueocean-executor-info plugin
    Signed-off-by: olivier lamy <olamy@apache.org>

--- blueocean-executor-info/src/main/js/Routes.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Route } from 'react-router';
-import { ExecutorInfoPage } from './ExecutorInfoPage';
-
-export default (
-    <Route path="/executor-info"
-           component={ExecutorInfoPage} />
-);
