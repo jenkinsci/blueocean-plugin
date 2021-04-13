4939abf0f34bd897f79940ab8937092f227e0503
olivier lamy <olamy@apache.org>
2021-04-13 13:28:18 +1000

    use a simple sha256hex
    Signed-off-by: olivier lamy <olamy@apache.org>

--- blueocean-bitbucket-pipeline/src/main/java/io/jenkins/blueocean/blueocean_bitbucket_pipeline/BitbucketCredentialUtils.java
+++ blueocean-bitbucket-pipeline/src/main/java/io/jenkins/blueocean/blueocean_bitbucket_pipeline/BitbucketCredentialUtils.java
@@ -2,7 +2,7 @@ package io.jenkins.blueocean.blueocean_bitbucket_pipeline;
 
 import io.jenkins.blueocean.blueocean_bitbucket_pipeline.cloud.BitbucketCloudScm;
 import io.jenkins.blueocean.blueocean_bitbucket_pipeline.server.BitbucketServerScm;
-import org.apache.commons.codec.digest.DigestUtils;
+import io.jenkins.blueocean.commons.DigestUtils;
 import org.apache.commons.lang.StringUtils;
 
 /**
@@ -28,6 +28,6 @@ public class BitbucketCredentialUtils {
             return scmId;
         }
 
-        return String.format("%s:%s", BitbucketServerScm.ID, DigestUtils.sha256Hex(apiUrl));
+        return String.format( "%s:%s", BitbucketServerScm.ID, DigestUtils.sha256Hex(apiUrl));
     }
 }
