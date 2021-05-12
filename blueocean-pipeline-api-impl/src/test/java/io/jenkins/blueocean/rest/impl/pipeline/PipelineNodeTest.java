cb299496f90eff4e69341c0ce71e9c1b61eb2057
Ramon Leon <manuelramonleonjimenez@gmail.com>
2021-05-12 14:12:08 +0200

    Update blueocean-pipeline-api-impl/src/test/java/io/jenkins/blueocean/rest/impl/pipeline/PipelineNodeTest.java
    Co-authored-by: Jesse Glick <jglick@cloudbees.com>

--- blueocean-pipeline-api-impl/src/test/java/io/jenkins/blueocean/rest/impl/pipeline/PipelineNodeTest.java
+++ blueocean-pipeline-api-impl/src/test/java/io/jenkins/blueocean/rest/impl/pipeline/PipelineNodeTest.java
@@ -2292,12 +2292,7 @@ public class PipelineNodeTest extends PipelineBaseTest {
         Assert.assertEquals("param2", parameters.get(1).get("name"));
         Assert.assertEquals("StringParameterDefinition", parameters.get(1).get("type"));
         Assert.assertEquals("string param", parameters.get(1).get("description"));
-
-        if (Jenkins.getVersion().isOlderThan(new VersionNumber("2.281"))) {
-            Assert.assertNull(((Map) parameters.get(1).get("defaultParameterValue")).get("value"));
-        } else {
-            Assert.assertEquals("", ((Map) parameters.get(1).get("defaultParameterValue")).get("value"));
-        }
+        Assert.assertNull(Util.fixEmpty(((Map) parameters.get(1).get("defaultParameterValue")).get("value")));
 
         resp = post("/organizations/jenkins/pipelines/pipeline1/runs/", ImmutableMap.of("parameters",
                                                                                         ImmutableList.of(ImmutableMap.of("name", "param1", "value", "abc"), ImmutableMap.of("name", "param2", "value", "def"))
