406bba750654f5724e4e52c8fab12a4da011695c
Rene Bredlau <git@unrelated.de>
2021-05-19 15:38:21 +0200

    Renamed test (was ignored before by filename pattern). Rewrote asserts to reflect actually behavior of the code.

--- blueocean-git-pipeline/src/test/java/io/jenkins/blueocean/blueocean_git_pipeline/GitScmNonNetworkTest.java
+++ blueocean-git-pipeline/src/test/java/io/jenkins/blueocean/blueocean_git_pipeline/GitScmNonNetworkTest.java
@@ -1,43 +1,44 @@
 package io.jenkins.blueocean.blueocean_git_pipeline;
 
+import io.jenkins.blueocean.commons.DigestUtils;
 import org.junit.Assert;
 import org.junit.Test;
 
 /**
  * Unit tests fot GitScm that don't run via network
  */
-public class GitScmTest2 {
+public class GitScmNonNetworkTest {
 
     @Test
     public void shouldMakeCredentialIdForHttp() {
         String result = GitScm.makeCredentialId("http://example.org/git/foo.git");
-        Assert.assertEquals("git:http://example.org/git/foo.git", result);
+        Assert.assertEquals("git:" + DigestUtils.sha256Hex("http://example.org"), result);
     }
 
     @Test
     public void shouldMakeCredentialIdForHttps() {
         String result = GitScm.makeCredentialId("https://example.org/git/foo.git");
-        Assert.assertEquals("git:https://example.org/git/foo.git", result);
+        Assert.assertEquals("git:" + DigestUtils.sha256Hex("https://example.org"), result);
     }
 
     @Test
     public void shouldNotMakeCredentialIdForSsh() {
         String result = GitScm.makeCredentialId("ssh://example.org/git/foo.git");
-        Assert.assertEquals(null, result);
+        Assert.assertNull(result);
     }
 
     @Test
     public void shouldNotMakeCredentialIdForGit() {
         String result = GitScm.makeCredentialId("git://example.org/git/foo.git");
-        Assert.assertEquals(null, result);
+        Assert.assertNull(result);
     }
 
     @Test
     public void shouldNotMakeCredentialIdForBadUrls() {
-        Assert.assertEquals(null, GitScm.makeCredentialId("this is not a url worth mentioning"));
-        Assert.assertEquals(null, GitScm.makeCredentialId("git://"));
-        Assert.assertEquals(null, GitScm.makeCredentialId("http://"));
-        Assert.assertEquals(null, GitScm.makeCredentialId(""));
-        Assert.assertEquals(null, GitScm.makeCredentialId(null));
+        Assert.assertNull(GitScm.makeCredentialId("this is not a url worth mentioning"));
+        Assert.assertNull(GitScm.makeCredentialId("git://"));
+        Assert.assertNull(GitScm.makeCredentialId("http://"));
+        Assert.assertNull(GitScm.makeCredentialId(""));
+        Assert.assertNull(GitScm.makeCredentialId(null));
     }
 }
