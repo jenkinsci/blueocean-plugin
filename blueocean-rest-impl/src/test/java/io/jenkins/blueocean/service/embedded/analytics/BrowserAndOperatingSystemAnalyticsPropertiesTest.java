26f13e88d32f57eafc2be4da1381a29e734bebf1
Olivier Lamy <olamy@apache.org>
2021-06-04 09:36:41 +1000

    Update blueocean-rest-impl/src/test/java/io/jenkins/blueocean/service/embedded/analytics/BrowserAndOperatingSystemAnalyticsPropertiesTest.java
    Co-authored-by: Liam Newman <bitwiseman@gmail.com>

--- blueocean-rest-impl/src/test/java/io/jenkins/blueocean/service/embedded/analytics/BrowserAndOperatingSystemAnalyticsPropertiesTest.java
+++ blueocean-rest-impl/src/test/java/io/jenkins/blueocean/service/embedded/analytics/BrowserAndOperatingSystemAnalyticsPropertiesTest.java
@@ -21,7 +21,7 @@ public class BrowserAndOperatingSystemAnalyticsPropertiesTest {
     @Test
     public void parsesUserAgentAndCreatesPropertiesForChrome() throws Exception {
         BrowserAndOperatingSystemAnalyticsProperties props = setup("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36");
-        Map<String, Object> actual = new HashMap<>(props.properties( new Analytics.TrackRequest( "bob", null)));
+        Map<String, Object> actual = new HashMap<>(props.properties(new Analytics.TrackRequest("bob", null)));
         Assert.assertEquals(6, actual.size());
         Assert.assertEquals("41", actual.get("browserVersionMajor"));
         Assert.assertEquals("10", actual.get("osVersionMinor"));
