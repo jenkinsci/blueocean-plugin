4ca332c07ca0b1f58f4dc0852a71b27b428ad6be
Ivan Pavlovic <pavlovic.ivan.26@gmail.com>
2022-04-25 22:54:32 +0200

    Add test to assert PipelineNodeImpl.isRestartable() behaves as expected

--- blueocean-pipeline-api-impl/src/test/java/io/jenkins/blueocean/rest/impl/pipeline/PipelineNodeImplTest.java
+++ blueocean-pipeline-api-impl/src/test/java/io/jenkins/blueocean/rest/impl/pipeline/PipelineNodeImplTest.java
@@ -1,10 +1,15 @@
 package io.jenkins.blueocean.rest.impl.pipeline;
 
+import io.jenkins.blueocean.rest.model.BlueRun;
 import io.jenkins.blueocean.service.embedded.rest.QueueUtil;
+import org.apache.http.nio.protocol.PipeliningClientExchangeHandler;
+import org.jenkinsci.plugins.pipeline.modeldefinition.actions.RestartDeclarativePipelineAction;
+import org.jenkinsci.plugins.pipeline.modeldefinition.model.SkippedStageReason;
 import org.jenkinsci.plugins.workflow.job.WorkflowJob;
 import org.jenkinsci.plugins.workflow.job.WorkflowRun;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.jvnet.hudson.test.Issue;
 import org.mockito.Mock;
 import org.mockito.internal.verification.VerificationModeFactory;
 import org.powermock.api.mockito.PowerMockito;
@@ -12,8 +17,9 @@ import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
+import java.util.List;
+
+import static org.junit.Assert.*;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({ WorkflowRun.class, WorkflowJob.class, QueueUtil.class })
@@ -59,5 +65,58 @@ public class PipelineNodeImplTest {
 
         PowerMockito.verifyStatic(QueueUtil.class, VerificationModeFactory.times(4));
         QueueUtil.getRun(job, 1); // need to call again to handle verify
+    }
+
+    @Issue("JENKINS-54250")
+    @Test public void getRun_isRestartable_withDefaults() throws Exception {
+        PipelineNodeImpl underTest = PowerMockito.mock(PipelineNodeImpl.class);
+        RestartDeclarativePipelineAction action = PowerMockito.mock(RestartDeclarativePipelineAction.class);
+        List<String> restartableStages = PowerMockito.mock(List.class);
+
+        PowerMockito.field(PipelineNodeImpl.class, "run").set(underTest, this.run);
+        PowerMockito.when(run.getAction(RestartDeclarativePipelineAction.class)).thenReturn(action);
+        PowerMockito.when(this.run.getParent()).thenReturn(job);
+        PowerMockito.when(job.isRestartableFromStage()).thenReturn(true);
+        PowerMockito.when(action.getRestartableStages()).thenReturn(restartableStages);
+        PowerMockito.when(restartableStages.contains(underTest.getDisplayName())).thenReturn(true);
+        PowerMockito.when(underTest.getStateObj()).thenReturn(BlueRun.BlueRunState.FINISHED);
+        PowerMockito.when(underTest.isRestartable()).thenCallRealMethod();
+
+        assertTrue(underTest.isRestartable());
+    }
+
+    @Issue("JENKINS-54250")
+    @Test public void getRun_isRestartable() throws Exception {
+        PipelineNodeImpl underTest = PowerMockito.mock(PipelineNodeImpl.class);
+        RestartDeclarativePipelineAction action = PowerMockito.mock(RestartDeclarativePipelineAction.class);
+        List<String> restartableStages = PowerMockito.mock(List.class);
+        PowerMockito.field(PipelineNodeImpl.class, "run").set(underTest, this.run);
+        PowerMockito.when(run.getAction(RestartDeclarativePipelineAction.class)).thenReturn(action);
+        PowerMockito.when(this.run.getParent()).thenReturn(job);
+        PowerMockito.when(job.isRestartableFromStage()).thenReturn(true);
+        PowerMockito.when(action.getRestartableStages()).thenReturn(restartableStages);
+        PowerMockito.when(restartableStages.contains(underTest.getDisplayName())).thenReturn(true);
+        PowerMockito.when(underTest.getStateObj()).thenReturn(BlueRun.BlueRunState.FINISHED);
+        PowerMockito.when(underTest.isRestartable()).thenCallRealMethod();
+
+        assertTrue(underTest.isRestartable());
+    }
+
+    @Issue("JENKINS-54250")
+    @Test public void getRun_isNotRestartable() throws Exception {
+        PipelineNodeImpl underTest = PowerMockito.mock(PipelineNodeImpl.class);
+        RestartDeclarativePipelineAction action = PowerMockito.mock(RestartDeclarativePipelineAction.class);
+        List<String> restartableStages = PowerMockito.mock(List.class);
+
+        PowerMockito.field(PipelineNodeImpl.class, "run").set(underTest, this.run);
+        PowerMockito.when(run.getAction(RestartDeclarativePipelineAction.class)).thenReturn(action);
+        PowerMockito.when(this.run.getParent()).thenReturn(job);
+        PowerMockito.when(job.isRestartableFromStage()).thenReturn(false);
+        PowerMockito.when(action.getRestartableStages()).thenReturn(restartableStages);
+        PowerMockito.when(restartableStages.contains(underTest.getDisplayName())).thenReturn(true);
+        PowerMockito.when(underTest.getStateObj()).thenReturn(BlueRun.BlueRunState.FINISHED);
+        PowerMockito.when(underTest.isRestartable()).thenCallRealMethod();
+
+        assertFalse(underTest.isRestartable());
     }
 }
