f90394fa080f1d96ac72ff3c9dae0fe876f99a94
Okafor Peace <87018599+Ladyprowess@users.noreply.github.com>
2022-05-18 22:04:52 +0100

    Make a comment more inclusive
    Rather than "master" use "controller"

--- blueocean-rest/src/main/java/io/jenkins/blueocean/rest/factory/BlueFavoriteResolver.java
+++ blueocean-rest/src/main/java/io/jenkins/blueocean/rest/factory/BlueFavoriteResolver.java
@@ -11,7 +11,7 @@ import io.jenkins.blueocean.rest.model.BlueFavorite;
  *
  * For example:
  *
- * A favorite multi-branch project might resolve to a master branch pipeline
+ * A favorite multi-branch project might resolve to a controller branch pipeline
  *
  * @author Vivek Pandey
  */
