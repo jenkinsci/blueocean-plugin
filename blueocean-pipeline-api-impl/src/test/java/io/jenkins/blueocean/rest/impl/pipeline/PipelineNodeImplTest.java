802c2c4f7b9563edb85aa16e94f85ab2936d80a3
Ivan Pavlovic <pavlovic.ivan.26@gmail.com>
2022-08-31 20:12:10 +0200

    Remove PowerMockito and use Mockito to mock the tests

--- blueocean-pipeline-api-impl/src/test/java/io/jenkins/blueocean/rest/impl/pipeline/PipelineNodeImplTest.java
+++ blueocean-pipeline-api-impl/src/test/java/io/jenkins/blueocean/rest/impl/pipeline/PipelineNodeImplTest.java
@@ -1,12 +1,20 @@
 package io.jenkins.blueocean.rest.impl.pipeline;
 
+import com.sun.tools.javac.comp.Flow;
+import edu.umd.cs.findbugs.annotations.NonNull;
+import io.jenkins.blueocean.rest.Reachable;
+import io.jenkins.blueocean.rest.hal.Link;
 import io.jenkins.blueocean.rest.model.BlueRun;
 import io.jenkins.blueocean.service.embedded.rest.QueueUtil;
 import org.apache.http.nio.protocol.PipeliningClientExchangeHandler;
 import org.jenkinsci.plugins.pipeline.modeldefinition.actions.RestartDeclarativePipelineAction;
 import org.jenkinsci.plugins.pipeline.modeldefinition.model.SkippedStageReason;
+import org.jenkinsci.plugins.pipeline.modeldefinition.model.Stage;
+import org.jenkinsci.plugins.workflow.actions.StageAction;
+import org.jenkinsci.plugins.workflow.graph.FlowNode;
 import org.jenkinsci.plugins.workflow.job.WorkflowJob;
 import org.jenkinsci.plugins.workflow.job.WorkflowRun;
+import org.jenkinsci.plugins.workflow.pipelinegraphanalysis.TimingInfo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.jvnet.hudson.test.Issue;
@@ -16,6 +24,7 @@ import org.mockito.Mockito;
 import org.mockito.internal.verification.VerificationModeFactory;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -68,54 +77,73 @@ public class PipelineNodeImplTest {
     }
 
     @Issue("JENKINS-54250")
-    @Test public void getRun_isRestartable_withDefaults() throws Exception {
-        PipelineNodeImpl underTest = PowerMockito.mock(PipelineNodeImpl.class);
-        RestartDeclarativePipelineAction action = PowerMockito.mock(RestartDeclarativePipelineAction.class);
-        List<String> restartableStages = PowerMockito.mock(List.class);
-
-        PowerMockito.field(PipelineNodeImpl.class, "run").set(underTest, this.run);
-        PowerMockito.when(run.getAction(RestartDeclarativePipelineAction.class)).thenReturn(action);
-        PowerMockito.when(this.run.getParent()).thenReturn(job);
-        PowerMockito.when(action.isRestartEnabled()).thenReturn(true);
-        PowerMockito.when(action.getRestartableStages()).thenReturn(restartableStages);
-        PowerMockito.when(restartableStages.contains(underTest.getDisplayName())).thenReturn(true);
-        PowerMockito.when(underTest.getStateObj()).thenReturn(BlueRun.BlueRunState.FINISHED);
-        PowerMockito.when(underTest.isRestartable()).thenCallRealMethod();
-
-        assertTrue(underTest.isRestartable());
-    }
-
-    @Issue("JENKINS-54250")
     @Test public void getRun_isRestartable() throws Exception {
-        PipelineNodeImpl underTest = PowerMockito.mock(PipelineNodeImpl.class);
-        RestartDeclarativePipelineAction action = PowerMockito.mock(RestartDeclarativePipelineAction.class);
-        List<String> restartableStages = PowerMockito.mock(List.class);
-        PowerMockito.field(PipelineNodeImpl.class, "run").set(underTest, this.run);
-        PowerMockito.when(run.getAction(RestartDeclarativePipelineAction.class)).thenReturn(action);
-        PowerMockito.when(this.run.getParent()).thenReturn(job);
-        PowerMockito.when(action.isRestartEnabled()).thenReturn(true);
-        PowerMockito.when(action.getRestartableStages()).thenReturn(restartableStages);
-        PowerMockito.when(restartableStages.contains(underTest.getDisplayName())).thenReturn(true);
-        PowerMockito.when(underTest.getStateObj()).thenReturn(BlueRun.BlueRunState.FINISHED);
-        PowerMockito.when(underTest.isRestartable()).thenCallRealMethod();
+        FlowNode flowNode = Mockito.mock(FlowNode.class);
+        NodeRunStatus nodeRunStatus = Mockito.mock(NodeRunStatus.class);
+        TimingInfo timingInfo = Mockito.mock(TimingInfo.class);
+        StageAction stageAction = Mockito.mock(StageAction.class);
+
+        Mockito.when(flowNode.getAction(StageAction.class)).thenReturn(stageAction);
+        Mockito.when(flowNode.getDisplayName()).thenReturn("DummyName");
+
+        FlowNodeWrapper node = Mockito.mock(FlowNodeWrapper.class, Mockito.withSettings().useConstructor(flowNode, nodeRunStatus, timingInfo, this.run));
+
+        Reachable parent = Mockito.mock(Reachable.class);
+        Link link = Mockito.mock(Link.class);
+
+        Mockito.when(node.getStatus()).thenReturn(nodeRunStatus);
+        Mockito.when(node.getTiming()).thenReturn(timingInfo);
+        Mockito.when(timingInfo.getTotalDurationMillis()).thenReturn(1000L);
+        Mockito.when(parent.getLink()).thenReturn(link);
+        Mockito.when(node.getId()).thenReturn("dummyId");
+        Mockito.when(link.rel(node.getId())).thenReturn(link);
+        Mockito.when(node.getDisplayName()).thenReturn("DummyName");
+
+        PipelineNodeImpl underTest = Mockito.mock(PipelineNodeImpl.class, Mockito.withSettings().useConstructor(node, parent, this.run));
+        assertNotNull(underTest);
+
+        RestartDeclarativePipelineAction restartableAction = Mockito.mock(RestartDeclarativePipelineAction.class);
+        List<String> restartableStages = Mockito.mock(List.class);
+        Mockito.when(this.run.getAction(RestartDeclarativePipelineAction.class)).thenReturn(restartableAction);
+        Mockito.when(restartableAction.isRestartEnabled()).thenReturn(true);
+        Mockito.when(restartableAction.getRestartableStages()).thenReturn(restartableStages);
+        Mockito.when(underTest.getDisplayName()).thenReturn("DummyName");
+        Mockito.when(restartableStages.contains(underTest.getDisplayName())).thenReturn(true);
+        Mockito.when(underTest.getStateObj()).thenReturn(BlueRun.BlueRunState.FINISHED);
+        Mockito.when(underTest.isRestartable()).thenCallRealMethod();
 
         assertTrue(underTest.isRestartable());
     }
 
     @Issue("JENKINS-54250")
     @Test public void getRun_isNotRestartable() throws Exception {
-        PipelineNodeImpl underTest = PowerMockito.mock(PipelineNodeImpl.class);
-        RestartDeclarativePipelineAction action = PowerMockito.mock(RestartDeclarativePipelineAction.class);
-        List<String> restartableStages = PowerMockito.mock(List.class);
+        FlowNode flowNode = Mockito.mock(FlowNode.class);
+        NodeRunStatus nodeRunStatus = Mockito.mock(NodeRunStatus.class);
+        TimingInfo timingInfo = Mockito.mock(TimingInfo.class);
+        StageAction stageAction = Mockito.mock(StageAction.class);
 
-        PowerMockito.field(PipelineNodeImpl.class, "run").set(underTest, this.run);
-        PowerMockito.when(run.getAction(RestartDeclarativePipelineAction.class)).thenReturn(action);
-        PowerMockito.when(this.run.getParent()).thenReturn(job);
-        PowerMockito.when(action.isRestartEnabled()).thenReturn(false);
-        PowerMockito.when(restartableStages.contains(underTest.getDisplayName())).thenReturn(true);
-        PowerMockito.when(underTest.getStateObj()).thenReturn(BlueRun.BlueRunState.FINISHED);
-        PowerMockito.when(underTest.isRestartable()).thenCallRealMethod();
+        Mockito.when(flowNode.getAction(StageAction.class)).thenReturn(stageAction);
+        Mockito.when(flowNode.getDisplayName()).thenReturn("DummyName");
 
+        FlowNodeWrapper node = Mockito.mock(FlowNodeWrapper.class, Mockito.withSettings().useConstructor(flowNode, nodeRunStatus, timingInfo, this.run));
+
+        Reachable parent = Mockito.mock(Reachable.class);
+        Link link = Mockito.mock(Link.class);
+
+        Mockito.when(node.getStatus()).thenReturn(nodeRunStatus);
+        Mockito.when(node.getTiming()).thenReturn(timingInfo);
+        Mockito.when(timingInfo.getTotalDurationMillis()).thenReturn(1000L);
+        Mockito.when(parent.getLink()).thenReturn(link);
+        Mockito.when(node.getId()).thenReturn("dummyId");
+        Mockito.when(link.rel(node.getId())).thenReturn(link);
+        Mockito.when(node.getDisplayName()).thenReturn("DummyName");
+
+        PipelineNodeImpl underTest = Mockito.mock(PipelineNodeImpl.class, Mockito.withSettings().useConstructor(node, parent, this.run));
+        assertNotNull(underTest);
+
+        RestartDeclarativePipelineAction restartableAction = Mockito.mock(RestartDeclarativePipelineAction.class);
+        Mockito.when(this.run.getAction(RestartDeclarativePipelineAction.class)).thenReturn(restartableAction);
+        Mockito.when(restartableAction.isRestartEnabled()).thenReturn(false);
         assertFalse(underTest.isRestartable());
     }
 }
