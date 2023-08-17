79ac6bcf6a2db7c1d1094aa459fe6dbab09f08ce
Jesse Glick <jglick@cloudbees.com>
2023-08-17 16:57:17 -0400

    `PipelineApiTest.testPipelineQueue` was not fully fixed in #2450

--- blueocean-rest-impl/src/test/java/io/jenkins/blueocean/service/embedded/PipelineApiTest.java
+++ blueocean-rest-impl/src/test/java/io/jenkins/blueocean/service/embedded/PipelineApiTest.java
@@ -18,6 +18,7 @@ import hudson.model.Job;
 import hudson.model.ParametersAction;
 import hudson.model.ParametersDefinitionProperty;
 import hudson.model.Project;
+import hudson.model.Queue;
 import hudson.model.Result;
 import hudson.model.Run;
 import hudson.model.StringParameterDefinition;
@@ -547,6 +548,10 @@ public class PipelineApiTest extends BaseTest {
         b2.doStop();
         j.assertBuildStatus(Result.ABORTED, j.waitForCompletion(b1));
         j.assertBuildStatus(Result.ABORTED, j.waitForCompletion(b2));
+        for (Queue.Item i : j.jenkins.getQueue().getItems()) {
+            j.jenkins.getQueue().cancel(i);
+        }
+        j.waitUntilNoActivity();
     }
 
     @Test
