bf8411c04e147c7ecbd6ddacb3d43c63872f186d
Olivier Lamy <olamy@apache.org>
2023-09-16 22:09:23 +1000

    use max of retry and default retry
    Signed-off-by: Olivier Lamy <olamy@apache.org>

--- acceptance-tests/src/main/java/io/blueocean/ath/ATHJUnitRunner.java
+++ acceptance-tests/src/main/java/io/blueocean/ath/ATHJUnitRunner.java
@@ -172,7 +172,8 @@ public class ATHJUnitRunner extends BlockJUnit4ClassRunner {
         eachNotifier.fireTestStarted();
         List<Throwable> failures = new ArrayList<>();
         try {
-            int n = retry == null ? Integer.getInteger("ath.defaultRetry",1) : retry.value();
+            int defaultRetry = Integer.getInteger("ath.defaultRetry",1);
+            int n = retry == null ? defaultRetry : Math.max(retry.value(), defaultRetry);
 
             for (int i = 0; i < n; i++) {
                 try {
