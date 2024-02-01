28c098bc7cdf9f5719a1d48eb57488187d5bf79d
Devin Nusbaum <dnusbaum@cloudbees.com>
2024-02-01 12:47:21 -0500

    Replace DownstreamJobListener with a TransientActionFactory based on DownstreamBuildAction

--- blueocean-pipeline-api-impl/src/main/java/io/jenkins/blueocean/listeners/NodeDownstreamBuildAction.java
+++ blueocean-pipeline-api-impl/src/main/java/io/jenkins/blueocean/listeners/NodeDownstreamBuildAction.java
@@ -1,14 +1,26 @@
 package io.jenkins.blueocean.listeners;
 
+import hudson.Extension;
+import hudson.model.Action;
+import hudson.model.Actionable;
 import hudson.model.InvisibleAction;
+import hudson.model.Queue;
+import hudson.model.Run;
 import io.jenkins.blueocean.rest.Reachable;
 import io.jenkins.blueocean.rest.hal.Link;
+import io.jenkins.blueocean.rest.hal.LinkResolver;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Objects;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import jenkins.model.TransientActionFactory;
 import org.jenkinsci.plugins.workflow.actions.FlowNodeAction;
 import org.jenkinsci.plugins.workflow.graph.FlowNode;
+import org.jenkinsci.plugins.workflow.support.steps.build.DownstreamBuildAction;
 import org.kohsuke.stapler.export.Exported;
 import org.kohsuke.stapler.export.ExportedBean;
-
-import java.util.Objects;
 
 /**
  * Annotates a FlowNode to point to a downstream build triggered by said node. Applied by
@@ -16,6 +28,7 @@ import java.util.Objects;
  */
 @ExportedBean
 public class NodeDownstreamBuildAction extends InvisibleAction implements FlowNodeAction, Reachable {
+    private static final Logger LOGGER = Logger.getLogger(NodeDownstreamBuildAction.class.getName());
 
     private final Link link;
     private final String description;
@@ -56,5 +69,42 @@ public class NodeDownstreamBuildAction extends InvisibleAction implements FlowNo
     @Override
     public int hashCode() {
         return Objects.hash(link, description);
+    }
+
+    @Extension
+    public static class FactoryImpl extends TransientActionFactory<FlowNode> {
+
+        @Override
+        public Class<FlowNode> type() {
+            return FlowNode.class;
+        }
+
+        @Override
+        public Collection<? extends Action> createFor(FlowNode node) {
+            try {
+                Queue.Executable executable = node.getExecution().getOwner().getExecutable();
+                if (executable instanceof Actionable) {
+                    DownstreamBuildAction action = ((Actionable) executable).getAction(DownstreamBuildAction.class);
+                    if (action != null) {
+                        for (DownstreamBuildAction.DownstreamBuild downstreamBuild : action.getDownstreamBuilds()) {
+                            if (downstreamBuild.getFlowNodeId().equals(node.getId())) {
+                                Run<?, ?> downstream = downstreamBuild.getBuild();
+                                if (downstream != null) {
+                                    Link link = LinkResolver.resolveLink(downstream);
+                                    String description = downstream.getDescription();
+                                    if (description == null) {
+                                        description = downstream.getFullDisplayName();
+                                    }
+                                    return Collections.singleton(new NodeDownstreamBuildAction(link, description));
+                                }
+                            }
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                LOGGER.log(Level.WARNING, null, e);
+            }
+            return Collections.emptySet();
+        }
     }
 }
