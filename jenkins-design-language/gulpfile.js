26e9c44bb0af15754d8fd920f776394a99d505b7
Tim Jacomb <timjacomb1@gmail.com>
2024-04-11 19:20:22 +0100

    WIP - Upgrade node.js and required dependencies

--- jenkins-design-language/gulpfile.js
+++ jenkins-design-language/gulpfile.js
@@ -70,26 +70,45 @@ const config = {
     },
 };
 
-// Watch all
-
-gulp.task("watch", ["build"], () => {
-   gulp.watch(config.react.sources, ["compile-react"]);
-   gulp.watch(config.less.watch, ["less"]);
-});
-
-// Watch only styles, for when you're using Storybook
-
-gulp.task("watch-styles", ["build"], () => {
-   gulp.watch(config.less.watch, ["less"]);
-});
-
 // Default to all
 
-gulp.task("default", ["lint", "test", "build", "validate"]);
+var builder = require('@jenkins-cd/js-builder');
+
+builder.src([
+    'src/js',
+    'less',
+    'dist' // for icons & fonts; NOTE: would be nice to find another way to do this as files in dist creates issues for jest & eslint
+]);
+
+builder.tests(config.test.sources);
+
+// redefine 'lint' to check only react and test sources (avoid dist)
+builder.defineTask("lint", () => (
+    gulp.src([config.react.sources, config.test.sources])
+        .pipe(lint())
+        .pipe(lint.format())
+        .pipe(lint.failAfterError())
+));
+
+
+gulp.task("default", gulp.series("lint", "test", "build", "validate"));
 
 // Build all
 
-gulp.task("build", ["compile-typescript", "compile-react", "less", "copy"]);
+gulp.task("build", gulp.series("compile-typescript", "compile-react", "less", "copy"));
+
+// Watch all
+
+gulp.task("watch", gulp.series("build", () => {
+   gulp.watch(config.react.sources, ["compile-react"]);
+   gulp.watch(config.less.watch, ["less"]);
+}));
+
+// Watch only styles, for when you're using Storybook
+
+gulp.task("watch-styles", gulp.series("build", () => {
+   gulp.watch(config.less.watch, ["less"]);
+}));
 
 // Compile react sources
 
@@ -167,25 +186,6 @@ gulp.task("validate", () => {
         }
     }
 });
-
-var builder = require('@jenkins-cd/js-builder');
-
-builder.src([
-    'src/js',
-    'less',
-    'dist' // for icons & fonts; NOTE: would be nice to find another way to do this as files in dist creates issues for jest & eslint
-]);
-
-builder.tests(config.test.sources);
-
-// redefine 'lint' to check only react and test sources (avoid dist)
-builder.defineTask("lint", () => (
-    gulp.src([config.react.sources, config.test.sources])
-        .pipe(lint())
-        .pipe(lint.format())
-        .pipe(lint.failAfterError())
-));
-
 
 //
 // Create the main bundle.
