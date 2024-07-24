abbaf682120ba2843709358dcb041b4087a76d76
Basil Crow <me@basilcrow.com>
2024-07-24 16:50:37 -0700

    Simplify JSON parsing

--- blueocean-rest-impl/src/main/webapp/scripts/analytics.js
+++ blueocean-rest-impl/src/main/webapp/scripts/analytics.js
@@ -5,8 +5,7 @@ window.addEventListener('load', function() {
         headers: crumb.wrap({
             'Content-Type': 'application/json'
         }),
-        // TODO simplify when Prototype.js is removed
-        body: Object.toJSON ? Object.toJSON(eventData) : JSON.stringify(eventData)
+        body: JSON.stringify(eventData)
     }).then(function(rsp) {
         if (!rsp.ok) {
             console.error('Could not send pageview event');
