b226938eb14dfd6f19997cb0358970051e66664c
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2025-04-07 17:26:04 +0100

    Update TryBlueOceanMenuTest.java

--- blueocean-rest-impl/src/test/java/io/jenkins/blueocean/service/embedded/TryBlueOceanMenuTest.java
+++ blueocean-rest-impl/src/test/java/io/jenkins/blueocean/service/embedded/TryBlueOceanMenuTest.java
@@ -1,5 +1,6 @@
 package io.jenkins.blueocean.service.embedded;
 
+import org.htmlunit.html.DomElement;
 import org.htmlunit.html.HtmlAnchor;
 import org.htmlunit.html.HtmlPage;
 import org.junit.Assert;
@@ -9,6 +10,9 @@ import org.xml.sax.SAXException;
 
 import java.io.IOException;
 
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 /**
  * @author Vivek Pandey
  */
@@ -16,8 +20,17 @@ public class TryBlueOceanMenuTest extends BaseTest{
     @Test
     public void testOpenBlueOcean() throws IOException, SAXException {
         JenkinsRule.WebClient webClient = j.createWebClient();
-        HtmlPage page = webClient.getPage(j.getInstance().getPrimaryView());
-        HtmlAnchor anchor = page.getAnchorByHref("/jenkins/blue/organizations/jenkins/pipelines/");
-        Assert.assertEquals(Messages.BlueOceanUrlAction_DisplayName(), anchor.getTextContent());
+        HtmlPage page = webClient.getPage(j.getInstance());
+
+        DomElement sidePanel = page.querySelector("#side-panel");
+        HtmlAnchor anchor = sidePanel.getElementsByTagName("a")
+            .stream()
+            .filter(HtmlAnchor.class::isInstance)
+            .map(HtmlAnchor.class::cast)
+            .filter(a -> a.asNormalizedText().equals(Messages.BlueOceanUrlAction_DisplayName()))
+            .findFirst()
+            .orElseThrow();
+        Assert.assertEquals("/jenkins/blue/organizations/jenkins/pipelines/", anchor.getHrefAttribute());
+        assertThat(anchor.getAttribute("class"), containsString("task-link"));
     }
 }
