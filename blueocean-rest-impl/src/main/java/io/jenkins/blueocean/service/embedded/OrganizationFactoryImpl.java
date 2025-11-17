95fc5519438b21d25dd7f82557ba044815d9922a
James Nord <jtnord@users.noreply.github.com>
2025-11-17 17:52:57 +0000

    use supplier syntax in logging
    Co-authored-by: Jesse Glick <jglick@cloudbees.com>

--- blueocean-rest-impl/src/main/java/io/jenkins/blueocean/service/embedded/OrganizationFactoryImpl.java
+++ blueocean-rest-impl/src/main/java/io/jenkins/blueocean/service/embedded/OrganizationFactoryImpl.java
@@ -43,7 +43,7 @@ public class OrganizationFactoryImpl extends OrganizationFactory {
             if (root instanceof ItemGroup<?> group) {
                 this.instance = new OrganizationImpl(name, group);
             } else {
-                LOG.log(Level.WARNING, "Specified BLUE_ORGANIZATION_ROOT_FOLDER ''{0}'' not found, or not a Folder. Falling back to Jenkins root folder.", ROOT_FOLDER_NAME);
+                LOG.warning(() -> "Specified BLUE_ORGANIZATION_ROOT_FOLDER '" + ROOT_FOLDER_NAME "' not found, or not a Folder. Falling back to Jenkins root folder.");
                 this.instance = new OrganizationImpl(name, Jenkins.get());
             }
         } else {
